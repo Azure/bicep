9d1a81fac8390721c191d8c6084eefd343479cc8
Shenglong Li <shenglong.li@outlook.com>
2020-06-09 21:18:39 -0700

    Add PositionHelper and implement GetPosition with binary search

---
+++ Bicep.LangServer.UnitTests/Utils/PositionHelperTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using Bicep.LanguageServer.Utils;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OmniSharp.Extensions.LanguageServer.Protocol.Models;
+
+namespace Bicep.LangServer.UnitTests.Utils
+{
+    [TestClass]
+    public class PositionHelperTests
+    {
+        [TestMethod]
+        public void GetPosition_EmptyLineStarts_ThrowsArgumentException()
+        {
+            Action sut = () => PositionHelper.GetPosition(new List<int>().AsReadOnly(), 10);
+
+            sut.Should().Throw<ArgumentException>().WithMessage("*must not be empty*");
+        }
+
+        [TestMethod]
+        public void GetPosition_NegtiveOffset_ThrowsArgumentOutOfRangeException()
+        {
+            IReadOnlyList<int> lineStarts = new List<int> { 0, 24 }.AsReadOnly();
+            Action sut = () => PositionHelper.GetPosition(lineStarts, -9);
+
+            sut.Should().Throw<ArgumentOutOfRangeException>().WithMessage("*must not be a negative number*");
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(GetData), DynamicDataSourceType.Method)]
+        public void GetPosition_ValidLineStartsAndOffset_ReturnsConvertedPosition(IReadOnlyList<int> lineStarts, int offset, Position expectedPosition)
+        {
+            Position position = PositionHelper.GetPosition(lineStarts, offset);
+
+            position.Should().Be(expectedPosition);
+        }
+
+
+        public static IEnumerable<object[]> GetData()
+        {
+            yield return new object[] { new List<int> { 0, 12, 45 }.AsReadOnly(), 0, new Position(0, 0) };
+            yield return new object[] { new List<int> { 0, 12, 45 }.AsReadOnly(), 5, new Position(0, 5) };
+            yield return new object[] { new List<int> { 0, 12, 45 }.AsReadOnly(), 12, new Position(1, 0) };
+            yield return new object[] { new List<int> { 0, 12, 45 }.AsReadOnly(), 34, new Position(1, 22) };
+            yield return new object[] { new List<int> { 0, 12, 45 }.AsReadOnly(), 45, new Position(2, 0) };
+            yield return new object[] { new List<int> { 0, 12, 45 }.AsReadOnly(), 99, new Position(2, 54) };
+        }
+    }
+}
---
+++ src/Bicep.LangServer/Utils/PositionHelper.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using OmniSharp.Extensions.LanguageServer.Protocol.Models;
+
+namespace Bicep.LanguageServer.Utils
+{
+    public static class PositionHelper
+    {
+        public static Position GetPosition(IReadOnlyList<int> lineStarts, int offset)
+        {
+            if (lineStarts.Count == 0)
+            {
+                throw new ArgumentException($"{nameof(lineStarts)} must not be empty.");
+            }
+
+            if (offset < 0)
+            {
+                throw new ArgumentOutOfRangeException($"{nameof(offset)} must not be a negative number.");
+            }
+
+            int line = BinarySearch(lineStarts, offset);
+
+            if (line < 0)
+            {
+                // If the actual line start was not found,
+                // the binary search returns the 2's-complement of the next line start, so substracting 1.
+                line = ~line - 1;
+            }
+
+            return new Position(line, offset - lineStarts[line]);
+        }
+
+        private static int BinarySearch(IReadOnlyList<int> values, int target)
+        {
+            int start = 0;
+            int end = values.Count - 1;
+
+            while (start <= end)
+            {
+                int mid = start + (end - start) / 2;
+
+                if (values[mid] == target)
+                {
+                    return mid;
+                }
+                else if (values[mid] < target)
+                {
+                    start = mid + 1;
+                } else
+                {
+                    end = mid - 1;
+                }
+            }
+
+            return ~start;
+        }
+    }
+}
