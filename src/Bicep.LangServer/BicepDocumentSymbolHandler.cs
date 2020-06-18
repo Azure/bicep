d20e790015274942b4a134357ec150a7277992df
Marcin Jastrzebski <majastrz@microsoft.com>
2020-06-17 22:59:41 -0700

    Return empty symbols

---
+++ src/Bicep.LangServer/BicepDocumentSymbolHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using OmniSharp.Extensions.LanguageServer.Protocol;
+using OmniSharp.Extensions.LanguageServer.Protocol.Models;
+using OmniSharp.Extensions.LanguageServer.Protocol.Server;
+
+namespace Bicep.LanguageServer
+{
+    public class BicepDocumentSymbolHandler: DocumentSymbolHandler
+    {
+        public BicepDocumentSymbolHandler(ProgressManager progressManager)
+            : base(GetSymbolRegistrationOptions(), progressManager)
+        {
+        }
+
+        public override Task<SymbolInformationOrDocumentSymbolContainer> Handle(DocumentSymbolParams request, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(new SymbolInformationOrDocumentSymbolContainer(new SymbolInformationOrDocumentSymbol()));
+        }
+
+        private static DocumentSymbolRegistrationOptions GetSymbolRegistrationOptions()
+        {
+            return new DocumentSymbolRegistrationOptions
+            {
+                DocumentSelector = DocumentSelector.ForLanguage("bicep")
+            };
+        }
+    }
+}
--- src/Bicep.LangServer/Program.cs
+++ src/Bicep.LangServer/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
 using OmniSharp.Extensions.LanguageServer.Protocol.Models;
 using OmniSharp.Extensions.LanguageServer.Protocol.Server;
 using OmniSharp.Extensions.LanguageServer.Server;
@@ -14,7 +15,8 @@ namespace Bicep.LanguageServer
                 options
                     .WithInput(Console.OpenStandardInput())
                     .WithOutput(Console.OpenStandardOutput())
-                    .WithHandler<BicepTextDocumentSyncHandler>());
+                    .WithHandler<BicepTextDocumentSyncHandler>()
+                    .WithHandler<BicepDocumentSymbolHandler>());
 
             server.Document.PublishDiagnostics(new PublishDiagnosticsParams());
 
