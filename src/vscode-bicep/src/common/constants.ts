18cf025acbecfbb304c3a63c69e5dc42cefd983a
Jorge Cotillo <jcotillo@microsoft.com>
2020-07-01 21:03:55 -0700

    vscode dotnet runtime retrieval (#59)
    * initial update
    
    * removed promise
    
    * added error log info
    
    * dynamically retrieve bits
    
    * renamed function
    
    * updated error message
    
    * updated output channel var names
    
    * removed compilation from compile npm task
    
    * removed env var from launch.json
    
    Co-authored-by: Jorge Cotillo <jorge.cotillo@cotillo-corp.com>

---
+++ src/vscode-bicep/src/common/constants.ts
@@ -0,0 +1,34 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.md in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+// The dotnet version to download and run the language server against
+export const downloadDotnetVersion = '3.1';
+
+// The language server output label
+export const bicepOutputLanguageServer = "Bicep Language Server";
+
+// The extension output label
+export const bicepOutputExtension = "Bicep Extension";
+
+// The language server folder name
+export const languageServerFolderName = 'bicepLanguageServer';
+
+// The language server name
+export const languageServerName = 'Bicep Language Server';
+
+// The language server binary
+export const languageServerDllName = 'Bicep.LangServer.dll';
+
+// The default tracing level
+export const defaultTraceLevel = 'Warning';
+
+// The language server path
+export const languageServerPath = "bicepLanguageServerPath";
+
+// The workspace (user settings)
+export namespace workspaceSettings {
+    export const prefix = 'bicepLanguageServer';
+    export const traceLevel = 'bicepLanguageServer.traceLevel';
+}
