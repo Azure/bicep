44052c456a62831b2dcd7cc734345aba620359db
Anthony Martin <anthony.ct.martin@gmail.com>
2020-08-05 07:54:27 -0700

    Use Monaco for online demo

--- src/Bicep.WebDemo/Program.cs
+++ src/Bicep.WebDemo/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace Bicep.Wasm
+namespace Bicep.WebDemo
 {
     public class Program
     {
