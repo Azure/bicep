8460e693f30de737626d0f43466b498d4e375772
Anthony Martin <anthony.ct.martin@gmail.com>
2020-08-14 06:50:04 -0700

    Add basic diagnostics tests

---
+++ src/Bicep.LangServer.UnitTests/DocumentSymbolTests.cs
@@ -0,0 +1,117 @@
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OmniSharp.Extensions.LanguageServer.Protocol.Models;
+using OmniSharp.Extensions.LanguageServer.Protocol.Document;
+using OmniSharp.Extensions.LanguageServer.Protocol;
+using Bicep.LangServer.UnitTests.Assertions;
+
+namespace Bicep.LangServer.UnitTests
+{
+    [TestClass]
+    public class DocumentSymbolTests : ClientIntegrationTestBase
+    {
+        [TestMethod]
+        public async Task RequestDocumentSymbol_should_return_full_symbol_list()
+        {
+            var documentUri = DocumentUri.From("/template.bicep");
+            var diagsReceived = new TaskCompletionSource<PublishDiagnosticsParams>();
+
+            var client = await StartServerWithClientConnection(options => 
+            {
+                options.OnPublishDiagnostics(diags => {
+                    diagsReceived.SetResult(diags);
+                });
+            });
+
+            // client opens the document
+            client.TextDocument.DidOpenTextDocument(new DidOpenTextDocumentParams
+            {
+                TextDocument = new TextDocumentItem
+                {
+                    LanguageId = "bicep",
+                    Version = 0,
+                    Uri = documentUri,
+                    Text = @"
+parameter myParam string = 'test'
+resource myRes 'myRp/provider@2019-01-01' = {
+  name = 'test'
+  }
+  output myOutput string = 'myOutput'
+",
+                },
+            });
+
+            // client requests symbols
+            var symbols = await client.TextDocument.RequestDocumentSymbol(new DocumentSymbolParams
+            {
+                TextDocument = new TextDocumentIdentifier
+                {
+                    Uri = documentUri,
+                },
+            });
+
+            symbols.Should().SatisfyRespectively(
+                x => {
+                    x.DocumentSymbol.Name.Should().Be("myParam");
+                    x.DocumentSymbol.Kind.Should().Be(SymbolKind.Field);
+                    x.DocumentSymbol.Range.Should().HaveRange((1, 0), (2, 0));
+                },
+                x => {
+                    x.DocumentSymbol.Name.Should().Be("myRes");
+                    x.DocumentSymbol.Kind.Should().Be(SymbolKind.Object);
+                    x.DocumentSymbol.Range.Should().HaveRange((2, 0), (5, 0));
+                },
+                x => {
+                    x.DocumentSymbol.Name.Should().Be("myOutput");
+                    x.DocumentSymbol.Kind.Should().Be(SymbolKind.Interface);
+                    x.DocumentSymbol.Range.Should().HaveRange((5, 2), (6, 0));
+                }
+            );
+
+            // client deletes the output and renames the resource
+            client.TextDocument.DidChangeTextDocument(new DidChangeTextDocumentParams
+            {
+                TextDocument = new VersionedTextDocumentIdentifier
+                {
+                    Version = 1,
+                    Uri = documentUri,
+                },
+                ContentChanges = new Container<TextDocumentContentChangeEvent>(
+                    // for now we're sending the full document every time, nothing fancy..
+                    new TextDocumentContentChangeEvent
+                    {
+                        Text = @"
+parameter myParam string = 'test'
+resource myRenamedRes 'myRp/provider@2019-01-01' = {
+  name = 'test'
+  }
+",
+                    }
+                )
+            });
+
+            // client requests symbols
+            symbols = await client.TextDocument.RequestDocumentSymbol(new DocumentSymbolParams
+            {
+                TextDocument = new TextDocumentIdentifier
+                {
+                    Uri = documentUri,
+                },
+            });
+
+            symbols.Should().SatisfyRespectively(
+                x => {
+                    x.DocumentSymbol.Name.Should().Be("myParam");
+                    x.DocumentSymbol.Kind.Should().Be(SymbolKind.Field);
+                    x.DocumentSymbol.Range.Should().HaveRange((1, 0), (2, 0));
+                },
+                x => {
+                    x.DocumentSymbol.Name.Should().Be("myRenamedRes");
+                    x.DocumentSymbol.Kind.Should().Be(SymbolKind.Object);
+                    x.DocumentSymbol.Range.Should().HaveRange((2, 0), (5, 0));
+                }
+            );
+        }
+    }
+}
---
+++ src/Bicep.LangServer.UnitTests/TextDocumentSyncTests.cs
@@ -0,0 +1,95 @@
+﻿using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OmniSharp.Extensions.LanguageServer.Protocol.Models;
+using OmniSharp.Extensions.LanguageServer.Protocol.Document;
+using OmniSharp.Extensions.LanguageServer.Protocol;
+using Bicep.LangServer.UnitTests.Assertions;
+
+namespace Bicep.LangServer.UnitTests
+{
+    [TestClass]
+    public class TextDocumentSyncTests : ClientIntegrationTestBase
+    {
+        [TestMethod]
+        public async Task DidOpenTextDocument_should_trigger_PublishDiagnostics()
+        {
+            var documentUri = DocumentUri.From("/template.bicep");
+            var diagsReceived = new TaskCompletionSource<PublishDiagnosticsParams>();
+
+            var client = await StartServerWithClientConnection(options => 
+            {
+                options.OnPublishDiagnostics(diags => {
+                    diagsReceived.SetResult(diags);
+                });
+            });
+
+            client.TextDocument.DidOpenTextDocument(new DidOpenTextDocumentParams
+            {
+                TextDocument = new TextDocumentItem
+                {
+                    LanguageId = "bicep",
+                    Version = 1,
+                    Uri = documentUri,
+                    Text = @"
+parameter myParam string = 2
+resource myRes 'invalidFormat' = {
+
+}
+randomToken
+",
+                },
+            });
+
+            var response = await WithTimeout(diagsReceived.Task);
+            response.Diagnostics.Should().SatisfyRespectively(
+                d => {
+                    d.Range.Should().HaveRange((1, 27), (1, 28));
+                    d.Should().HaveCodeAndSeverity("BCP027", DiagnosticSeverity.Error);
+                },
+                d => {
+                    d.Range.Should().HaveRange((2, 15), (2, 30));
+                    d.Should().HaveCodeAndSeverity("BCP029", DiagnosticSeverity.Error);
+                },
+                d => {
+                    d.Range.Should().HaveRange((5, 0), (5, 11));
+                    d.Should().HaveCodeAndSeverity("BCP007", DiagnosticSeverity.Error);
+                }
+            );
+
+            diagsReceived = new TaskCompletionSource<PublishDiagnosticsParams>();
+            client.TextDocument.DidChangeTextDocument(new DidChangeTextDocumentParams
+            {
+                TextDocument = new VersionedTextDocumentIdentifier
+                {
+                    Version = 2,
+                    Uri = documentUri,
+                },
+                ContentChanges = new Container<TextDocumentContentChangeEvent>(
+                    new TextDocumentContentChangeEvent
+                    {
+                        Text = @"
+parameter myParam string = 'fixed!'
+resource myRes 'invalidFormat' = {
+
+}
+randomToken
+",
+                    }
+                ),
+            });
+
+            response = await WithTimeout(diagsReceived.Task);
+            response.Diagnostics.Should().SatisfyRespectively(
+                d => {
+                    d.Range.Should().HaveRange((2, 15), (2, 30));
+                    d.Should().HaveCodeAndSeverity("BCP029", DiagnosticSeverity.Error);
+                },
+                d => {
+                    d.Range.Should().HaveRange((5, 0), (5, 11));
+                    d.Should().HaveCodeAndSeverity("BCP007", DiagnosticSeverity.Error);
+                }
+            );
+        }
+    }
+}
