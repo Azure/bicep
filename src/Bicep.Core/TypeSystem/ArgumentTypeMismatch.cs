4339601727bae8ea5e3e5df306f54ae9b1d7f378
Shenglong Li <shenglong.li@outlook.com>
2020-08-20 22:25:38 -0700

    Add concret type mismatch classes

---
+++ src/Bicep.Core/TypeSystem/ArgumentTypeMismatch.cs
@@ -0,0 +1,31 @@
+﻿using Bicep.Core.SemanticModel;
+
+namespace Bicep.Core.TypeSystem
+{
+    public class ArgumentTypeMismatch
+    {
+        public ArgumentTypeMismatch(FunctionOverload source, int argumentIndex, TypeSymbol argumentType, TypeSymbol parameterType)
+        {
+            this.Source = source;
+            this.ArgumentIndex = argumentIndex;
+            this.ArgumentType = argumentType;
+            this.ParameterType = parameterType;
+        }
+
+        public FunctionOverload Source { get; }
+
+        public int ArgumentIndex { get; }
+
+        public TypeSymbol ArgumentType { get; }
+
+        public TypeSymbol ParameterType { get; }
+
+        public void Deconstruct(out FunctionOverload source, out int argumentIndex,  out TypeSymbol argumentType, out TypeSymbol parameterType)
+        {
+            source = this.Source;
+            argumentIndex = this.ArgumentIndex;
+            argumentType = this.ArgumentType;
+            parameterType = this.ParameterType;
+        }
+    }
+}
