79a1d576d8165fe655a29f37a076cf3d20fc950a
Marcin Jastrzebski <majastrz@microsoft.com>
2020-08-22 14:29:52 -0700

    cli tests (#323)

---
+++ src/Bicep.Cli.UnitTests/TextWriterHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Bicep.Cli.UnitTests
+{
+    public static class TextWriterHelper
+    {
+        public static string InvokeWriterAction(Action<TextWriter> action)
+        {
+            var buffer = new StringBuilder();
+            using var writer = new StringWriter(buffer);
+
+            action(writer);
+
+            writer.Flush();
+
+            return buffer.ToString();
+        }
+
+        public static (string, string) InvokeWriterAction(Action<TextWriter, TextWriter> action)
+        {
+            var firstBuffer = new StringBuilder();
+            using var firstWriter = new StringWriter(firstBuffer);
+
+            var secondBuffer = new StringBuilder();
+            using var secondWriter = new StringWriter(secondBuffer);
+
+            action(firstWriter, secondWriter);
+
+            firstWriter.Flush();
+            secondWriter.Flush();
+
+            return (firstBuffer.ToString(), secondBuffer.ToString());
+        }
+    }
+}
--- src/Bicep.Cli/CommandLine/ArgumentParser.cs
+++ src/Bicep.Cli/CommandLine/ArgumentParser.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
-using System.Reflection;
 using Bicep.Cli.CommandLine.Arguments;
 
 namespace Bicep.Cli.CommandLine
@@ -40,17 +39,15 @@ namespace Bicep.Cli.CommandLine
             return $"{versionSplit[0]} ({versionSplit[1]})";
         }
 
-        public static void PrintVersion()
+        public static void PrintVersion(TextWriter writer)
         {
-            var output =
-$@"Bicep CLI version {GetVersionString()}
-"; //newline is intentional
+            var output = $@"Bicep CLI version {GetVersionString()}{Environment.NewLine}";
 
-            Console.Out.Write(output);
-            Console.Out.Flush();
+            writer.Write(output);
+            writer.Flush();
         }
 
-        public static void PrintUsage()
+        public static void PrintUsage(TextWriter writer)
         {
             var exeName = GetExeName();
             var output = 
@@ -72,8 +69,8 @@ Usage:
       --help       Shows this usage information
 "; // this newline is intentional
 
-            Console.Out.Write(output);
-            Console.Out.Flush();
+            writer.Write(output);
+            writer.Flush();
         }
 
         private static BuildArguments ParseBuild(string[] files)
