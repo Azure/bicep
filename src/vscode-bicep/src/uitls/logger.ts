6955440230e3b40ea0bb962aace2c1e3e684001a
Shenglong Li <shenglong.li@outlook.com>
2020-08-24 00:06:58 -0700

    Clean up code

--- src/vscode-bicep/src/uitls/logger.ts
+++ src/vscode-bicep/src/uitls/logger.ts
@@ -1,3 +1,7 @@
+/* --------------------------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ * ------------------------------------------------------------------------------------------ */
 import * as vscode from "vscode";
 import * as winston from "winston";
 import * as Transport from "winston-transport";
