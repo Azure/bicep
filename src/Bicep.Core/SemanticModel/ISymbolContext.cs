37285d41861a1d7614c3e057f288550f1dd27e3e
Marcin Jastrzebski <majastrz@microsoft.com>
2020-08-25 14:50:53 -0700

    postmerge

--- src/Bicep.Core/SemanticModel/ISymbolContext.cs
+++ src/Bicep.Core/SemanticModel/ISymbolContext.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+using System.Collections.Generic;
 using Bicep.Core.Syntax;
 
 namespace Bicep.Core.SemanticModel
