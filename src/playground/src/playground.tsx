9b3d8312fba8921232d24d492f3596e37c1ee942
Anthony Martin <anthony.ct.martin@gmail.com>
2020-08-28 10:32:17 -0700

    Add copy link button

--- src/playground/src/playground.tsx
+++ src/playground/src/playground.tsx
@@ -1,14 +1,34 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { ButtonGroup, Dropdown, Nav, Navbar, NavLink } from 'react-bootstrap';
 
 import './playground.css';
 import { examples } from './examples';
 import { JsonEditor } from './jsonEditor';
 import { BicepEditor } from './bicepEditor';
+import { copyShareLinkToClipboard, handleShareLink } from './utils';
+
+let initialFile = examples['101/1vm-2nics-2subnets-1vnet'];
+handleShareLink(content => initialFile = content ?? initialFile);
 
 export const Playground : React.FC = () => {
   const [jsonContent, setJsonContent] = useState('');
-  const [exampleFile, setExampleFile] = useState(examples['101/1vm-2nics-2subnets-1vnet']);
+  const [bicepContent, setBicepContent] = useState('');
+  const [initialContent, setInitialContent] = useState(initialFile);
+  const [copied, setCopied] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener('hashchange', () => handleShareLink(content => {
+        if (content != null) {
+          setInitialContent(content);
+        }
+      }));
+  }, []);
+
+  const handlCopyClick = () => {
+    setCopied(true);
+    setTimeout(() => setCopied(false), 2000);
+    copyShareLinkToClipboard(bicepContent);
+  }
 
   const dropdownItems = Object.keys(examples).map(example => (
     <Dropdown.Item key={example} eventKey={example}>{example}</Dropdown.Item>
@@ -18,7 +38,8 @@ export const Playground : React.FC = () => {
     <Navbar bg="dark" variant="dark">
       <Navbar.Brand>Bicep Playground</Navbar.Brand>
       <Nav className="ml-auto">
-        <Dropdown as={ButtonGroup} onSelect={key => setExampleFile(examples[key])}>
+        <NavLink onClick={handlCopyClick}>{copied ? 'Copied' : 'Copy Link'}</NavLink>
+        <Dropdown as={ButtonGroup} onSelect={key => setInitialContent(examples[key])}>
           <Dropdown.Toggle as={NavLink}>Sample Template</Dropdown.Toggle>
           <Dropdown.Menu className="dropdown-menu-right">
             {dropdownItems}
@@ -28,7 +49,7 @@ export const Playground : React.FC = () => {
     </Navbar>
     <div className="playground-container">
       <div className="playground-editorpane">
-        <BicepEditor handleJsonChange={setJsonContent} initialCode={exampleFile} />
+        <BicepEditor onBicepChange={setBicepContent} onJsonChange={setJsonContent} initialCode={initialContent} />
       </div>
       <div className="playground-editorpane">
         <JsonEditor content={jsonContent} />
