aa0735927de8e343987e4a213c48d8aac88a5f18
Justin Yoo <justin.yoojh@gmail.com>
2020-09-02 11:04:48 +0900

    Add sample .bicep file for Windows 10 VM for live-streaming

---
+++ docs/examples/201/vm-windows-with-custom-script-extension/install.ps1
@@ -0,0 +1,5 @@
+#Install Chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+#Install Software
+choco install microsoft-edge obs-studio obs-ndi skype -y
