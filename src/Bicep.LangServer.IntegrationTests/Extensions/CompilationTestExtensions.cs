0956856d86d68dcf70aed5763fa0ec1a7fd67b42
Marcin Jastrzebski <majastrz@microsoft.com>
2020-09-10 20:38:04 -0700

    added tests for go to def, find refs, hovers, highligths, and renames also increased test parallelism

---
+++ src/Bicep.LangServer.IntegrationTests/Extensions/CompilationTestExtensions.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System.Collections.Generic;
+using System.Linq;
+using Bicep.Core.SemanticModel;
+using Bicep.Core.Syntax;
+using Bicep.Core.Syntax.Visitors;
+
+namespace Bicep.LangServer.IntegrationTests.Extensions
+{
+    public static class CompilationTestExtensions
+    {
+        public static IDictionary<SyntaxBase, Symbol> ReconstructSymbolTable(this Compilation compilation)
+        {
+            var model = compilation.GetSemanticModel();
+
+            var syntaxNodes = SyntaxAggregator.Aggregate(compilation.ProgramSyntax, new List<SyntaxBase>(), (accumulated, node) =>
+            {
+                accumulated.Add(node);
+                return accumulated;
+            }, accumulated => accumulated);
+
+            return syntaxNodes
+                .Where(syntax => model.GetSymbolInfo(syntax) != null)
+                .ToDictionary(syntax => syntax, syntax => model.GetSymbolInfo(syntax)!);
+        }
+    }
+}
---
+++ src/Bicep.LangServer.IntegrationTests/HoverTests.cs
@@ -0,0 +1,187 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Bicep.Core.Navigation;
+using Bicep.Core.Samples;
+using Bicep.Core.SemanticModel;
+using Bicep.Core.Syntax;
+using Bicep.Core.Syntax.Visitors;
+using Bicep.Core.Text;
+using Bicep.LangServer.IntegrationTests.Assertions;
+using Bicep.LangServer.IntegrationTests.Extensions;
+using Bicep.LanguageServer.Utils;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OmniSharp.Extensions.LanguageServer.Protocol;
+using OmniSharp.Extensions.LanguageServer.Protocol.Document;
+using OmniSharp.Extensions.LanguageServer.Protocol.Models;
+using SymbolKind = Bicep.Core.SemanticModel.SymbolKind;
+
+namespace Bicep.LangServer.IntegrationTests
+{
+    [TestClass]
+    public class HoverTests
+    {
+        [DataTestMethod]
+        [DynamicData(nameof(GetData), DynamicDataSourceType.Method, DynamicDataDisplayNameDeclaringType = typeof(DataSet), DynamicDataDisplayName = nameof(DataSet.GetDisplayName))]
+        public async Task HoveringOverSymbolReferencesAndDeclarationsShouldProduceHovers(DataSet dataSet)
+        {
+            var uri = DocumentUri.From($"/{dataSet.Name}");
+            var client = await IntegrationTestHelper.StartServerWithText(dataSet.Bicep, uri);
+
+            // construct a parallel compilation
+            var compilation = new Compilation(SyntaxFactory.CreateFromText(dataSet.Bicep));
+            var symbolTable = compilation.ReconstructSymbolTable();
+            var lineStarts = TextCoordinateConverter.GetLineStarts(dataSet.Bicep);
+
+            var symbolReferences = SyntaxAggregator.Aggregate(
+                compilation.ProgramSyntax,
+                new List<SyntaxBase>(),
+                (accumulated, node) =>
+                {
+                    if (node is ISymbolReference || node is IDeclarationSyntax)
+                    {
+                        accumulated.Add(node);
+                    }
+
+                    return accumulated;
+                },
+                accumulated => accumulated);
+
+            foreach (SyntaxBase symbolReference in symbolReferences)
+            {
+                var hover = await client.RequestHover(new HoverParams
+                {
+                    TextDocument = new TextDocumentIdentifier(uri),
+                    Position = PositionHelper.GetPosition(lineStarts, symbolReference.Span.Position)
+                });
+
+                if (symbolTable.TryGetValue(symbolReference, out var symbol) == false)
+                {
+                    // symbol ref not bound to a symbol
+                    ValidateEmptyHover(hover);
+                    continue;
+                }
+
+                switch (symbol!.Kind)
+                {
+                    case SymbolKind.Error:
+                        // error symbol
+                        ValidateEmptyHover(hover);
+                        break;
+
+                    case SymbolKind.Function when symbolReference is VariableAccessSyntax:
+                        // variable got bound to a function
+                        ValidateEmptyHover(hover);
+                        break;
+
+                    default:
+                        ValidateHover(hover, symbol);
+                        break;
+                }
+            }
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(GetData), DynamicDataSourceType.Method, DynamicDataDisplayNameDeclaringType = typeof(DataSet), DynamicDataDisplayName = nameof(DataSet.GetDisplayName))]
+        public async Task HoveringOverNonHoverableElementsShouldProduceEmptyHovers(DataSet dataSet)
+        {
+            // local function
+            bool IsNonHoverable(SyntaxBase node) => !(node is ISymbolReference) && !(node is IDeclarationSyntax);
+
+            var uri = DocumentUri.From($"/{dataSet.Name}");
+            var client = await IntegrationTestHelper.StartServerWithText(dataSet.Bicep, uri);
+
+            // construct a parallel compilation
+            var compilation = new Compilation(SyntaxFactory.CreateFromText(dataSet.Bicep));
+            var symbolTable = compilation.ReconstructSymbolTable();
+            var lineStarts = TextCoordinateConverter.GetLineStarts(dataSet.Bicep);
+
+            var nonHoverableNodes = SyntaxAggregator.Aggregate(
+                compilation.ProgramSyntax,
+                new List<SyntaxBase>(),
+                (accumulated, node) =>
+                {
+                    if (IsNonHoverable(node) && !(node is ProgramSyntax))
+                    {
+                        accumulated.Add(node);
+                    }
+
+                    return accumulated;
+                },
+                accumulated => accumulated,
+                // don't visit hoverable nodes or their children
+                (accumulated, node) => IsNonHoverable(node));
+
+            foreach (SyntaxBase node in nonHoverableNodes)
+            {
+                var hover = await client.RequestHover(new HoverParams
+                {
+                    TextDocument = new TextDocumentIdentifier(uri),
+                    Position = PositionHelper.GetPosition(lineStarts, node.Span.Position)
+                });
+
+                ValidateEmptyHover(hover);
+            }
+        }
+
+        private static void ValidateHover(Hover hover, Symbol symbol)
+        {
+            hover.Range.Should().NotBeNull();
+            hover.Contents.Should().NotBeNull();
+
+            hover.Contents.HasMarkedStrings.Should().BeFalse();
+            hover.Contents.HasMarkupContent.Should().BeTrue();
+            hover.Contents.MarkedStrings.Should().BeNull();
+            hover.Contents.MarkupContent.Should().NotBeNull();
+
+            hover.Contents.MarkupContent.Kind.Should().Be(MarkupKind.Markdown);
+            hover.Contents.MarkupContent.Value.Should().StartWith("```bicep\n");
+            hover.Contents.MarkupContent.Value.Should().EndWith("```");
+
+            switch (symbol)
+            {
+                case ParameterSymbol parameter:
+                    hover.Contents.MarkupContent.Value.Should().Contain($"param {parameter.Name}: {parameter.Type}");
+                    break;
+
+                case VariableSymbol variable:
+                    hover.Contents.MarkupContent.Value.Should().Contain($"var {variable.Name}: {variable.Type}");
+                    break;
+
+                case ResourceSymbol resource:
+                    hover.Contents.MarkupContent.Value.Should().Contain($"resource {resource.Name}");
+                    hover.Contents.MarkupContent.Value.Should().Contain(resource.Type.Name);
+                    break;
+
+                case OutputSymbol output:
+                    hover.Contents.MarkupContent.Value.Should().Contain($"output {output.Name}: {output.Type}");
+                    break;
+
+                case FunctionSymbol function:
+                    hover.Contents.MarkupContent.Value.Should().Contain($"function {function.Name}(");
+                    break;
+
+                default:
+                    throw new AssertFailedException($"Unexpected symbol type '{symbol.GetType().Name}'");
+            }
+        }
+
+        private void ValidateEmptyHover(Hover hover)
+        {
+            hover.Range.Should().BeNull();
+            hover.Contents.Should().NotBeNull();
+            hover.Contents.HasMarkedStrings.Should().BeTrue();
+            hover.Contents.HasMarkupContent.Should().BeFalse();
+            hover.Contents.MarkupContent.Should().BeNull();
+            hover.Contents.MarkedStrings.Should().SatisfyRespectively(ms => ms.Value.Should().BeEmpty());
+        }
+
+        private static IEnumerable<object[]> GetData()
+        {
+            return DataSets.AllDataSets.ToDynamicTestData();
+        }
+    }
+}
