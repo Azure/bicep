226982f48e8588218bb6bbe4de42d5bfbc604208
Anthony Martin <antmarti@microsoft.com>
2020-09-14 13:18:18 -0700

    Allow example selection in playground (#485)
    * Example template switcher in playground
    
    * Minor tweaks

---
+++ src/playground/src/jsonEditor.tsx
@@ -0,0 +1,29 @@
+import React, { useRef } from 'react';
+import MonacoEditor from 'react-monaco-editor';
+
+interface JsonEditorProps {
+  content: string;
+}
+
+export const JsonEditor : React.FC<JsonEditorProps> = props=> {
+  const options = {
+    automaticLayout: true,
+    minimap: {
+      enabled: false,
+    },
+    readOnly: true,
+  };
+  
+  const monacoRef = useRef<MonacoEditor>();
+  
+  // clear the selection after rendering completes
+  setTimeout(() => monacoRef.current.editor.setSelection({startColumn: 1, startLineNumber: 1, endColumn: 1, endLineNumber: 1}), 0);
+
+  return <MonacoEditor
+    ref={monacoRef}
+    language="json"
+    theme="vs-dark"
+    value={props.content}
+    options={options}
+  />
+};
---
+++ src/playground/src/lspInterop.ts
@@ -0,0 +1,26 @@
+import { editor, languages } from 'monaco-editor/esm/vs/editor/editor.api';
+
+let interop;
+
+export function initializeInterop(self): Promise<boolean> {
+  return new Promise<boolean>((resolve, reject) => {
+    self['BicepInitialize'] = (newInterop) => {
+      interop = newInterop;
+      resolve(true);
+    }
+  
+    const test = require('../../Bicep.Wasm/bin/Release/netstandard2.1/wwwroot/_framework/blazor.webassembly.js');  
+  });
+}
+
+export function getSemanticTokensLegend(): languages.SemanticTokensLegend {
+  return interop.invokeMethod('GetSemanticTokensLegend');
+}
+
+export function getSemanticTokens(content: string): languages.SemanticTokens {
+  return interop.invokeMethod('GetSemanticTokens', content);
+}
+
+export function compileAndEmitDiagnostics(content: string): {template: string, diagnostics: editor.IMarkerData[]} {
+  return interop.invokeMethod('CompileAndEmitDiagnostics', content);
+}
