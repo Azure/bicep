7d419749f260899dcd096dae9fc4fc28991bcf26
Anthony Martin <antmarti@microsoft.com>
2020-09-17 18:39:35 -0700

    Handle language server shutdown gracefully (#507)

--- src/Bicep.LangServer/Program.cs
+++ src/Bicep.LangServer/Program.cs
@@ -9,12 +9,38 @@ namespace Bicep.LanguageServer
     public class Program
     {
         public static async Task Main(string[] args)
-        {
-            // the server uses JSON-RPC over stdin & stdout to communicate,
-            // so be careful not to use console for logging!
-            var server = new Server(Console.OpenStandardInput(), Console.OpenStandardOutput());
+            => await RunWithCancellation(async cancellationToken =>
+            {
+                // the server uses JSON-RPC over stdin & stdout to communicate,
+                // so be careful not to use console for logging!
+                var server = new Server(Console.OpenStandardInput(), Console.OpenStandardOutput());
 
-            await server.Run(CancellationToken.None);
+                await server.Run(cancellationToken);
+            });
+
+        private static async Task RunWithCancellation(Func<CancellationToken, Task> runFunc)
+        {
+            var cancellationTokenSource = new CancellationTokenSource();
+
+            Console.CancelKeyPress += (sender, e) =>
+            {
+                cancellationTokenSource.Cancel();
+                e.Cancel = true;
+            };
+
+            AppDomain.CurrentDomain.ProcessExit += (sender, e) =>
+            {
+                cancellationTokenSource.Cancel();
+            };
+
+            try
+            {
+                await runFunc(cancellationTokenSource.Token);
+            }
+            catch (OperationCanceledException exception) when (exception.CancellationToken == cancellationTokenSource.Token)
+            {
+                // this is expected - no need to rethrow
+            }
         }
     }
 }
