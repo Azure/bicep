f2626bb45039ddc155a318be7bf91a20580f7750
Shenglong Li <shenglong.li@outlook.com>
2020-10-17 22:40:24 -0700

    Enable eslint for *.js files

--- src/vscode-bicep/gulpfile.js
+++ src/vscode-bicep/gulpfile.js
@@ -1,24 +1,30 @@
-var gulp = require('gulp');
-var nbgv = require('nerdbank-gitversioning')
-const fs = require('fs');
-const util = require('util');
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+var gulp = require("gulp");
+var nbgv = require("nerdbank-gitversioning");
+const fs = require("fs");
+const util = require("util");
 
-gulp.task('setversion', function() {
-    const renameAsync = util.promisify(fs.rename);
+gulp.task("setversion", function () {
+  const renameAsync = util.promisify(fs.rename);
 
-    return renameAsync('package-lock.json', 'package-lock.backup').then(function() {
-        return nbgv.setPackageVersion().finally(function() {
-            return renameAsync('package-lock.backup', 'package-lock.json');
-        });
-    });
+  return renameAsync("package-lock.json", "package-lock.backup").then(
+    function () {
+      return nbgv.setPackageVersion().finally(function () {
+        return renameAsync("package-lock.backup", "package-lock.json");
+      });
+    }
+  );
 });
 
-gulp.task('resetversion', function() {
-    const renameAsync = util.promisify(fs.rename);
+gulp.task("resetversion", function () {
+  const renameAsync = util.promisify(fs.rename);
 
-    return renameAsync('package-lock.json', 'package-lock.backup').then(function() {
-        return nbgv.resetPackageVersionPlaceholder().finally(function() {
-            return renameAsync('package-lock.backup', 'package-lock.json');
-        });
-    });
+  return renameAsync("package-lock.json", "package-lock.backup").then(
+    function () {
+      return nbgv.resetPackageVersionPlaceholder().finally(function () {
+        return renameAsync("package-lock.backup", "package-lock.json");
+      });
+    }
+  );
 });
