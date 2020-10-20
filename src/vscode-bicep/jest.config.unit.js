da831952dcdb6aab4ebc7998c989022598bb027a
Shenglong Li <shenglong.li@outlook.com>
2020-10-20 12:11:57 -0700

    Collect coverage for all files

--- src/vscode-bicep/jest.config.unit.js
+++ src/vscode-bicep/jest.config.unit.js
@@ -1,10 +1,10 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 module.exports = {
-  roots: ["<rootDir>/out/test/unit"],
   testMatch: ["<rootDir>/out/test/unit/**/*.test.js"],
   verbose: true,
   collectCoverage: true,
+  collectCoverageFrom: ["<rootDir>/out/**/*.js"],
   coveragePathIgnorePatterns: ["/test/"],
   setupFilesAfterEnv: ["<rootDir>/out/test/unit/setup.js"],
 };
