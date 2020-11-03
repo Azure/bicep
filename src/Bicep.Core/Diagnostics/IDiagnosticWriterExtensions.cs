63416a033a4c8686aa95ec1934468daec839c291
Anthony Martin <anthony.ct.martin@gmail.com>
2020-11-02 18:40:45 -0800

    Check for multiple instances of targetScope declaration

--- src/Bicep.Core/Diagnostics/IDiagnosticWriterExtensions.cs
+++ src/Bicep.Core/Diagnostics/IDiagnosticWriterExtensions.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 using System.Collections.Generic;
+using Bicep.Core.Parser;
 
 namespace Bicep.Core.Diagnostics
 {
@@ -16,5 +17,8 @@ namespace Bicep.Core.Diagnostics
 
         public static void WriteMultiple(this IDiagnosticWriter diagnosticWriter, params Diagnostic[] diagnostics)
             => WriteMultiple(diagnosticWriter, (IEnumerable<Diagnostic>) diagnostics);
+
+        public static void Write(this IDiagnosticWriter diagnosticWriter, IPositionable positionable, DiagnosticBuilder.DiagnosticBuilderDelegate buildDiagnosticFunc)
+            => diagnosticWriter.Write(buildDiagnosticFunc(DiagnosticBuilder.ForPosition(positionable)));
     }
 }
