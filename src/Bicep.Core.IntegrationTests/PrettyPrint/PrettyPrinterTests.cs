6b9911110baabe4c4e1a9a710f1333d62482f9b9
Shenglong Li <shenglong.li@outlook.com>
2020-11-04 21:39:58 -0800

    Add formatter round trip test

--- src/Bicep.Core.IntegrationTests/PrettyPrint/PrettyPrinterTests.cs
+++ src/Bicep.Core.IntegrationTests/PrettyPrint/PrettyPrinterTests.cs
@@ -3,6 +3,8 @@
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
+using System.Text;
+using Bicep.Core.Parser;
 using Bicep.Core.PrettyPrint;
 using Bicep.Core.PrettyPrint.Options;
 using Bicep.Core.Samples;
@@ -28,12 +30,7 @@ namespace Bicep.Core.IntegrationTests.PrettyPrint
             var options = new PrettyPrintOptions(NewlineOption.Auto, IndentKindOption.Space, 2, true);
 
             var formattedOutput = PrettyPrinter.PrintProgram(program, options);
-
             formattedOutput.Should().NotBeNull();
-
-            // The program should still be parsed without any errors after formatting.
-            program = ParserHelper.Parse(formattedOutput!);
-            program.GetParseDiagnostics().Should().BeEmpty();
 
             var resultsFile = FileHelper.SaveResultFile(this.TestContext!, Path.Combine(dataSet.Name, DataSet.TestFileMainFormatted), formattedOutput!);
 
@@ -41,6 +38,36 @@ namespace Bicep.Core.IntegrationTests.PrettyPrint
                 formattedOutput!,
                 expectedLocation: OutputHelper.GetBaselineUpdatePath(dataSet, DataSet.TestFileMainFormatted),
                 actualLocation: resultsFile);
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(GetData), DynamicDataSourceType.Method, DynamicDataDisplayNameDeclaringType = typeof(DataSet), DynamicDataDisplayName = nameof(DataSet.GetDisplayName))]
+        public void PrintProgram_ProgramWithoutDiagnostics_ShouldRoundTrip(DataSet dataSet)
+        {
+            var program = ParserHelper.Parse(dataSet.Bicep);
+            var options = new PrettyPrintOptions(NewlineOption.Auto, IndentKindOption.Space, 2, true);
+
+            var formattedOutput = PrettyPrinter.PrintProgram(program, options);
+            formattedOutput.Should().NotBeNull();
+
+            // The program should still be parsed without any errors after formatting.
+            var formattedProgram = ParserHelper.Parse(formattedOutput!);
+            formattedProgram.GetParseDiagnostics().Should().BeEmpty();
+
+            var buffer = new StringBuilder();
+            var printVisitor = new PrintVisitor(buffer,x =>
+                // Remove newlines and whitespaces.
+                (x is Token token && token.Type == TokenType.NewLine) ||
+                (x is SyntaxTrivia trivia && trivia.Type == SyntaxTriviaType.Whitespace));
+
+            printVisitor.Visit(program);
+            string programText = buffer.ToString();
+
+            buffer.Clear();
+            printVisitor.Visit(program);
+            string formattedProgramText = buffer.ToString();
+
+            formattedProgramText.Should().Be(programText);
         }
 
         private static IEnumerable<object[]> GetData() => DataSets.DataSetsWithNoDiagnostics.ToDynamicTestData();
