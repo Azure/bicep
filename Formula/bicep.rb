0d12eb2a99c92423ef14fec2accd731d907f00e3
Lyon Till <158992+ljtill@users.noreply.github.com>
2020-11-12 15:21:58 +0000

    Bump Homebrew version from v0.1.37 to v0.2.3 (#879)
    * Update bicep.rb
    
    Update the URL & SHA256 to latest version
    
    * Update bicep.rb
    
    Update the 'url' & 'sha256' to match the new v0.2.3 release

--- Formula/bicep.rb
+++ Formula/bicep.rb
@@ -1,8 +1,8 @@
 class Bicep < Formula
   desc "Bicep: next generation template language for Azure Resource Manager (ARM)"
   homepage "https://github.com/Azure/bicep"
-  url "https://github.com/Azure/bicep/releases/download/v0.1.37-alpha/bicep-osx-x64"
-  sha256 "9502397b2680db391135d9558efefb1e14c3b1c4cd6385d8b53e50bf49b2dcbf"
+  url "https://github.com/Azure/bicep/releases/download/v0.2.3/bicep-osx-x64"
+  sha256 "a96e8aab745fe67dacc37d6d1f14dcf41fb439dabd8c40297fde030d2d4819a0"
 
   license "MIT"
 
