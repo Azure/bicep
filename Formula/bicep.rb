717d36184cfa9f86db71fcf63f9d760908955006
Anthony Martin <antmarti@microsoft.com>
2020-11-24 11:21:13 -0800

    Add version to Bicep formula (#995)

--- Formula/bicep.rb
+++ Formula/bicep.rb
@@ -1,6 +1,7 @@
 class Bicep < Formula
   desc "Bicep: next generation template language for Azure Resource Manager (ARM)"
   homepage "https://github.com/Azure/bicep"
+  version "0.2.59"
   url "https://github.com/Azure/bicep/releases/download/v0.2.59/bicep-osx-x64"
   sha256 "9b07831f1ac59a613ed8f95f23e691387ad2f3f0d70872b0b4737c4736c09013"
 
