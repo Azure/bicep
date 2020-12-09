c42a9083d83c1c664e2c7db5c3907ab362d64264
Shenglong Li <shenglong.li@outlook.com>
2020-12-09 10:04:32 -0800

    Implement retrying logic for VSCode E2E tests (#1096)
    * Run e2e tests with retry
    
    * Fix retryWhile

--- src/vscode-bicep/src/test/e2e/setup.ts
+++ src/vscode-bicep/src/test/e2e/setup.ts
@@ -5,3 +5,5 @@ process.env.NODE_ENV = "test";
 // Workaround for https://github.com/microsoft/vscode-test/issues/37.
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 jest.mock("vscode", () => (global as any).vscode, { virtual: true });
+
+jest.setTimeout(20000);
--- src/vscode-bicep/src/test/utils/time.ts
+++ src/vscode-bicep/src/test/utils/time.ts
@@ -3,3 +3,24 @@
 export function sleep(ms: number): Promise<void> {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
+
+export async function retryWhile<T>(
+  func: () => Promise<T>,
+  predicate: (result: T) => boolean,
+  retryOptions?: Readonly<{
+    interval?: number;
+    count?: number;
+  }>
+): Promise<T> {
+  let result = await func();
+
+  const interval = retryOptions?.interval ?? 2000;
+  let count = retryOptions?.count ?? 3;
+
+  while (predicate(result) && count--) {
+    result = await func();
+    await sleep(interval);
+  }
+
+  return result;
+}
