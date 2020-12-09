eae706a92d1bd19bedc592c67717151508f9a88d
Marcin Jastrzebski <majastrz@microsoft.com>
2020-12-08 23:32:49 -0800

    fix

--- src/vscode-bicep/src/test/e2e/runTests.ts
+++ src/vscode-bicep/src/test/e2e/runTests.ts
@@ -2,7 +2,7 @@
 // Licensed under the MIT License.
 import * as path from "path";
 import * as cp from "child_process";
-import * as fs from "fs";
+import * as os from "os";
 import {
   runTests,
   downloadAndUnzipVSCode,
@@ -19,25 +19,32 @@ async function go() {
     // some of our builds run as root in a container, which requires passing
     // the user data folder path to vs code itself
     const userDataDir = path.resolve(__dirname, "user-data");
+    const userDataArgument = `--user-data-dir="${userDataDir}"`;
 
-    fs.mkdirSync(userDataDir, { recursive: true });
+    const isRoot = os.userInfo().username == "root";
+
+    const extensionInstallArguments = [
+      "--install-extension",
+      "ms-dotnettools.vscode-dotnet-runtime",
+    ];
+
+    if (isRoot) {
+      extensionInstallArguments.push(userDataArgument);
+    }
 
     // Install .NET Install Tool as a dependency.
-    cp.spawnSync(
-      cliPath,
-      [
-        "--install-extension",
-        "ms-dotnettools.vscode-dotnet-runtime",
-        `--user-data-dir="${userDataDir}"`,
-      ],
-      { encoding: "utf-8", stdio: "inherit" }
-    );
+    cp.spawnSync(cliPath, extensionInstallArguments, {
+      encoding: "utf-8",
+      stdio: "inherit",
+    });
 
     await runTests({
       vscodeExecutablePath,
       extensionDevelopmentPath: path.resolve(__dirname, "../../.."),
       extensionTestsPath: path.resolve(__dirname, "index"),
-      launchArgs: ["--enable-proposed-api", `--user-data-dir="${userDataDir}"`],
+      launchArgs: isRoot
+        ? ["--enable-proposed-api", userDataArgument]
+        : ["--enable-proposed-api"],
     });
 
     process.exit(0);
