f337ec1284e5b270130ab07271f09ffd6221d6f7
Tarun Sunkaraneni <tsunkaraneni@microsoft.com>
2021-02-15 15:21:27 -0700

    added missing import.

--- src/Bicep.LangServer.IntegrationTests/Helpers/TextDocumentParamHelper.cs
+++ src/Bicep.LangServer.IntegrationTests/Helpers/TextDocumentParamHelper.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT License.
 using OmniSharp.Extensions.LanguageServer.Protocol;
 using OmniSharp.Extensions.LanguageServer.Protocol.Models;
+using Bicep.Core;
 
 namespace Bicep.LangServer.IntegrationTests.Helpers
 {
