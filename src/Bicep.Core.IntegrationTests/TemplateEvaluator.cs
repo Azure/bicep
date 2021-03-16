00d5f1137724aac1ac75240d2f33207742f28d7e
Anthony Martin <anthony.ct.martin@gmail.com>
2021-03-16 07:25:28 -0400

    Add more evaluation tests

--- src/Bicep.Core.IntegrationTests/TemplateEvaluator.cs
+++ src/Bicep.Core.IntegrationTests/TemplateEvaluator.cs
@@ -19,16 +19,39 @@ namespace Bicep.Core.IntegrationTests
 {
     public class TemplateEvaluator
     {
+        const string TestTenantId = "d4c73686-f7cd-458e-b377-67adcd46b624";
+        const string TestManagementGroupName = "3fc9f36e-8699-43af-b038-1c103980942f";
+        const string TestSubscriptionId = "f91a30fd-f403-4999-ae9f-ec37a6d81e13";
+        const string TestResourceGroupName = "testResourceGroup";
+        const string TestLocation = "West US";
+
         public delegate JToken OnListDelegate(string functionName, string resourceId, string apiVersion, JToken? body);
 
         public delegate JToken OnReferenceDelegate(string resourceId, string apiVersion, bool fullBody);
 
         public record EvaluationConfiguration(
-            ImmutableDictionary<string, JToken> Parameters,
+            string TenantId,
+            string ManagementGroup,
+            string SubscriptionId,
+            string ResourceGroup,
+            string RgLocation,
+            Dictionary<string, JToken> Parameters,
             OnListDelegate? OnListFunc,
-            OnReferenceDelegate? OnReferenceFunc);
+            OnReferenceDelegate? OnReferenceFunc)
+        {
+            public static EvaluationConfiguration Default = new(
+                TestTenantId,
+                TestManagementGroupName,
+                TestSubscriptionId,
+                TestResourceGroupName,
+                TestLocation,
+                new(),
+                null,
+                null
+            );
+        }
 
-        private static string GetResourceId(TemplateResource resource)
+        private static string GetResourceId(string scopeString, TemplateResource resource)
         {
             var typeSegments = resource.Type.Value.Split('/');
             var nameSegments = resource.Name.Value.Split('/');
@@ -36,21 +59,28 @@ namespace Bicep.Core.IntegrationTests
             var types = new [] { typeSegments.First() }
                 .Concat(typeSegments.Skip(1).Zip(nameSegments, (type, name) => $"{type}/{name}"));
 
-            return string.Join('/', types);
+            return $"{scopeString}providers/{string.Join('/', types)}";
         }
 
-        private static void ProcessTemplateLanguageExpressions(Template template, EvaluationConfiguration configuration)
+        private static void ProcessTemplateLanguageExpressions(Template template, EvaluationConfiguration config, TemplateDeploymentScope deploymentScope)
         {
-            var resourceLookup = template.Resources.ToOrdinalInsensitiveDictionary(GetResourceId);
+            var scopeString = deploymentScope switch {
+                TemplateDeploymentScope.Tenant => "/",
+                TemplateDeploymentScope.ManagementGroup => $"/providers/Microsoft.Management/managementGroups/{config.ManagementGroup}/",
+                TemplateDeploymentScope.Subscription => $"/subscriptions/{config.SubscriptionId}/",
+                TemplateDeploymentScope.ResourceGroup => $"/subscriptions/{config.SubscriptionId}/resourceGroups/{config.ResourceGroup}/",
+                _ => throw new InvalidOperationException(),
+            };
+
+            var resourceLookup = template.Resources.ToOrdinalInsensitiveDictionary(x => GetResourceId(scopeString, x));
 
             var evaluationContext = TemplateEngine.GetExpressionEvaluationContext(template);
-            var oldEvaluateFunction = evaluationContext.EvaluateFunction;
+            var defaultEvaluateFunction = evaluationContext.EvaluateFunction;
             evaluationContext.EvaluateFunction = (FunctionExpression functionExpression, JToken[] parameters, TemplateErrorAdditionalInfo additionalInfo) =>
             {
-                if (functionExpression.Function.StartsWithOrdinalInsensitively("list") &&
-                    configuration.OnListFunc is not null)
+                if (functionExpression.Function.StartsWithOrdinalInsensitively("list") && config.OnListFunc is not null)
                 {
-                    return configuration.OnListFunc(
+                    return config.OnListFunc(
                         functionExpression.Function,
                         parameters[0].ToString(),
                         parameters[1].ToString(),
@@ -63,23 +93,19 @@ namespace Bicep.Core.IntegrationTests
                     var apiVersion = parameters.Length > 1 ? parameters[1].ToString() : null;
                     var fullBody = parameters.Length > 2 ? parameters[2].ToString().EqualsOrdinalInsensitively("Full") : false;
 
-                    // TODO use exact matching with the scope of the template
-                    var foundResource = resourceLookup.FirstOrDefault(x => resourceId.EndsWithOrdinalInsensitively(x.Key)).Value;
-                    if (apiVersion is not null || foundResource is not null)
+                    if (resourceLookup.TryGetValue(resourceId, out var foundResource) && 
+                        (apiVersion is null || StringComparer.OrdinalIgnoreCase.Equals(apiVersion, foundResource.ApiVersion.Value)))
                     {
-                        if (apiVersion is null || foundResource is not null && apiVersion.EqualsOrdinalInsensitively(foundResource.ApiVersion.Value))
-                        {
-                            return fullBody ? foundResource.ToJToken() : foundResource.Properties.ToJToken();
-                        }
+                        return fullBody ? foundResource.ToJToken() : foundResource.Properties.ToJToken();
+                    }
 
-                        if (configuration.OnReferenceFunc is not null)
-                        {
-                            return configuration.OnReferenceFunc(resourceId, apiVersion, fullBody);
-                        }
+                    if (apiVersion is not null && config.OnReferenceFunc is not null)
+                    {
+                        return config.OnReferenceFunc(resourceId, apiVersion, fullBody);
                     }
                 }
 
-                var value = oldEvaluateFunction(functionExpression, parameters, additionalInfo);
+                var value = defaultEvaluateFunction(functionExpression, parameters, additionalInfo);
                 return value;
             };
 
@@ -87,7 +113,7 @@ namespace Bicep.Core.IntegrationTests
             {
                 var resource = template.Resources[i];
 
-                resource.Properties.Value = ExpressionsEngine.EvaluateLanguageExpressionsOptimistically(
+                resource.Properties.Value = ExpressionsEngine.EvaluateLanguageExpressionsRecursive(
                     root: resource.Properties.Value,
                     evaluationContext: evaluationContext);
             }
@@ -96,7 +122,7 @@ namespace Bicep.Core.IntegrationTests
             {
                 foreach (var outputKey in template.Outputs.Keys.ToList())
                 {
-                    template.Outputs[outputKey].Value.Value = ExpressionsEngine.EvaluateLanguageExpressionsOptimistically(
+                    template.Outputs[outputKey].Value.Value = ExpressionsEngine.EvaluateLanguageExpressionsRecursive(
                         root: template.Outputs[outputKey].Value.Value,
                         evaluationContext: evaluationContext);
                 }
@@ -105,10 +131,7 @@ namespace Bicep.Core.IntegrationTests
 
         public static JToken Evaluate(JToken? templateJtoken, Func<EvaluationConfiguration, EvaluationConfiguration>? configurationBuilder = null)
         {
-            var configuration = new EvaluationConfiguration(
-                ImmutableDictionary<string, JToken>.Empty,
-                null,
-                null);
+            var configuration = EvaluationConfiguration.Default;
 
             if (configurationBuilder is not null)
             {
@@ -118,12 +141,8 @@ namespace Bicep.Core.IntegrationTests
             return EvaluateTemplate(templateJtoken, configuration);
         }
 
-        private static JToken EvaluateTemplate(JToken? templateJtoken, EvaluationConfiguration configuration)
+        private static JToken EvaluateTemplate(JToken? templateJtoken, EvaluationConfiguration config)
         {
-            const string testSubscriptionId = "f91a30fd-f403-4999-ae9f-ec37a6d81e13";
-            const string testResourceGroupName = "testResourceGroup";
-            const string testLocation = "West US";
-
             DeploymentsInterop.Initialize();
 
             templateJtoken = templateJtoken ?? throw new ArgumentNullException(nameof(templateJtoken));
@@ -140,25 +159,27 @@ namespace Bicep.Core.IntegrationTests
             if (deploymentScope == TemplateDeploymentScope.Subscription || deploymentScope == TemplateDeploymentScope.ResourceGroup)
             {
                 metadata["subscription"] = new JObject {
-                    ["id"] = $"/subscriptions/{testSubscriptionId}",
+                    ["id"] = $"/subscriptions/{config.SubscriptionId}",
+                    ["subscriptionId"] = config.SubscriptionId,
+                    ["tenantId"] = config.TenantId,
                 };
             }
             if (deploymentScope == TemplateDeploymentScope.ResourceGroup)
             {
                 metadata["resourceGroup"] = new JObject {
-                    ["id"] = $"/subscriptions/{testSubscriptionId}/resourceGroups/{testResourceGroupName}",
-                    ["location"] = testLocation,
+                    ["id"] = $"/subscriptions/{config.SubscriptionId}/resourceGroups/{config.ResourceGroup}",
+                    ["location"] = config.RgLocation,
                 };
             };
 
             var template = TemplateEngine.ParseTemplate(templateJtoken.ToString());
 
             TemplateEngine.ValidateTemplate(template, "2020-06-01", deploymentScope);
-            TemplateEngine.ParameterizeTemplate(template, new InsensitiveDictionary<JToken>(configuration.Parameters), metadata, new InsensitiveDictionary<JToken>());
+            TemplateEngine.ParameterizeTemplate(template, new InsensitiveDictionary<JToken>(config.Parameters), metadata, new InsensitiveDictionary<JToken>());
 
             TemplateEngine.ProcessTemplateLanguageExpressions(template, "2020-06-01");
 
-            ProcessTemplateLanguageExpressions(template, configuration);
+            ProcessTemplateLanguageExpressions(template, config, deploymentScope);
 
             TemplateEngine.ValidateProcessedTemplate(template, "2020-06-01", deploymentScope);
 
