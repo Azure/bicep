87e87efc913c1904a7cdb4746575fae2ef82a47e
Anthony Martin <antmarti@microsoft.com>
2021-03-17 21:23:26 -0400

    Add fancy hover assertions

--- src/Bicep.LangServer.IntegrationTests/HoverTests.cs
+++ src/Bicep.LangServer.IntegrationTests/HoverTests.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Threading.Tasks;
 using Azure.Bicep.Types.Az;
+using Bicep.Core.Extensions;
 using Bicep.Core.Navigation;
 using Bicep.Core.Parsing;
 using Bicep.Core.Samples;
@@ -19,6 +20,7 @@ using Bicep.LangServer.IntegrationTests.Extensions;
 using Bicep.LangServer.IntegrationTests.Helpers;
 using Bicep.LanguageServer.Utils;
 using FluentAssertions;
+using FluentAssertions.Execution;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OmniSharp.Extensions.LanguageServer.Protocol;
 using OmniSharp.Extensions.LanguageServer.Protocol.Document;
@@ -33,6 +35,20 @@ namespace Bicep.LangServer.IntegrationTests
     {
         [NotNull]
         public TestContext? TestContext { get; set; }
+
+        private static IAssertionScope CreateAssertionScopeWithContext(SyntaxTree syntaxTree, Hover? hover, IPositionable requestedPosition)
+        {
+            var assertionScope = new AssertionScope();
+
+            // TODO: figure out how to set this only on failure, rather than always calculating it
+            assertionScope.AddReportable(
+                "hover context",
+                PrintHelper.PrintWithAnnotations(syntaxTree, new [] { 
+                    new PrintHelper.Annotation(requestedPosition.Span, "cursor position"),
+                }, 1, true));
+
+            return assertionScope;
+        }
 
         [DataTestMethod]
         [DynamicData(nameof(GetData), DynamicDataSourceType.Method, DynamicDataDisplayNameDeclaringType = typeof(DataSet), DynamicDataDisplayName = nameof(DataSet.GetDisplayName))]
@@ -63,50 +79,54 @@ namespace Bicep.LangServer.IntegrationTests
 
             foreach (SyntaxBase symbolReference in symbolReferences)
             {
-                var syntaxPosition = symbolReference switch
+                var nodeForHover = symbolReference switch
                 {
-                    ITopLevelDeclarationSyntax d => d.Keyword.Span.Position,
-                    ResourceAccessSyntax r => r.ResourceName.Span.Position,
-                    _ => symbolReference.Span.Position,
+                    ITopLevelDeclarationSyntax d => d.Keyword,
+                    ResourceAccessSyntax r => r.ResourceName,
+                    _ => symbolReference,
                 };
 
                 var hover = await client.RequestHover(new HoverParams
                 {
                     TextDocument = new TextDocumentIdentifier(uri),
-                    Position = PositionHelper.GetPosition(lineStarts, syntaxPosition)
+                    Position = PositionHelper.GetPosition(lineStarts, nodeForHover.Span.Position)
                 });
 
-                if (symbolTable.TryGetValue(symbolReference, out var symbol) == false)
+                // fancy method to give us some annotated source code to look at if any assertions fail :)
+                using (CreateAssertionScopeWithContext(compilation.SyntaxTreeGrouping.EntryPoint, hover, nodeForHover.Span.ToZeroLengthSpan()))
                 {
-                    // symbol ref not bound to a symbol
-                    hover.Should().BeNull();
-                    continue;
-                }
-
-                switch (symbol!.Kind)
-                {
-                    case SymbolKind.Function when symbolReference is VariableAccessSyntax:
-                        // variable got bound to a function
+                    if (symbolTable.TryGetValue(symbolReference, out var symbol) == false)
+                    {
+                        // symbol ref not bound to a symbol
                         hover.Should().BeNull();
-                        break;
+                        continue;
+                    }
 
-                    // when a namespace value is found and there was an error with the function call or
-                    // is a valid function call or namespace access, all these cases will have a hover range
-                    // with some text
-                    case SymbolKind.Error when symbolReference is InstanceFunctionCallSyntax:
-                    case SymbolKind.Function when symbolReference is InstanceFunctionCallSyntax:
-                    case SymbolKind.Namespace:
-                        ValidateInstanceFunctionCallHover(hover);
-                        break;
+                    switch (symbol!.Kind)
+                    {
+                        case SymbolKind.Function when symbolReference is VariableAccessSyntax:
+                            // variable got bound to a function
+                            hover.Should().BeNull();
+                            break;
 
-                    case SymbolKind.Error:
-                        // error symbol
-                        hover.Should().BeNull();
-                        break;
+                        // when a namespace value is found and there was an error with the function call or
+                        // is a valid function call or namespace access, all these cases will have a hover range
+                        // with some text
+                        case SymbolKind.Error when symbolReference is InstanceFunctionCallSyntax:
+                        case SymbolKind.Function when symbolReference is InstanceFunctionCallSyntax:
+                        case SymbolKind.Namespace:
+                            ValidateInstanceFunctionCallHover(hover);
+                            break;
 
-                    default:
-                        ValidateHover(hover, symbol);
-                        break;
+                        case SymbolKind.Error:
+                            // error symbol
+                            hover.Should().BeNull();
+                            break;
+
+                        default:
+                            ValidateHover(hover, symbol);
+                            break;
+                    }
                 }
             }
         }
@@ -154,7 +174,11 @@ namespace Bicep.LangServer.IntegrationTests
                     Position = PositionHelper.GetPosition(lineStarts, node.Span.Position)
                 });
 
-                hover.Should().BeNull();
+                // fancy method to give us some annotated source code to look at if any assertions fail :)
+                using (CreateAssertionScopeWithContext(compilation.SyntaxTreeGrouping.EntryPoint, hover, node.Span.ToZeroLengthSpan()))
+                {
+                    hover.Should().BeNull();
+                }
             }
         }
 
