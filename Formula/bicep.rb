187d4d2047dc83c69695ba79761f552bcb00c319
Mickaël Mottet <MCKLMT@users.noreply.github.com>
2021-04-10 14:23:16 +0200

    Update Homebrew to 0.3.255 (#2202)

--- Formula/bicep.rb
+++ Formula/bicep.rb
@@ -1,12 +1,12 @@
 # to get the SHA256 checksum, replace the below command with the appropriate version:
-# curl -Lf https://github.com/Azure/bicep/releases/download/v0.3.1/bicep-osx-x64 | shasum -a 256
+# curl -Lf https://github.com/Azure/bicep/releases/download/v0.3.255/bicep-osx-x64 | shasum -a 256
 
 class Bicep < Formula
   desc "Bicep: next generation template language for Azure Resource Manager (ARM)"
   homepage "https://github.com/Azure/bicep"
-  version "0.3.1"
-  url "https://github.com/Azure/bicep/releases/download/v0.3.1/bicep-osx-x64"
-  sha256 "89351c783d05ea00aad6ffedfce83ad9b5036313d76f4d255fc48fb030bcc9a7"
+  version "0.3.255"
+  url "https://github.com/Azure/bicep/releases/download/v0.3.255/bicep-osx-x64"
+  sha256 "2875f4c20c9fd49fa56d3c95dd610f64210e76ef1468be31035ec4895a044bd1"
 
   license "MIT"
 
