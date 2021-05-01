846ae9ea223a5c77b2cba287c14ef116dd486064
Shenglong Li <shenglong.li@outlook.com>
2021-05-01 01:18:59 -0700

    Fix snapshot tests

--- src/vscode-bicep/src/test/snapshot/App.test.tsx
+++ src/vscode-bicep/src/test/snapshot/App.test.tsx
@@ -1,5 +1,13 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+(window as any).acquireVsCodeApi = () => ({
+  postMessage: jest.fn(),
+  setState: jest.fn(),
+  getState: jest.fn(),
+});
+
 import renderer from "react-test-renderer";
 
 import { App } from "../../visualizer/app/components/App";
