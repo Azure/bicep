0f2b7650cdea8f1ffa98314bc0a34f7669fc262c
Bhavya Subramanian <bhsubra@microsoft.com>
2021-05-26 13:06:23 -0700

    Added comment in BicepTelemetryHandler

--- src/Bicep.LangServer/Handlers/BicepTelemetryHandler.cs
+++ src/Bicep.LangServer/Handlers/BicepTelemetryHandler.cs
@@ -10,6 +10,12 @@ using OmniSharp.Extensions.LanguageServer.Protocol.Workspace;
 
 namespace Bicep.LanguageServer.Handlers
 {
+    // This handler is used to send a request from the client to the server(E.g. command sent as part
+    // of completion item of top level declaration snippet) which in turn triggers a request from server to client,
+    // asking the client to log a telemetry event
+    // Flow of events:
+    // 1. workspace/executeCommand request is sent from the client to the server
+    // 2. The above triggers telemetry/event from server to client
     public class BicepTelemetryHandler : ExecuteCommandHandlerBase<BicepTelemetryEvent>
     {
         private readonly ITelemetryProvider TelemetryProvider;
