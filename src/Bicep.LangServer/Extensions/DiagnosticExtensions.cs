22071e41303d4b74ce28499c8994cf67395b7412
jofleish <jofleish@microsoft.com>
2021-05-26 12:33:02 -0400

    fix up init fields for LSP update

--- src/Bicep.LangServer/Extensions/DiagnosticExtensions.cs
+++ src/Bicep.LangServer/Extensions/DiagnosticExtensions.cs
@@ -18,16 +18,21 @@ namespace Bicep.LanguageServer.Extensions
 
         private static OmniSharp.Extensions.LanguageServer.Protocol.Models.Diagnostic CreateDiagnostic(IDiagnostic diagnostic, ImmutableArray<int> lineStarts)
         {
-            var omniDiag = new OmniSharp.Extensions.LanguageServer.Protocol.Models.Diagnostic()
+            var (descCode, codeDescription) = GetDiagnosticDocumentation(diagnostic);
+            return new OmniSharp.Extensions.LanguageServer.Protocol.Models.Diagnostic()
             {
                 Severity = ToDiagnosticSeverity(diagnostic.Level),
-                Code = diagnostic.Code,
+                Code = descCode ?? diagnostic.Code,
                 Message = diagnostic.Message,
                 Source = diagnostic.Source,
                 Range = diagnostic.ToRange(lineStarts),
                 Tags = ToDiagnosticTags(diagnostic.Label),
+                CodeDescription = codeDescription
             };
+        }
 
+        private static (string?, CodeDescription?) GetDiagnosticDocumentation(IDiagnostic diagnostic)
+        {
             if (!string.IsNullOrEmpty(diagnostic.Uri))
             {
                 // This shuffling of the Code to Uri gives us the message formatting
@@ -40,24 +45,21 @@ namespace Bicep.LanguageServer.Extensions
                 // Desired format:
                 //   Declared parameter must be referenced within the document scope. bicep core(https://aka.ms/bicep/linter/no-unused-params) [2,7]
 
-                omniDiag.Code = diagnostic.Uri;
-                omniDiag.CodeDescription = new CodeDescription()
-                {
-                    Href = new Uri(diagnostic.Uri)
-                };
+                return new(diagnostic.Uri, new CodeDescription() { Href = new Uri(diagnostic.Uri) });
             }
 
-            return omniDiag;
+            // no additional documentation
+            return new(null, null);
         }
 
         private static DiagnosticSeverity ToDiagnosticSeverity(DiagnosticLevel level)
-            => level switch
-            {
-                DiagnosticLevel.Info => DiagnosticSeverity.Information,
-                DiagnosticLevel.Warning => DiagnosticSeverity.Warning,
-                DiagnosticLevel.Error => DiagnosticSeverity.Error,
-                _ => throw new ArgumentException($"Unrecognized level {level}"),
-            };
+                    => level switch
+                    {
+                        DiagnosticLevel.Info => DiagnosticSeverity.Information,
+                        DiagnosticLevel.Warning => DiagnosticSeverity.Warning,
+                        DiagnosticLevel.Error => DiagnosticSeverity.Error,
+                        _ => throw new ArgumentException($"Unrecognized level {level}"),
+                    };
 
         private static Container<DiagnosticTag>? ToDiagnosticTags(DiagnosticLabel? label) => label switch
         {
