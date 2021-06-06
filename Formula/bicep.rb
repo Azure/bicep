2ee87fdad4b972a3911d2d7b43633480932d2d1e
Lyon Till <158992+ljtill@users.noreply.github.com>
2021-06-06 16:00:56 +0100

    Update bicep.rb
    Increment Bicep version

--- Formula/bicep.rb
+++ Formula/bicep.rb
@@ -4,7 +4,7 @@
 class Bicep < Formula
   desc "Bicep: next generation template language for Azure Resource Manager (ARM)"
   homepage "https://github.com/Azure/bicep"
-  version "0.4.1"
+  version "0.4.64"
   url "https://github.com/Azure/bicep/releases/download/v0.4.63/bicep-osx-x64"
   sha256 "ae3f091275c8f59e2ca4624e24a131d1b71d0790d29f1af8fde3b430e0803987"
 
