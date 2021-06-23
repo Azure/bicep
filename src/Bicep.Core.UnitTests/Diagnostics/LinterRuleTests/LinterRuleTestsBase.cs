feeb3139609e032693a455b285e849818963ebb1
John Fleisher <john.fleisher@gmail.com>
2021-06-23 10:37:16 -0400

    Linter: No hard-coded hosts rule use TTK regex (#3288)
    * WiP: disallowed hosts regex
    
    * WiP: disallowed hosts regex work
    
    * No hardcoded Urls for environment() hosts - matches TTK
    
    * remove excluded hosts config - unneeded setting
    
    * WiP: enhance regex rules
    
    * Nested disallowed hosts regex working
    
    * finalize perf test. Remove custom regex escaping
    
    * confine diagnostic span to matched substring
    
    * Update test baselines
    
    Co-authored-by: jofleish <jofleish@microsoft.com>
    Co-authored-by: Bicep Automation <bicep@noreply.github.com>

--- src/Bicep.Core.UnitTests/Diagnostics/LinterRuleTests/LinterRuleTestsBase.cs
+++ src/Bicep.Core.UnitTests/Diagnostics/LinterRuleTests/LinterRuleTestsBase.cs
@@ -2,19 +2,29 @@
 // Licensed under the MIT License.
 
 using Bicep.Core.Analyzers.Linter;
+using Bicep.Core.Analyzers.Linter.Rules;
 using Bicep.Core.Diagnostics;
 using Bicep.Core.UnitTests.Utils;
 using FluentAssertions;
+using FluentAssertions.Execution;
 using System.Linq;
 
 namespace Bicep.Core.UnitTests.Diagnostics.LinterRuleTests
 {
     public class LinterRuleTestsBase
     {
+        virtual protected void CompileAndTest(string ruleCode, string text, int expectedDiagnosticCount)
+        {
+            using (new AssertionScope($"linter errors for this code:\n{text}\n"))
+            {
+                var errors = GetDiagnostics(ruleCode, text);
+                errors.Should().HaveCount(expectedDiagnosticCount);
+            }
+        }
+
         protected IDiagnostic[] GetDiagnostics(string ruleCode, string text)
         {
             var compilationResult = CompilationHelper.Compile(text);
-
             var internalRuleErrors = compilationResult.Diagnostics.Where(d => d.Code == LinterAnalyzer.FailedRuleCode).ToArray();
             internalRuleErrors.Count().Should().Be(0, "There should never be linter FailedRuleCode errors");
 
--- src/Bicep.Core/Analyzers/Linter/Rules/NoHardcodedEnvironmentUrlsRule.cs
+++ src/Bicep.Core/Analyzers/Linter/Rules/NoHardcodedEnvironmentUrlsRule.cs
@@ -5,11 +5,13 @@ using Bicep.Core.Diagnostics;
 using Bicep.Core.Parsing;
 using Bicep.Core.Semantics;
 using Bicep.Core.Syntax;
+using Bicep.Core.Text;
 using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace Bicep.Core.Analyzers.Linter.Rules
 {
@@ -17,32 +19,58 @@ namespace Bicep.Core.Analyzers.Linter.Rules
     {
         public new const string Code = "no-hardcoded-env-urls";
 
-        private ImmutableHashSet<string>? DisallowedHosts;
+        public ImmutableArray<string>? DisallowedHosts;
+        private ImmutableArray<string>? ExcludedHosts;
+
+        private readonly Lazy<Regex> hostRegexLazy;
+        private Regex hostRegex => hostRegexLazy.Value;
+
+        private readonly Lazy<Regex> excludedRegexLazy;
+        private Regex excludedRegex => excludedRegexLazy.Value;
 
         public NoHardcodedEnvironmentUrlsRule() : base(
             code: Code,
             description: CoreResources.EnvironmentUrlHardcodedRuleDescription,
             docUri: new Uri("https://aka.ms/bicep/linter/no-hardcoded-env-urls"))
         {
+            this.hostRegexLazy = new Lazy<Regex>(CreateDisallowedHostRegex);
+            this.excludedRegexLazy = new Lazy<Regex>(CreateExcludedHostsRegex);
         }
 
         public override void Configure(IConfigurationRoot config)
         {
             base.Configure(config);
             this.DisallowedHosts = GetArray(nameof(DisallowedHosts).ToLower(), Array.Empty<string>())
-                                    .ToImmutableHashSet(StringComparer.OrdinalIgnoreCase);
+                                    .ToImmutableArray();
+            this.ExcludedHosts = GetArray(nameof(ExcludedHosts).ToLower(), Array.Empty<string>())
+                                    .ToImmutableArray();
         }
 
         public override string FormatMessage(params object[] values)
             => string.Format("{0} Found this disallowed host: \"{1}\"", this.Description, values.First());
 
+        /// <summary>
+        /// Note that this regex requires the match to be at a subdomain boundary
+        /// so that there is not a partial match "mid-word". The subdomain must
+        /// be proceeded by a text break (., or slash, or space) or at the beginning
+        /// of a line.
+        /// </summary>
+        /// <returns></returns>
+        public Regex CreateDisallowedHostRegex() =>
+            new Regex(string.Join('|', this.DisallowedHosts!.Value.Select(h => $@"(?<=\.|\s|^|/){Regex.Escape(h)}")),
+                        RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+        public Regex CreateExcludedHostsRegex() =>
+            new Regex(string.Join('|', this.ExcludedHosts!.Value.Select(h => $@"(?<=\.|\s|^|/){Regex.Escape(h)}")),
+                        RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
         public override IEnumerable<IDiagnostic> AnalyzeInternal(SemanticModel model)
         {
-            if (this.DisallowedHosts != null && this.DisallowedHosts.Any())
+            if (this.DisallowedHosts.HasValue && this.DisallowedHosts.Value.Any())
             {
-                var visitor = new Visitor(this.DisallowedHosts);
+                var visitor = new Visitor(this.hostRegex, this.excludedRegex);
                 visitor.Visit(model.SyntaxTree.ProgramSyntax);
-                return visitor.DisallowedHostSpans.Select(entry => CreateDiagnosticForSpan(entry.span, entry.host));
+                return visitor.DisallowedHostSpans.Select(entry => CreateDiagnosticForSpan(entry.Key, entry.Value));
             }
 
             return Enumerable.Empty<IDiagnostic>();
@@ -50,42 +78,40 @@ namespace Bicep.Core.Analyzers.Linter.Rules
 
         private class Visitor : SyntaxVisitor
         {
-            public readonly List<(string host, TextSpan span)> DisallowedHostSpans = new List<(string host, TextSpan span)>();
+            public readonly Dictionary<TextSpan, string> DisallowedHostSpans = new Dictionary<TextSpan, string>();
+            private readonly Regex hostRegex;
+            private readonly Regex exclusionRegex;
 
-            private readonly ImmutableHashSet<string> disallowedHosts;
-
-            public Visitor(ImmutableHashSet<string> disallowedHosts)
+            public Visitor(Regex disallowedRegex, Regex exclusionRegex)
             {
-                this.disallowedHosts = disallowedHosts;
+                this.hostRegex = disallowedRegex;
+                this.exclusionRegex = exclusionRegex;
             }
 
             public override void VisitStringSyntax(StringSyntax syntax)
             {
-                var disallowedHost = syntax.SegmentValues.Select(s => FindEnvironmentUrlInString(s))
-                    .Where(span => span != null)
-                    .FirstOrDefault();
-                if (disallowedHost != null)
+                foreach (var segment in syntax.SegmentValues)
                 {
-                    this.DisallowedHostSpans.Add((disallowedHost, syntax.Span));
-                }
+                    var exclusionMatches = exclusionRegex.Matches(segment);
 
-                base.VisitStringSyntax(syntax);
-            }
-
-            private string? FindEnvironmentUrlInString(string str)
-            {
-                foreach (var host in this.disallowedHosts)
-                {
-                    var index = str.IndexOf(host, StringComparison.InvariantCultureIgnoreCase);
-                    if (index >= 0)
+                    // does this segment have a host match
+                    foreach (Match match in this.hostRegex.Matches(segment))
                     {
-                        return host;
-                    }
-                };
+                        // exclusion is found containing the host match
+                        var isExcluded = exclusionMatches.Any(exclusionMatch =>
+                           match.Index > exclusionMatch.Index
+                           && match.Index + match.Length <= exclusionMatch.Index + exclusionMatch.Length);
 
-                return null;
+                        if (!isExcluded)
+                        {
+                            // create a span for the specific identified instance
+                            // to allow for multiple instances in a single syntax
+                            this.DisallowedHostSpans[new TextSpan(syntax.Span.Position+match.Index, match.Length)] = match.Value;
+                        }
+                    }
+                    base.VisitStringSyntax(syntax);
+                }
             }
         }
-
     }
 }
