c80047e6bbd04d4df3908b6e51edfafc9747e8f5
Bhavya Subramanian <bhsubra@microsoft.com>
2021-07-20 16:28:59 -0700

    Rebase from main

--- src/vscode-bicep/src/extension.ts
+++ src/vscode-bicep/src/extension.ts
@@ -2,7 +2,6 @@
 // Licensed under the MIT License.
 import vscode from "vscode";
 import {
-  AzureUserInput,
   createAzExtOutputChannel,
   registerUIExtensionVariables,
 } from "vscode-azureextensionui";
