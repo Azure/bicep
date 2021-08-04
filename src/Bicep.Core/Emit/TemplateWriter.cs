85d4e2ee0ad0138cd2b407944b38a0c8eec3dc2c
Norbu Sonam <t-norbusonam@microsoft.com>
2021-08-04 16:59:32 -0400

    updated line numbering

--- src/Bicep.Core/Emit/TemplateWriter.cs
+++ src/Bicep.Core/Emit/TemplateWriter.cs
@@ -2,16 +2,18 @@
 // Licensed under the MIT License.
 
 using System;
-using System.IO;
 using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using Azure.Deployments.Core.Definitions.Schema;
 using Azure.Deployments.Core.Helpers;
 using Azure.Deployments.Expression.Expressions;
-using Azure.Deployments.Core.Definitions.Schema;
 using Bicep.Core.Extensions;
 using Bicep.Core.Parsing;
 using Bicep.Core.Semantics;
+using Bicep.Core.Semantics.Metadata;
 using Bicep.Core.Semantics.Namespaces;
 using Bicep.Core.SourceMapping;
 using Bicep.Core.Syntax;
@@ -20,8 +22,6 @@ using Bicep.Core.TypeSystem;
 using Bicep.Core.TypeSystem.Az;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-using Bicep.Core.Semantics.Metadata;
-using System.Diagnostics;
 using Microsoft.WindowsAzure.ResourceStack.Common.Json;
 
 namespace Bicep.Core.Emit
@@ -89,7 +89,6 @@ namespace Bicep.Core.Emit
         }
         private readonly EmitterContext context;
         private readonly EmitterSettings settings;
-
         public readonly SourceMap sourceMap;
 
         public TemplateWriter(SemanticModel semanticModel, EmitterSettings settings)
@@ -397,10 +396,10 @@ namespace Bicep.Core.Emit
 
         private void EmitResource(ArmJsonTextWriter jsonWriter, DeclaredResourceMetadata resource, ExpressionEmitter emitter)
         {
-            jsonWriter.WriteStartObject();
-
             // Save current line (start of resource) for source map
             int startLine = jsonWriter.CurrentLine;
+
+            jsonWriter.WriteStartObject();
 
             // Note: conditions STACK with nesting.
             //
