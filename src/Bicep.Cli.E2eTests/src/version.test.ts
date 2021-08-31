990f5462763334883f8ad4365af7650f0b07ffd8
Marcin Jastrzebski <majastrz@microsoft.com>
2021-08-30 19:15:14 -0700

    converted CLI E2E tests from JS to TS

--- src/Bicep.Cli.E2eTests/src/version.test.ts
+++ src/Bicep.Cli.E2eTests/src/version.test.ts
@@ -1,4 +1,6 @@
-const runBicepCommand = require("../command");
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+import { runBicepCommand } from "./command";
 
 describe("bicep --version", () => {
   it("should output version information", () => {
