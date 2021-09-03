1baca42e5984996de43c97fa6bffc26af9abceaf
Anthony Martin <antmarti@microsoft.com>
2021-09-02 22:37:16 -0400

    Upgrade to latest FluentAssertions (#4279)

--- src/Bicep.Core.UnitTests/Assertions/DiagnosticAssertions.cs
+++ src/Bicep.Core.UnitTests/Assertions/DiagnosticAssertions.cs
@@ -28,12 +28,12 @@ namespace Bicep.Core.UnitTests.Assertions
                 return value is Diagnostic;
             }
 
-            public string Format(object value, FormattingContext context, FormatChild formatChild)
+            public void Format(object value, FormattedObjectGraph formattedGraph, FormattingContext context, FormatChild formatChild)
             {
                 var prefix = context.UseLineBreaks ? Environment.NewLine : string.Empty;
                 var diagnostic = (Diagnostic)value;
 
-                return $"{prefix}\"[{diagnostic.Code} ({diagnostic.Level})] {diagnostic.Message}\"";
+                formattedGraph.AddFragment($"{prefix}\"[{diagnostic.Code} ({diagnostic.Level})] {diagnostic.Message}\"");
             }
         }
 
