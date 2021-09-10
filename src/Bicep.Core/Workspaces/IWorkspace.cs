21249adcabe0aac758c7a778e1c55d29f8e128dd
Bhavya Subramanian <bhsubra@microsoft.com>
2021-09-10 11:32:46 -0700

    Deleted some unwanted changes

--- src/Bicep.Core/Workspaces/IWorkspace.cs
+++ src/Bicep.Core/Workspaces/IWorkspace.cs
@@ -1,9 +1,7 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
-using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
-using Bicep.Core.Configuration;
 
 namespace Bicep.Core.Workspaces
 {
@@ -12,11 +10,5 @@ namespace Bicep.Core.Workspaces
         (ImmutableArray<ISourceFile> added, ImmutableArray<ISourceFile> removed) UpsertSourceFiles(IEnumerable<ISourceFile> sourceFiles);
 
         void RemoveSourceFiles(IEnumerable<ISourceFile> sourceFiles);
-
-        void UpsertBicepConfig(Uri uri, BicepConfig bicepConfig);
-
-        BicepConfig? GetBicepConfig(Uri uri);
-
-        void RemoveBicepConfig(Uri uri);
     }
 }
