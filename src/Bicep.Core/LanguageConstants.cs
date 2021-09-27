1c97e309b701fb7fdd7fc5395abb597923677a78
bhsubra <bhsubra@microsoft.com>
2021-09-27 11:08:45 -0700

    Add lightbulb option to disable linter rule (#4493)

--- src/Bicep.Core/LanguageConstants.cs
+++ src/Bicep.Core/LanguageConstants.cs
@@ -53,6 +53,8 @@ namespace Bicep.Core
 
         public const string BicepConfigSettingsFileName = "bicepconfig.json";
 
+        public const string DisableLinterRuleCommandName = "bicep.DisableLinterRule";
+
         public static readonly Regex ArmTemplateSchemaRegex = new(@"https?:\/\/schema\.management\.azure\.com\/schemas\/([^""\/]+\/[a-zA-Z]*[dD]eploymentTemplate\.json)#?");
 
         public static readonly ImmutableSortedSet<string> DeclarationKeywords = new[] { ParameterKeyword, VariableKeyword, ResourceKeyword, OutputKeyword, ModuleKeyword }.ToImmutableSortedSet(StringComparer.Ordinal);
--- src/Bicep.LangServer/Server.cs
+++ src/Bicep.LangServer/Server.cs
@@ -68,6 +68,7 @@ namespace Bicep.LanguageServer
                     .WithHandler<BicepCompletionHandler>()
                     .WithHandler<BicepCodeActionHandler>()
                     .WithHandler<BicepDidChangeWatchedFilesHandler>()
+                    .WithHandler<BicepDisableLinterRuleCommandHandler>()
                     .WithHandler<BicepSignatureHelpHandler>()
                     .WithHandler<BicepSemanticTokensHandler>()
                     .WithHandler<BicepTelemetryHandler>()
