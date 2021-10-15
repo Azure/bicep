8ba7e5c6c9148204ebba6e863c4160e1308ef80a
Bhavya Subramanian <bhsubra@microsoft.com>
2021-10-15 15:57:40 -0700

    Add linter rule to use stable vm image:

---
+++ docs/linter-rules/use-stable-vm-image.md
@@ -0,0 +1,44 @@
+# Use stable vm image
+
+**Code**: use-stable-vm-image
+
+**Description**: Virtual machines shouldn't use preview images. We will check below properties and fail the linter rule if they contain string "preview": 
+* offer
+* sku
+* version
+
+The following example fails this test.
+
+```bicep
+resource vm 'Microsoft.Compute/virtualMachines@2020-06-01' = {
+  name: 'virtualMachineName'
+  location: resourceGroup().location
+  properties: {
+    storageProfile: {
+      imageReference: {
+        offer: 'WindowsServer-preview'
+        sku: '2019-Datacenter-preview'
+        version: 'preview'
+      }
+    }
+  }
+}
+```
+
+The following example passes this test.
+
+```bicep
+resource vm 'Microsoft.Compute/virtualMachines@2020-06-01' = {
+  name: 'virtualMachineName'
+  location: resourceGroup().location
+  properties: {
+    storageProfile: {
+      imageReference: {
+        offer: 'WindowsServer'
+        sku: '2019-Datacenter'
+        version: 'latest'
+      }
+    }
+  }
+}
+```
---
+++ src/Bicep.Core/Analyzers/Linter/Rules/UseStableVMImageRule.cs
@@ -0,0 +1,123 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using Bicep.Core.Diagnostics;
+using Bicep.Core.Semantics;
+using Bicep.Core.Syntax;
+
+namespace Bicep.Core.Analyzers.Linter.Rules
+{
+    // Virtual machines shouldn't use preview images.
+    // Below class checks imageReference properties - offer, sku and version and fails the rule
+    // if they contain string "preview"
+    public sealed class UseStableVMImageRule : LinterRuleBase
+    {
+        public new const string Code = "use-stable-vm-image";
+
+        public UseStableVMImageRule() : base(
+            code: Code,
+            description: CoreResources.UseStableVMImage,
+            docUri: new Uri($"https://aka.ms/bicep/linter/{Code}"),
+            diagnosticLabel: Diagnostics.DiagnosticLabel.Unnecessary)
+        {
+        }
+        public override string FormatMessage(params object[] values)
+        {
+            return string.Format(CoreResources.UseStableVMImageFormat, values);
+        }
+
+        public override IEnumerable<IDiagnostic> AnalyzeInternal(SemanticModel model)
+        {
+            var visitor = new ResourceVisitor(this, model);
+            visitor.Visit(model.SourceFile.ProgramSyntax);
+            return visitor.diagnostics;
+        }
+
+        private class ResourceVisitor : SyntaxVisitor
+        {
+            public List<IDiagnostic> diagnostics = new List<IDiagnostic>();
+
+            private readonly SemanticModel model;
+            private readonly UseStableVMImageRule parent;
+
+            public ResourceVisitor(UseStableVMImageRule parent, SemanticModel model)
+            {
+                this.model = model;
+                this.parent = parent;
+            }
+
+            public override void VisitResourceDeclarationSyntax(ResourceDeclarationSyntax syntax)
+            {
+                var visitor = new PropertiesVisitor(this.parent, diagnostics, model);
+                visitor.Visit(syntax);
+
+                // Note: PropertiesVisitor will navigate through all child resources, so don't call base
+            }
+        }
+
+        private class PropertiesVisitor : SyntaxVisitor
+        {
+            private List<IDiagnostic> diagnostics;
+
+            private const string imageReferencePropertyName = "imageReference";
+
+            private readonly SemanticModel model;
+            private readonly UseStableVMImageRule parent;
+
+            public PropertiesVisitor(UseStableVMImageRule parent, List<IDiagnostic> diagnostics, SemanticModel model)
+            {
+                this.model = model;
+                this.parent = parent;
+                this.diagnostics = diagnostics;
+            }
+
+            public override void VisitObjectPropertySyntax(ObjectPropertySyntax syntax)
+            {
+                string? propertyName = syntax.TryGetKeyText();
+                // Check all properties with the name 'imageReference', regardless of casing
+                if (propertyName != null && StringComparer.OrdinalIgnoreCase.Equals(propertyName, imageReferencePropertyName))
+                {
+                    if (syntax.Value is ObjectSyntax objectSyntax)
+                    {
+                        AddDiagnosticsIfObjectSyntaxPropertiesContainPreview(objectSyntax);
+                    }
+                    else if (syntax.Value is VariableAccessSyntax &&
+                             model.GetSymbolInfo(syntax.Value) is VariableSymbol variableSymbol &&
+                             variableSymbol.Value is ObjectSyntax variableValueSyntax)
+                    {
+                        AddDiagnosticsIfObjectSyntaxPropertiesContainPreview(variableValueSyntax);
+                    }
+                }
+                else
+                {
+                    base.VisitObjectPropertySyntax(syntax);
+                }
+            }
+
+            private bool ValueContainsPreview(string key, string value)
+            {
+                return (key.Equals("offer", StringComparison.OrdinalIgnoreCase) ||
+                        key.Equals("sku", StringComparison.OrdinalIgnoreCase) ||
+                        key.Equals("version", StringComparison.OrdinalIgnoreCase)) &&
+                        value.Contains("preview");
+            }
+
+            private void AddDiagnosticsIfObjectSyntaxPropertiesContainPreview(ObjectSyntax objectSyntax)
+            {
+                foreach (var child in objectSyntax.Children)
+                {
+                    if (child is ObjectPropertySyntax objectPropertySyntax &&
+                        objectPropertySyntax.TryGetKeyText() is string key &&
+                        objectPropertySyntax.Value is StringSyntax valueSyntax &&
+                        valueSyntax.TryGetLiteralValue() is string value &&
+                        ValueContainsPreview(key, value))
+                    {
+                        diagnostics.Add(parent.CreateDiagnosticForSpan(valueSyntax.Span, key));
+                    }
+                }
+            }
+        }
+    }
+}
