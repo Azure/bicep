4fb088e0997855ce7439badd9166a5de4e499274
Shenglong Li <shenglong.li@outlook.com>
2021-10-21 08:30:51 -0700

    Update Bicep.Core to use dotnet 5 (#4941)

--- src/Bicep.Core/Resources/ResourceTypeReferenceComparer.cs
+++ src/Bicep.Core/Resources/ResourceTypeReferenceComparer.cs
@@ -11,8 +11,10 @@ namespace Bicep.Core.Resources
         public static IEqualityComparer<ResourceTypeReference> Instance { get; }
             = new ResourceTypeReferenceComparer();
 
-        public bool Equals(ResourceTypeReference x, ResourceTypeReference y)
-            => StringComparer.OrdinalIgnoreCase.Equals(x.Namespace, y.Namespace) &&
+        public bool Equals(ResourceTypeReference? x, ResourceTypeReference? y)
+            => x is not null &&
+            y is not null &&
+            StringComparer.OrdinalIgnoreCase.Equals(x.Namespace, y.Namespace) &&
             x.Types.Length == y.Types.Length &&
             Enumerable.SequenceEqual(x.Types, y.Types, StringComparer.OrdinalIgnoreCase) &&
             StringComparer.OrdinalIgnoreCase.Equals(x.ApiVersion, y.ApiVersion);
