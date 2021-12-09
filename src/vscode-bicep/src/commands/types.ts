ae0248a14339b82794331fd4d88a9aa773d1ad04
Stephen Weatherford (MSFT) <StephenWeatherford@users.noreply.github.com>
2021-12-09 12:36:44 -0800

    Add telemetry for all registered commands (#5218)
    * Add telemetry for all registered commands
    
    * valid commands
    
    * clean-up
    
    * clean-up
    
    * lint
    
    * fix commands returning value
    
    * comment
    
    * CR fixes
    
    * revert upgrade of vscode-azureextensionui due to telemetry bug
    
    * Force telemetry to version 0.3.1 to avoid bug
    
    * more specific versioning
    
    * Lock vscode-azureextensionui to 0.8.0
    
    Co-authored-by: Stephen Weatherford <stephen.weatherford@microsoft.com>

--- src/vscode-bicep/src/commands/types.ts
+++ src/vscode-bicep/src/commands/types.ts
@@ -1,7 +1,15 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
+
+import { IActionContext } from "vscode-azureextensionui";
+
 export interface Command {
   readonly id: string;
 
-  execute(...args: unknown[]): unknown;
+  /**
+   * Executes the command
+   * @param context Optionally used to control telemetry and error-handling behavior
+   * @param args Optional arguments that are being passed to the command
+   */
+  execute(context: IActionContext, ...args: unknown[]): unknown;
 }
