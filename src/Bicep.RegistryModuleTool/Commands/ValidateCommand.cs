98d2e663b90e62034399f9765112eee2401a99f8
Shenglong Li <shenglong.li@outlook.com>
2022-01-11 11:45:41 -0800

    Allow additional files

--- src/Bicep.RegistryModuleTool/Commands/ValidateCommand.cs
+++ src/Bicep.RegistryModuleTool/Commands/ValidateCommand.cs
@@ -1,7 +1,6 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-using Bicep.Core;
 using Bicep.RegistryModuleTool.Exceptions;
 using Bicep.RegistryModuleTool.Extensions;
 using Bicep.RegistryModuleTool.ModuleFiles;
@@ -9,13 +8,9 @@ using Bicep.RegistryModuleTool.ModuleFileValidators;
 using Bicep.RegistryModuleTool.Proxies;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
 using System.CommandLine;
 using System.CommandLine.Invocation;
-using System.IO;
 using System.IO.Abstractions;
-using System.Linq;
-using System.Text;
 
 namespace Bicep.RegistryModuleTool.Commands
 {
@@ -28,17 +23,6 @@ namespace Bicep.RegistryModuleTool.Commands
 
         public sealed class CommandHandler : BaseCommandHandler
         {
-            private readonly static IReadOnlyList<string> AllowedFileNames = new[]
-            {
-                MainBicepFile.FileName,
-                MainArmTemplateFile.FileName,
-                MainArmTemplateParametersFile.FileName,
-                MetadataFile.FileName,
-                ReadmeFile.FileName,
-                VersionFile.FileName,
-                "bicepconfig.json"
-            };
-
             private readonly IEnvironmentProxy environmentProxy;
 
             private readonly IProcessProxy processProxy;
@@ -53,9 +37,6 @@ namespace Bicep.RegistryModuleTool.Commands
             protected override int Invoke(InvocationContext context)
             {
                 var valid = true;
-
-                this.Logger.LogInformation("Validating that no additional files are in the module folder...");
-                valid &= Validate(context.Console, () => this.ValidateNoAdditionalFiles());
 
                 this.Logger.LogInformation("Validating main Bicep file...");
 
@@ -103,46 +84,6 @@ namespace Bicep.RegistryModuleTool.Commands
                 }
 
                 return true;
-            }
-
-            private void ValidateNoAdditionalFiles()
-            {
-                var currentDirectoryPath = this.FileSystem.Directory.GetCurrentDirectory();
-                var filePaths = this.FileSystem.Directory.EnumerateFiles(currentDirectoryPath, "*", SearchOption.AllDirectories);
-                var allowedFilePaths = AllowedFileNames
-                    .Select(fileName => this.FileSystem.Path.Combine(currentDirectoryPath, fileName))
-                    .ToHashSet();
-
-                var notAllowedFilePaths = new List<string>();
-
-                foreach (var filePath in filePaths)
-                {
-                    if (allowedFilePaths.Contains(filePath))
-                    {
-                        continue;
-                    }
-
-                    if (filePath.EndsWith(LanguageConstants.LanguageFileExtension, StringComparison.OrdinalIgnoreCase))
-                    {
-                        // Any Bicep file is allowed.
-                        continue;
-                    }
-
-                    notAllowedFilePaths.Add(filePath);
-                }
-
-                if (notAllowedFilePaths.Any())
-                {
-                    var errorMessageBuilder = new StringBuilder();
-                    errorMessageBuilder.AppendLine("The files below are not allowed in the module:");
-
-                    foreach (var filePath in notAllowedFilePaths)
-                    {
-                        errorMessageBuilder.Append("  - ").AppendLine(filePath);
-                    }
-
-                    throw new InvalidModuleFileException(errorMessageBuilder.ToString());
-                }
             }
         }
     }
