52c12e8957d288301f5f57fcfeb83f86e0f2c73e
Bhavya Subramanian <bhsubra@microsoft.com>
2022-01-20 15:42:29 -0800

    Working login

--- src/vscode-bicep/src/azure-account.api.d.ts
+++ src/vscode-bicep/src/azure-account.api.d.ts
@@ -3,45 +3,49 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { SubscriptionModels } from '@azure/arm-subscriptions';
-import { TokenCredential } from '@azure/core-auth';
-import { Environment } from '@azure/ms-rest-azure-env';
-import { TokenCredentialsBase } from '@azure/ms-rest-nodeauth';
+import { Event, Terminal, Progress, CancellationToken } from 'vscode';
+import { ServiceClientCredentials } from 'ms-rest';
 import { ReadStream } from 'fs';
-import { CancellationToken, Event, Progress, Terminal } from 'vscode';
+import { TokenCredentialsBase } from '@azure/ms-rest-nodeauth';
+import { Environment } from '@azure/ms-rest-azure-env';
+import { SubscriptionModels } from '@azure/arm-subscriptions';
 
 export type AzureLoginStatus = 'Initializing' | 'LoggingIn' | 'LoggedIn' | 'LoggedOut';
 
-export interface AzureAccountExtensionApi {
-  readonly apiVersion: string;
-  readonly status: AzureLoginStatus;
-  readonly filters: AzureResourceFilter[];
-  readonly sessions: AzureSession[];
-  readonly subscriptions: AzureSubscription[];
-  readonly onStatusChanged: Event<AzureLoginStatus>;
-  readonly onFiltersChanged: Event<void>;
-  readonly onSessionsChanged: Event<void>;
-  readonly onSubscriptionsChanged: Event<void>;
-  readonly waitForFilters: () => Promise<boolean>;
-  readonly waitForLogin: () => Promise<boolean>;
-  readonly waitForSubscriptions: () => Promise<boolean>;
-  createCloudShell(os: 'Linux' | 'Windows'): CloudShell;
+export interface AzureAccount {
+	readonly status: AzureLoginStatus;
+	readonly onStatusChanged: Event<AzureLoginStatus>;
+	readonly waitForLogin: () => Promise<boolean>;
+	readonly sessions: AzureSession[];
+	readonly onSessionsChanged: Event<void>;
+	readonly subscriptions: AzureSubscription[];
+	readonly onSubscriptionsChanged: Event<void>;
+	readonly waitForSubscriptions: () => Promise<boolean>;
+	readonly filters: AzureResourceFilter[];
+	readonly onFiltersChanged: Event<void>;
+	readonly waitForFilters: () => Promise<boolean>;
+	createCloudShell(os: 'Linux' | 'Windows'): CloudShell;
 }
 
 export interface AzureSession {
-  readonly environment: Environment;
-  readonly userId: string;
-  readonly tenantId: string;
+	readonly environment: Environment;
+	readonly userId: string;
+	readonly tenantId: string;
 
-  /**
-   * The credentials object for azure-sdk-for-js modules https://github.com/azure/azure-sdk-for-js
-   */
-  readonly credentials2: TokenCredentialsBase | TokenCredential;
+	/**
+	 * The credentials object for azure-sdk-for-node modules https://github.com/azure/azure-sdk-for-node
+	 */
+	readonly credentials: ServiceClientCredentials;
+
+	/**
+	 * The credentials object for azure-sdk-for-js modules https://github.com/azure/azure-sdk-for-js
+	 */
+	readonly credentials2: TokenCredentialsBase;
 }
 
 export interface AzureSubscription {
-  readonly session: AzureSession;
-  readonly subscription: SubscriptionModels.Subscription;
+	readonly session: AzureSession;
+	readonly subscription: SubscriptionModels.Subscription;
 }
 
 export type AzureResourceFilter = AzureSubscription;
@@ -49,16 +53,16 @@ export type AzureResourceFilter = AzureSubscription;
 export type CloudShellStatus = 'Connecting' | 'Connected' | 'Disconnected';
 
 export interface UploadOptions {
-  contentLength?: number;
-  progress?: Progress<{ message?: string; increment?: number }>;
-  token?: CancellationToken;
+	contentLength?: number;
+	progress?: Progress<{ message?: string; increment?: number }>;
+	token?: CancellationToken;
 }
 
 export interface CloudShell {
-  readonly status: CloudShellStatus;
-  readonly onStatusChanged: Event<CloudShellStatus>;
-  readonly waitForConnection: () => Promise<boolean>;
-  readonly terminal: Promise<Terminal>;
-  readonly session: Promise<AzureSession>;
-  readonly uploadFile: (filename: string, stream: ReadStream, options?: UploadOptions) => Promise<void>;
+	readonly status: CloudShellStatus;
+	readonly onStatusChanged: Event<CloudShellStatus>;
+	readonly waitForConnection: () => Promise<boolean>;
+	readonly terminal: Promise<Terminal>;
+	readonly session: Promise<AzureSession>;
+	readonly uploadFile: (filename: string, stream: ReadStream, options?: UploadOptions) => Promise<void>;
 }
--- src/vscode-bicep/src/commands/deploy.ts
+++ src/vscode-bicep/src/commands/deploy.ts
@@ -4,17 +4,8 @@ import vscode from "vscode";
 import { Command } from "./types";
 import { LanguageClient } from "vscode-languageclient/node";
 import { IActionContext, parseError } from "vscode-azureextensionui";
-import { AzureAccountExtensionApi, AzureSession } from '../azure-account.api';
-import { commands, ExtensionContext, window } from 'vscode';
-import { ResourceManagementClient } from '@azure/arm-resources';
-import { SubscriptionClient, SubscriptionModels } from '@azure/arm-subscriptions';
-
-interface SubscriptionItem {
-  label: string;
-  description: string;
-  session: AzureSession;
-  subscription: SubscriptionModels.Subscription;
-}
+import { AzureAccount } from '../azure-account.api';
+import { commands, ExtensionContext } from 'vscode';
 
 export class DeployCommand implements Command {
   public readonly id = "bicep.deploy";
@@ -46,73 +37,14 @@ export class DeployCommand implements Command {
     }
 
     try {
-      const azureAccount = vscode.extensions.getExtension<AzureAccountExtensionApi>('ms-vscode.azure-account')!.exports;
+      const azureAccount = vscode.extensions.getExtension<AzureAccount>('ms-vscode.azure-account')!.exports;
 
       if (!(await azureAccount.waitForLogin())) {
-
+        return commands.executeCommand('azure-account.askForLogin');
       }
 
     } catch (err) {
       this.client.error("Deploy failed", parseError(err).message, true);
     }
   }
-}
-
-function showSubscriptions(api: AzureAccountExtensionApi) {
-  return async () => {
-    if (!(await api.waitForLogin())) {
-      return commands.executeCommand('azure-account.askForLogin');
-    }
-    const subscriptionItems = loadSubscriptionItems(api);
-    const result = await window.showQuickPick(subscriptionItems);
-    if (result) {
-      const resourceGroupItems = loadResourceGroupItems(result);
-      await window.showQuickPick(resourceGroupItems);
-    }
-  };
-}
-
-async function loadSubscriptionItems(api: AzureAccountExtensionApi) {
-  await api.waitForFilters();
-  const subscriptionItems: SubscriptionItem[] = [];
-  for (const session of api.sessions) {
-    const credentials = session.credentials2;
-    const subscriptionClient = new SubscriptionClient(credentials);
-    const subscriptions = await listAll(subscriptionClient.subscriptions, subscriptionClient.subscriptions.list());
-    subscriptionItems.push(...subscriptions.map(subscription => ({
-      label: subscription.displayName || '',
-      description: subscription.subscriptionId || '',
-      session,
-      subscription
-    })));
-  }
-  subscriptionItems.sort((a, b) => a.label.localeCompare(b.label));
-  return subscriptionItems;
-}
-
-async function loadResourceGroupItems(subscriptionItem: SubscriptionItem) {
-  const { session, subscription } = subscriptionItem;
-  const resources = new ResourceManagementClient(session.credentials2, subscription.subscriptionId!);
-  const resourceGroups = await listAll(resources.resourceGroups, resources.resourceGroups.list());
-  resourceGroups.sort((a, b) => (a.name || '').localeCompare(b.name || ''));
-  return resourceGroups.map(resourceGroup => ({
-    label: resourceGroup.name || '',
-    description: resourceGroup.location,
-    resourceGroup
-  }));
-}
-
-export function deactivate() {
-}
-
-export interface PartialList<T> extends Array<T> {
-  nextLink?: string;
-}
-
-async function listAll<T>(client: { listNext(nextPageLink: string): Promise<PartialList<T>>; }, first: Promise<PartialList<T>>): Promise<T[]> {
-  const all: T[] = [];
-  for (let list = await first; list.length || list.nextLink; list = list.nextLink ? await client.listNext(list.nextLink) : []) {
-    all.push(...list);
-  }
-  return all;
 }
