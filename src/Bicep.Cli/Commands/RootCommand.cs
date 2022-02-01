aa4b05437fcd2007c2967850c95988633f08caf2
Mats Estensen <mats.estensen@devoteam.no>
2022-02-01 17:21:17 +0100

    fix: remove unused cli option --no-summary
    Fixes #5828

--- src/Bicep.Cli/Commands/RootCommand.cs
+++ src/Bicep.Cli/Commands/RootCommand.cs
@@ -74,14 +74,12 @@ Usage:
     Options:
       --outdir <dir>    Saves the output at the specified directory.
       --outfile <file>  Saves the output as the specified file path.
-      --no-summary      Omits the summary at the end of the build.
       --stdout          Prints the output to stdout.
       --no-restore      Builds the bicep file without restoring external modules.
 
     Examples:
       bicep build file.bicep
       bicep build file.bicep --stdout
-      bicep build file.bicep --no-summary
       bicep build file.bicep --outdir dir1
       bicep build file.bicep --outfile file.json
       bicep build file.bicep --no-restore
