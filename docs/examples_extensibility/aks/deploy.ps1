c77d1987ea34f8e80b3df1801815dabbabbd2d77
Marcin Jastrzebski <majastrz@microsoft.com>
2022-02-09 20:20:02 -0800

    k8s script

--- docs/examples_extensibility/aks/deploy.ps1
+++ docs/examples_extensibility/aks/deploy.ps1
@@ -8,13 +8,26 @@ $sshRSAPublicKey = Get-Content "~\.ssh\id_rsa.pub";
 $env:BICEP_IMPORTS_ENABLED_EXPERIMENTAL = 'true'
 
 Select-AzSubscription 996a2f3f-ee01-4ffd-9765-d2c3fc98f30a
+
+# end-to-end deployment
 New-AzSubscriptionDeployment `
- -Name $baseName `
- -Location 'West Central US' `
- -TemplateFile '.\main.bicep' `
- -TemplateParameterObject @{
-   baseName = $baseName;
-   dnsPrefix = $baseName;
-   linuxAdminUsername = $adminUsername;
-   sshRSAPublicKey = $sshRSAPublicKey;
- }
+  -Name $baseName `
+  -Location 'West Central US' `
+  -TemplateFile '.\main.bicep' `
+  -TemplateParameterObject @{
+    baseName = $baseName;
+    dnsPrefix = $baseName;
+    linuxAdminUsername = $adminUsername;
+    sshRSAPublicKey = $sshRSAPublicKey;
+  }
+
+# deploy kubernetes resources individually
+$kubeConfig = ''
+
+New-AzResourceGroupDeployment `
+  -Name "$baseName-voteapp" `
+  -TemplateFile '.\modules/kubernetes.bicep' `
+  -ResourceGroupName $baseName `
+  -TemplateParameterObject @{
+    kubeConfig = $kubeConfig
+  }
