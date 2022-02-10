82f4a54f87094bf2b50c07a08a3da6c81f4de2f5
Marcin Jastrzebski <majastrz@microsoft.com>
2022-02-09 19:55:52 -0800

    script updates

---
+++ docs/examples_extensibility/aks/deploy.ps1
@@ -0,0 +1,20 @@
+# In order to use this functionality, you must first have been enrolled in the extensibility preview.
+# You will also need to ensure you've set the BICEP_IMPORTS_ENABLED_EXPERIMENTAL env var to 'true' in both VSCode and your CLI environment.
+
+$baseName="majastrzkub"
+$adminUsername="marcin"
+$sshRSAPublicKey = Get-Content "~\.ssh\id_rsa.pub";
+
+$env:BICEP_IMPORTS_ENABLED_EXPERIMENTAL = 'true'
+
+Select-AzSubscription 996a2f3f-ee01-4ffd-9765-d2c3fc98f30a
+New-AzSubscriptionDeployment `
+ -Name $baseName `
+ -Location 'West Central US' `
+ -TemplateFile '.\main.bicep' `
+ -TemplateParameterObject @{
+   baseName = $baseName;
+   dnsPrefix = $baseName;
+   linuxAdminUsername = $adminUsername;
+   sshRSAPublicKey = $sshRSAPublicKey;
+ }
