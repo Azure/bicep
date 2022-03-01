6ca3a83b728381fb6b62414d5f868bb7dc0e9e78
Stephen Weatherford (MSFT) <StephenWeatherford@users.noreply.github.com>
2022-03-01 10:58:40 -0800

    Clean up default configuration (#6086)
    Co-authored-by: Stephen Weatherford <stephen.weatherford@microsoft.com>

--- src/Bicep.Core.UnitTests/Configuration/ConfigurationManagerTests.cs
+++ src/Bicep.Core.UnitTests/Configuration/ConfigurationManagerTests.cs
@@ -72,22 +72,22 @@ namespace Bicep.Core.UnitTests.Configuration
         ""no-hardcoded-env-urls"": {
           ""level"": ""warning"",
           ""disallowedhosts"": [
-            ""gallery.azure.com"",
-            ""management.core.windows.net"",
-            ""management.azure.com"",
-            ""database.windows.net"",
-            ""core.windows.net"",
-            ""login.microsoftonline.com"",
-            ""graph.windows.net"",
-            ""trafficmanager.net"",
-            ""datalake.azure.net"",
-            ""azuredatalakestore.net"",
-            ""azuredatalakeanalytics.net"",
-            ""vault.azure.net"",
             ""api.loganalytics.io"",
             ""asazure.windows.net"",
+            ""azuredatalakeanalytics.net"",
+            ""azuredatalakestore.net"",
+            ""batch.core.windows.net"",
+            ""core.windows.net"",
+            ""database.windows.net"",
+            ""datalake.azure.net"",
+            ""gallery.azure.com"",
+            ""graph.windows.net"",
+            ""login.microsoftonline.com"",
+            ""management.azure.com"",
+            ""management.core.windows.net"",
             ""region.asazure.windows.net"",
-            ""batch.core.windows.net""
+            ""trafficmanager.net"",
+            ""vault.azure.net""
           ],
           ""excludedhosts"": [
             ""schema.management.azure.com""
