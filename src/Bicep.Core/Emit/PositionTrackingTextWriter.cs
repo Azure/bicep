cf26d3afc0f9a2c0a4fd61d0440f29f842e282f1
Nick Brown <nichb@microsoft.com>
2022-03-11 15:31:59 -0800

    New implementation for JsonTextWriter wrapper class to track position

---
+++ src/Bicep.Core/Emit/PositionTrackingTextWriter.cs
@@ -0,0 +1,62 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System.IO;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Bicep.Core.Emit
+{
+    public class PositionTrackingTextWriter : TextWriter
+    {
+        public int CurrentPos;
+        public int CurrentLine;
+
+        private readonly TextWriter _internalWriter;
+        private bool _endOfLine;
+
+        public PositionTrackingTextWriter(TextWriter textWriter)
+        {
+            _internalWriter = textWriter;
+        }
+
+        public override Encoding Encoding => _internalWriter.Encoding;
+
+        public override void Write(char value)
+        {
+            if (_endOfLine)
+            {
+                CurrentPos = 0;
+                CurrentLine++;
+            }
+
+            _internalWriter.Write(value);
+
+            CurrentPos++;
+
+            if (value == '\n') // check for carriage return char?
+            {
+                _endOfLine = true;
+            }
+        }
+    }
+
+    public class PositionTrackingJsonTextWriter : JsonTextWriter
+    {
+        public int CurrentPos => _trackingWriter.CurrentPos;
+        public int CurrentLine => _trackingWriter.CurrentLine;
+        
+        private PositionTrackingTextWriter _trackingWriter;
+
+        public static PositionTrackingJsonTextWriter Create(TextWriter textWriter)
+        {
+            var trackingWriter = new PositionTrackingTextWriter(textWriter);
+            return new PositionTrackingJsonTextWriter(trackingWriter);
+        }
+
+        private PositionTrackingJsonTextWriter(PositionTrackingTextWriter textWriter) : base(textWriter)
+        {
+            _trackingWriter = textWriter;
+        }
+    }
+}
