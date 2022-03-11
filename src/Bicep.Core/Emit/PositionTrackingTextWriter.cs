db1a9314fe854d703fe43c3f19f1bb065e4938d4
Nick Brown <nichb@microsoft.com>
2022-03-11 15:31:59 -0800

    New implementation for JsonTextWriter wrapper class to track position

--- src/Bicep.Core/Emit/PositionTrackingTextWriter.cs
+++ src/Bicep.Core/Emit/PositionTrackingTextWriter.cs
@@ -1,57 +1,62 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-using System.Collections.Generic;
-using System.Collections.Immutable;
 using System.IO;
 using System.Text;
 using Newtonsoft.Json;
 
 namespace Bicep.Core.Emit
 {
-    public class PositionTrackingJsonTextWriter : JsonTextWriter
+    public class PositionTrackingTextWriter : TextWriter
     {
+        public int CurrentPos;
+        public int CurrentLine;
 
+        private readonly TextWriter _internalWriter;
+        private bool _endOfLine;
 
-        private class PositionTrackingTextWriter : TextWriter
+        public PositionTrackingTextWriter(TextWriter textWriter)
         {
-            private readonly TextWriter internalWriter;
-
-            public int CurrentPos;
-
-            public List<int> CommaPositions = new();
-
-            public PositionTrackingTextWriter(TextWriter textWriter)
-            {
-                this.internalWriter = textWriter;
-            }
-
-            public override Encoding Encoding => this.internalWriter.Encoding;
-
-            public override void Write(char value)
-            {
-                if (value == ',')
-                {
-                    CommaPositions.Add(CurrentPos);
-                }
-
-                this.internalWriter.Write(value);
-
-                CurrentPos++;
-            }
+            _internalWriter = textWriter;
         }
 
-        private readonly PositionTrackingTextWriter _trackingWriter;
+        public override Encoding Encoding => _internalWriter.Encoding;
 
+        public override void Write(char value)
+        {
+            if (_endOfLine)
+            {
+                CurrentPos = 0;
+                CurrentLine++;
+            }
+
+            _internalWriter.Write(value);
+
+            CurrentPos++;
+
+            if (value == '\n') // check for carriage return char?
+            {
+                _endOfLine = true;
+            }
+        }
+    }
+
+    public class PositionTrackingJsonTextWriter : JsonTextWriter
+    {
         public int CurrentPos => _trackingWriter.CurrentPos;
-        public List<int> CommaPositions => _trackingWriter.CommaPositions;
+        public int CurrentLine => _trackingWriter.CurrentLine;
         
-        private readonly PositionTrackingTextWriter _trackingWriter;
+        private PositionTrackingTextWriter _trackingWriter;
 
         public static PositionTrackingJsonTextWriter Create(TextWriter textWriter)
         {
             var trackingWriter = new PositionTrackingTextWriter(textWriter);
             return new PositionTrackingJsonTextWriter(trackingWriter);
+        }
+
+        private PositionTrackingJsonTextWriter(PositionTrackingTextWriter textWriter) : base(textWriter)
+        {
+            _trackingWriter = textWriter;
         }
     }
 }
