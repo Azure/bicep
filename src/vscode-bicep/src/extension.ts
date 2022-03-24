543aef51439524aca3fb52a6bd6f5c74be476972
Bhavya Subramanian <bhsubra@microsoft.com>
2022-03-24 11:18:48 -0700

    Intial changes

--- src/vscode-bicep/src/extension.ts
+++ src/vscode-bicep/src/extension.ts
@@ -1,12 +1,12 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 import vscode from "vscode";
-
+import { BicepVisualizerViewManager } from "./visualizer";
+import { createAzExtOutputChannel } from "./utils/AzExtOutputChannel";
+import { OutputChannelManager } from "./utils/OutputChannelManager";
 import { registerAzureUtilsExtensionVariables } from "@microsoft/vscode-azext-azureutils";
-import {
-  createAzExtOutputChannel,
-  registerUIExtensionVariables,
-} from "@microsoft/vscode-azext-utils";
+import { registerUIExtensionVariables } from "@microsoft/vscode-azext-utils";
+import { TreeManager } from "./tree/TreeManager";
 
 import {
   BuildCommand,
@@ -21,15 +21,12 @@ import {
   BicepCacheContentProvider,
   launchLanguageServiceWithProgressReport,
 } from "./language";
-import { TreeManager } from "./tree/TreeManager";
 import {
   activateWithTelemetryAndErrorHandling,
   createLogger,
   Disposable,
   resetLogger,
 } from "./utils";
-import { OutputChannelManager } from "./utils/OutputChannelManager";
-import { BicepVisualizerViewManager } from "./visualizer";
 
 class BicepExtension extends Disposable {
   private constructor(public readonly extensionUri: vscode.Uri) {
