4fb63857ba2279a25360b5df1e045ef89159e3c0
Engin Polat <polatengin@hotmail.com>
2022-04-20 21:47:24 +0000

    adding parameters file writer

---
+++ src/Bicep.Cli/Services/ParametersFileWriter.cs
@@ -0,0 +1,62 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using Bicep.Core.Emit;
+using Bicep.Core.Exceptions;
+using Bicep.Core.Semantics;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+
+namespace Bicep.Cli.Services
+{
+    public class ParametersFileWriter
+    {
+        private readonly InvocationContext invocationContext;
+
+        public ParametersFileWriter(InvocationContext invocationContext)
+        {
+            this.invocationContext = invocationContext;
+        }
+
+        public EmitResult ToFile(Compilation compilation, string outputPath)
+        {
+            var fileStream = CreateFileStream(outputPath);
+            using (fileStream)
+            {
+                var semanticModel = compilation.GetEntrypointSemanticModel();
+
+                return new TemplateEmitter(semanticModel, invocationContext.EmitterSettings).EmitParametersFile(fileStream);
+            }
+        }
+
+        public EmitResult ToStream(Compilation compilation, Stream stream)
+        {
+            return new TemplateEmitter(compilation.GetEntrypointSemanticModel(), invocationContext.EmitterSettings).EmitParametersFile(stream);
+        }
+
+        public EmitResult ToStdout(Compilation compilation)
+        {
+            using var writer = new JsonTextWriter(invocationContext.OutputWriter)
+            {
+                Formatting = Formatting.Indented
+            };
+
+            var emitter = new TemplateEmitter(compilation.GetEntrypointSemanticModel(), invocationContext.EmitterSettings);
+
+            return emitter.EmitParametersFile(writer);
+        }
+
+        private static FileStream CreateFileStream(string path)
+        {
+            try
+            {
+                return new FileStream(path, FileMode.Create, FileAccess.ReadWrite, FileShare.None);
+            }
+            catch (Exception exception)
+            {
+                throw new BicepException(exception.Message, exception);
+            }
+        }
+    }
+}
