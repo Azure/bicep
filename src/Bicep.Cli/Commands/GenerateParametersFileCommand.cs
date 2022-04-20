7051d05d6470b1a045b6619542eabb98f21a2235
Engin Polat <polatengin@hotmail.com>
2022-04-20 21:40:49 +0000

    creating generate params file command handler

---
+++ src/Bicep.Cli/Commands/GenerateParametersFileCommand.cs
@@ -0,0 +1,69 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using Bicep.Cli.Arguments;
+using Bicep.Cli.Logging;
+using Bicep.Cli.Services;
+using Bicep.Core.FileSystem;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace Bicep.Cli.Commands
+{
+    public class GenerateParametersFileCommand : ICommand
+    {
+        private readonly ILogger logger;
+        private readonly IDiagnosticLogger diagnosticLogger;
+        private readonly InvocationContext invocationContext;
+        private readonly CompilationService compilationService;
+        private readonly ParametersFileWriter writer;
+
+        public GenerateParametersFileCommand(
+            ILogger logger,
+            IDiagnosticLogger diagnosticLogger,
+            InvocationContext invocationContext,
+            CompilationService compilationService,
+            ParametersFileWriter writer)
+        {
+            this.logger = logger;
+            this.diagnosticLogger = diagnosticLogger;
+            this.invocationContext = invocationContext;
+            this.compilationService = compilationService;
+            this.writer = writer;
+        }
+
+        public async Task<int> RunAsync(GenerateParametersFileArguments args)
+        {
+            var inputPath = PathHelper.ResolvePath(args.InputFile);
+
+            if (invocationContext.EmitterSettings.EnableSymbolicNames)
+            {
+                logger.LogWarning(CliResources.SymbolicNamesDisclaimerMessage);
+            }
+
+            if (invocationContext.Features.ResourceTypedParamsAndOutputsEnabled)
+            {
+                logger.LogWarning(CliResources.ResourceTypesDisclaimerMessage);
+            }
+
+            var compilation = await compilationService.CompileAsync(inputPath, args.NoRestore);
+
+            if (diagnosticLogger.ErrorCount < 1)
+            {
+                if (args.OutputToStdOut)
+                {
+                    writer.ToStdout(compilation);
+                }
+                else
+                {
+                    var outputPath = PathHelper.ResolveParametersFileOutputPath(inputPath);
+
+                    writer.ToFile(compilation, outputPath);
+                }
+            }
+
+            // return non-zero exit code on errors
+            return diagnosticLogger.ErrorCount > 0 ? 1 : 0;
+        }
+    }
+}
