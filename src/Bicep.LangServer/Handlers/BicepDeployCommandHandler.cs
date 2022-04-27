4fe6f36b328e2aa28c15a52eb81b3bcdd43b5f94
Bhavya Subramanian <bhsubra@microsoft.com>
2022-04-27 10:12:20 -0700

    Initial changes

--- src/Bicep.LangServer/Handlers/BicepDeployCommandHandler.cs
+++ src/Bicep.LangServer/Handlers/BicepDeployCommandHandler.cs
@@ -1,13 +1,20 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Azure.ResourceManager;
+using Bicep.Core.Semantics;
+using Bicep.LanguageServer.CompilationManager;
 using Bicep.LanguageServer.Deploy;
 using Bicep.LanguageServer.Telemetry;
 using MediatR;
+using Newtonsoft.Json;
 using OmniSharp.Extensions.JsonRpc;
+using OmniSharp.Extensions.LanguageServer.Protocol;
 using OmniSharp.Extensions.LanguageServer.Protocol.Workspace;
 
 namespace Bicep.LanguageServer.Handlers
@@ -16,12 +23,14 @@ namespace Bicep.LanguageServer.Handlers
 
     public class BicepDeployCommandHandler : ExecuteTypedResponseCommandHandlerBase<BicepDeployParams, string>
     {
+        private readonly ICompilationManager compilationManager;
         private readonly IDeploymentCollectionProvider deploymentCollectionProvider;
         private readonly ITelemetryProvider telemetryProvider;
 
-        public BicepDeployCommandHandler(IDeploymentCollectionProvider deploymentCollectionProvider, ISerializer serializer, ITelemetryProvider telemetryProvider)
+        public BicepDeployCommandHandler(ICompilationManager compilationManager, IDeploymentCollectionProvider deploymentCollectionProvider, ISerializer serializer, ITelemetryProvider telemetryProvider)
             : base(LangServerConstants.DeployCommand, serializer)
         {
+            this.compilationManager = compilationManager;
             this.deploymentCollectionProvider = deploymentCollectionProvider;
             this.telemetryProvider = telemetryProvider;
         }
@@ -32,6 +41,8 @@ namespace Bicep.LanguageServer.Handlers
 
             var credential = new CredentialFromTokenAndTimeStamp(request.token, request.expiresOnTimestamp);
             var armClient = new ArmClient(credential);
+
+            var missingParameters = FindMissingParameters(request.documentPath, request.parameterFilePath);
 
             (bool isSuccess, string deploymentOutput) = await DeploymentHelper.CreateDeployment(
                 deploymentCollectionProvider,
@@ -46,6 +57,35 @@ namespace Bicep.LanguageServer.Handlers
             PostDeployResultTelemetryEvent(request.deployId, isSuccess);
 
             return deploymentOutput;
+        }
+
+        private IEnumerable<ParameterSymbol> FindMissingParameters(string documentPath, string parameterFilePath)
+        {
+            var documentUri = DocumentUri.FromFileSystemPath(documentPath);
+            var compilationContext = compilationManager.GetCompilation(documentUri);
+
+            if (compilationContext is null)
+            {
+                return Enumerable.Empty<ParameterSymbol>();
+            }
+
+            var semanticModel = compilationContext.Compilation.GetEntrypointSemanticModel();
+            var parameterDeclarations = semanticModel.Root.ParameterDeclarations;
+
+            if (string.IsNullOrEmpty(parameterFilePath))
+            {
+                return parameterDeclarations;
+            }
+
+            var parametersFileContents = File.ReadAllText(parameterFilePath);
+            Dictionary<string, dynamic>? dict = JsonConvert.DeserializeObject<Dictionary<string, dynamic>>(parametersFileContents);
+
+            if (dict is null)
+            {
+                return parameterDeclarations;
+            }
+
+            return parameterDeclarations.Where(x => !dict.ContainsKey(x.Name));
         }
 
         private void PostDeployStartTelemetryEvent(string deployId)
