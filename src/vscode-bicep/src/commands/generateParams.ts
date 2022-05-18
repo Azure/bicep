52b391dfc9e14e16dfd15d1d86cdd1c5d1ebee69
Engin Polat <polatengin@hotmail.com>
2022-05-18 18:48:54 +0000

    calling generateParams from language server when the command invoked

---
+++ src/vscode-bicep/src/commands/generateParams.ts
@@ -0,0 +1,45 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+import vscode from "vscode";
+import { Command } from "./types";
+import { LanguageClient } from "vscode-languageclient/node";
+import { IActionContext, parseError } from "@microsoft/vscode-azext-utils";
+import { OutputChannelManager } from "../utils/OutputChannelManager";
+import { findOrCreateActiveBicepFile } from "./findOrCreateActiveBicepFile";
+
+export class GenerateParamsCommand implements Command {
+  public readonly id = "bicep.generateParams";
+  public constructor(
+    private readonly client: LanguageClient,
+    private readonly outputChannelManager: OutputChannelManager
+  ) {}
+
+  public async execute(
+    context: IActionContext,
+    documentUri?: vscode.Uri | undefined
+  ): Promise<void> {
+    documentUri = await findOrCreateActiveBicepFile(
+      context,
+      documentUri,
+      "Choose which Bicep file to generate parameters file for"
+    );
+
+    try {
+      console.log(`Generating parameters file for ${documentUri.fsPath}...`);
+      const generateParamsOutput: string = await this.client.sendRequest(
+        "workspace/executeCommand",
+        {
+          command: "generateParams",
+          arguments: [documentUri.fsPath],
+        }
+      );
+      this.outputChannelManager.appendToOutputChannel(generateParamsOutput);
+    } catch (err) {
+      this.client.error(
+        "Generate params failed",
+        parseError(err).message,
+        true
+      );
+    }
+  }
+}
