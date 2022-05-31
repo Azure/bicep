40990586fb5d8ff44552ce269b0854f2a6b04d75
Shenglong Li <shenglong.li@outlook.com>
2022-05-31 16:53:29 -0700

    Update dependencies

---
+++ src/monarch/babel.config.js
@@ -0,0 +1,9 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+module.exports = {
+  env: {
+    test: {
+      plugins: ["@babel/plugin-transform-modules-commonjs"]
+    }
+  }
+};
