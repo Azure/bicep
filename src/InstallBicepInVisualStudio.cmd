b5a26fae11c4ec9b4a53b00709f1a7783b5f6add
Bhavya Subramanian <bhsubra@microsoft.com>
2022-07-06 10:26:08 -0700

    Added InstallBicepInVisualStudio.cmd

---
+++ src/InstallBicepInVisualStudio.cmd
@@ -0,0 +1,21 @@
+@echo off
+
+SET VsWhereExePath=%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe
+set "ExtensionsRoot=%~dp0"
+
+for /f "usebackq delims=" %%i in (`"%VsWhereExePath%" -latest -prerelease -products * -property enginePath`) do (
+  set VSIXInstallerExePath=%%i
+)
+
+echo VSIXIntallerPath %VSIXInstallerExePath%
+
+if exist "%VSIXInstallerExePath%\VSIXInstaller.exe" (
+  SET BicepVsixPath=%~dp0Bicep.VSLanguageServerClient.Vsix\bin\**\Bicep.VSLanguageServerClient.Vsix.vsix
+
+  if exist %BicepVsixPath% (
+    echo Installing: %BicepVsixPath%
+      call "%VSIXInstallerExePath%\VSIXInstaller.exe" /quiet %BicepVsixPath%
+    ) else (
+      echo Failed to install vsix: %BicepVsixPath%
+    )
+)
