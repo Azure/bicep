3ec700bdd27ae301b1de0be5ac75986554a96fe8
Muhammad Usman <t-musman@microsoft.com>
2022-07-08 14:03:52 -0500

    Added params semantic model class draft

---
+++ src/Bicep.Core/Semantics/ParamSemanticModel.cs
@@ -0,0 +1,27 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System.Collections.Generic;
+using Bicep.Core.Diagnostics;
+using Bicep.Core.Syntax;
+using Bicep.Core.Workspaces;
+
+namespace Bicep.Core.Semantics
+{
+    public class ParamSemanticModel
+    {
+        private readonly BicepParamFile bicepParamFile;
+
+        // private readonly ParamBinder paramBinder; 
+        public ParamSemanticModel(BicepParamFile bicepParamFile)
+        {
+            this.bicepParamFile = bicepParamFile;
+
+            //TODO: Verify if SymbolContext is needed 
+            // this.paramBinder = new(bicepParamFile, ); 
+        }
+
+        public IEnumerable<IDiagnostic> GetDiagnostics()
+        => bicepParamFile.ProgramSyntax.GetParseDiagnostics();
+    }
+}
