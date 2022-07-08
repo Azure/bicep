7528f2306a49e4a8650e48eba385bbc3779cec63
cqthyy <ccao35@wisc.edu>
2022-07-08 11:40:36 -0700

    parameter semantic model class set up for binder integration tests

---
+++ src/Bicep.Core/Semantics/ParamsSemanticModel.cs
@@ -0,0 +1,24 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+
+using Bicep.Core.Workspaces;
+
+namespace Bicep.Core.Semantics
+{
+    public class ParamsSemanticModel
+    {
+        public ParamsSemanticModel(BicepParamFile sourceFile, ISymbolContext symbolContext)
+        {
+            ParamBinder = new ParamBinder(sourceFile, symbolContext);
+        }
+            
+        public ParamBinder ParamBinder { get; }
+        
+        /// <summary>
+        /// Gets the file that was compiled.
+        /// </summary>
+        public ParamFileSymbol Root => this.ParamBinder.ParamFileSymbol;
+    }
+
+}
