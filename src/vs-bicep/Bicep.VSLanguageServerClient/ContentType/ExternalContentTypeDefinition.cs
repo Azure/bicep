e2549d1aa49ae514f9bdcb56469f08acf4798c11
Bhavya Subramanian <bhsubra@microsoft.com>
2022-07-20 11:23:23 -0700

    Added comment for CodeRemoteContentTypeName

--- src/vs-bicep/Bicep.VSLanguageServerClient/ContentType/ExternalContentTypeDefinition.cs
+++ src/vs-bicep/Bicep.VSLanguageServerClient/ContentType/ExternalContentTypeDefinition.cs
@@ -5,6 +5,9 @@ namespace Bicep.VSLanguageServerClient.ContentType
 {
     public sealed class ExternalContentTypeDefinition
     {
+        /// <summary>
+        /// Code remote content type name. Lights up all TextMate features (colorization, brace completion, folding ranges).
+        /// </summary>
         public const string CodeRemoteContentTypeName = "code-languageserver-preview";
     }
 }
