a500b0953f91cb4284cd4e5a2016ee29260e4949
Marcin Jastrzebski <majastrz@microsoft.com>
2022-07-28 23:32:11 -0700

    fixed exception due to hard-coded index expression

--- src/Bicep.Core.IntegrationTests/ScenarioTests.cs
+++ src/Bicep.Core.IntegrationTests/ScenarioTests.cs
@@ -3872,5 +3872,68 @@ var value = (useFirst ? test1 : test2).tata
 
             result.Should().NotHaveAnyDiagnostics();
        }
+
+        /// <summary>
+        /// https://github.com/Azure/bicep/issues/6863
+        /// </summary>
+        [TestMethod]
+        public void Test_Issue6863()
+        {
+            var result = CompilationHelper.Compile(@"
+@description('Region to deploy to')
+param Location string = resourceGroup().location
+
+var Names = [
+  'fruit-primary'
+  'fruit-secondary'
+]
+
+var Service_Bus_Queues = [
+  'apples'
+  'oranges'
+]
+
+resource serviceBuses 'Microsoft.ServiceBus/namespaces@2021-11-01' = [for name in Names: {
+  name: name
+  location: Location
+  sku: {
+    name: 'Premium'
+    tier: 'Premium'
+  }
+  properties: {
+    zoneRedundant: false
+  }
+}]
+
+resource queues 'Microsoft.ServiceBus/namespaces/queues@2021-11-01' = [for item in Service_Bus_Queues: {
+  parent: serviceBuses[0]
+  name: item
+}]
+
+resource queueAuthorizationRules 'Microsoft.ServiceBus/namespaces/queues/authorizationRules@2021-11-01' = [for (item, index) in Service_Bus_Queues: {
+  parent: queues[index]
+  name: 'Listen'
+  properties: {
+    rights: [
+      'Listen'
+    ]
+  }
+}]
+");
+
+            result.Should().NotHaveAnyDiagnostics();
+
+            result.Template.Should().HaveValueAtPath("$.resources[0].copy.name", "serviceBuses");
+            result.Template.Should().HaveValueAtPath("$.resources[0].name", "[variables('Names')[copyIndex()]]");
+            result.Template.Should().NotHaveValueAtPath("$.resources[0].dependsOn");
+
+            result.Template.Should().HaveValueAtPath("$.resources[1].copy.name", "queues");
+            result.Template.Should().HaveValueAtPath("$.resources[1].name", "[format('{0}/{1}', variables('Names')[0], variables('Service_Bus_Queues')[copyIndex()])]");
+            result.Template.Should().HaveValueAtPath("$.resources[1].dependsOn", new JArray("[resourceId('Microsoft.ServiceBus/namespaces', variables('Names')[0])]"));
+
+            result.Template.Should().HaveValueAtPath("$.resources[2].copy.name", "queueAuthorizationRules");
+            result.Template.Should().HaveValueAtPath("$.resources[2].name", "[format('{0}/{1}/{2}', variables('Names')[0], variables('Service_Bus_Queues')[copyIndex()], 'Listen')]");
+            result.Template.Should().HaveValueAtPath("$.resources[2].dependsOn", new JArray("[resourceId('Microsoft.ServiceBus/namespaces/queues', variables('Names')[0], variables('Service_Bus_Queues')[copyIndex()])]"));
+        }
     }
 }
