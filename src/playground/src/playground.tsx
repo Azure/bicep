78f87a2ef855b036ff1fdd8d4eeb96cd4117b36a
Anthony Martin <antmarti@microsoft.com>
2022-08-04 16:23:46 -0400

    Add appInsights, ability to load quickstart samples to playground (#7832)
    * Add application insights to playground
    
    * Add back support for quickstarts

--- src/playground/src/playground.tsx
+++ src/playground/src/playground.tsx
@@ -1,20 +1,28 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 import React, { useEffect, useRef, useState } from 'react';
-import { Button, Container, Nav, Navbar, OverlayTrigger, Row, Spinner, Tooltip } from 'react-bootstrap';
+import { Button, ButtonGroup, Col, Container, Dropdown, FormControl, Nav, Navbar, OverlayTrigger, Row, Spinner, Tooltip } from 'react-bootstrap';
 
 import './playground.css';
 import { JsonEditor } from './jsonEditor';
 import { BicepEditor } from './bicepEditor';
 import { copyShareLinkToClipboard, handleShareLink } from './utils';
+import { quickstartsPaths, getQuickstartsLink } from './examples';
 import { decompile } from './lspInterop';
+import { IAppInsights } from '@microsoft/applicationinsights-common';
 
-export const Playground : React.FC = () => {
+interface Props {
+  insights: IAppInsights,
+}
+
+export const Playground : React.FC<Props> = (props) => {
+  const { insights } = props;
   const [jsonContent, setJsonContent] = useState('');
   const [bicepContent, setBicepContent] = useState('');
   const [initialContent, setInitialContent] = useState('');
   const [copied, setCopied] = useState(false);
   const [loading, setLoading] = useState(false);
+  const [filterText, setFilterText] = useState('');
   const uploadInputRef = useRef<HTMLInputElement>();
 
   async function withLoader(action: () => Promise<void>) {
@@ -26,6 +34,20 @@ export const Playground : React.FC = () => {
     }
   }
 
+  async function loadExample(filePath: string) {
+    withLoader(async () => {
+      const response = await fetch(getQuickstartsLink(filePath));
+
+      if (!response.ok) {
+        throw response.text();
+      }
+
+      insights.trackEvent({ name: 'loadExample' }, { path: filePath });
+      const bicepText = await response.text();
+      setInitialContent(bicepText);
+    });
+  }
+
   useEffect(() => {
     window.addEventListener('hashchange', () => handleShareLink(content => {
       if (content !== null) {
@@ -35,14 +57,16 @@ export const Playground : React.FC = () => {
 
     handleShareLink(content => {
       if (content !== null) {
+        insights.trackEvent({ name: 'openSharedLink' });
         setInitialContent(content);
       } else {
-        setInitialContent('')
+        setInitialContent('');
       }
     });
   }, []);
 
   const handlCopyClick = () => {
+    insights.trackEvent({ name: 'copySharedLink' });
     setCopied(true);
     setTimeout(() => setCopied(false), 2000);
     copyShareLinkToClipboard(bicepContent);
@@ -53,6 +77,7 @@ export const Playground : React.FC = () => {
     reader.onload = async (e) => {
       withLoader(async () => {
         try {
+          insights.trackEvent({ name: 'decompileJson' });
           const jsonContents = e.target.result.toString();
           const bicepContents = decompile(jsonContents);
           setInitialContent(bicepContents);
@@ -64,6 +89,14 @@ export const Playground : React.FC = () => {
 
     reader.readAsText(file);
   }
+
+  const filteredExamples = quickstartsPaths
+    .filter(x => x.toLowerCase().indexOf(filterText.toLowerCase()) !== -1)
+    .sort((a, b) => a > b ? 1 : -1);
+
+  const dropdownItems = filteredExamples.map(path => (
+    <Dropdown.Item key={path} eventKey={path} active={false}>{path}</Dropdown.Item>
+  ));
 
   const createTooltip = (text: string) => (
     <Tooltip id="button-tooltip">
@@ -82,6 +115,21 @@ export const Playground : React.FC = () => {
         <OverlayTrigger placement="bottom" overlay={createTooltip('Upload an ARM template JSON file to decompile to Bicep')}>
           <Button size="sm" variant="primary" className="mx-1" onClick={() => uploadInputRef.current.click()}>Decompile</Button>
         </OverlayTrigger>
+        <Dropdown as={ButtonGroup} onSelect={loadExample} onToggle={() => setFilterText('')}>
+          <OverlayTrigger placement="bottom" overlay={createTooltip('Select an Azure Quickstarts sample file')}>
+            <Dropdown.Toggle as={Button} size="sm" variant="primary" className="mx-1">Sample Template</Dropdown.Toggle>
+          </OverlayTrigger>
+          <Dropdown.Menu align="end">
+          <Col>
+            <FormControl
+              autoFocus
+              placeholder="Type to filter..."
+              onChange={(e) => setFilterText(e.target.value)}
+              value={filterText} />
+          </Col>
+            {dropdownItems}
+          </Dropdown.Menu>
+        </Dropdown>
       </Nav>
     </Navbar>
     <div className="playground-container">
