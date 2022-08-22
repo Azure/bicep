c73806fe540e2c57b58421d009460dd4b888a5dd
Bhavya Subramanian <bhsubra@microsoft.com>
2022-08-22 11:33:41 -0700

    Added integration test

---
+++ src/vs-bicep/Bicep.VSLanguageServerClient.IntegrationTests/Utilities/LanguageServerActivationUtility.cs
@@ -0,0 +1,18 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace Bicep.VSLanguageServerClient.IntegrationTests.Utilities
+{
+    public static class LanguageServerActivationUtility
+    {
+        public static void WaitForContentTypeReady()
+        {
+            //var serviceForComponentModel = ServiceProvider.GlobalProvider.GetService(typeof(SComponentModel).GUID);
+            //if (serviceForComponentModel is IComponentModel componentModel)
+            //{
+            //    var languageClientBrokers = componentModel.DefaultExportProvider.GetExports<ILanguageServiceBroker2>();
+
+            //}
+        }
+    }
+}
