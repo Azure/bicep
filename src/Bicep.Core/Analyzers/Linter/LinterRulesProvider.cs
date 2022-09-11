eba45c51cc204c6bd8d6def68dbcca994c3d3f0f
Stephen Weatherford (MSFT) <StephenWeatherford@users.noreply.github.com>
2022-09-10 20:05:14 -0700

    Fix 8318 by removing reflection code (#8327)
    Co-authored-by: Stephen Weatherford <Stephen.Weatherford.com>

--- src/Bicep.Core/Analyzers/Linter/LinterRulesProvider.cs
+++ src/Bicep.Core/Analyzers/Linter/LinterRulesProvider.cs
@@ -4,9 +4,11 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
 using Bicep.Core.Analyzers.Interfaces;
+using Bicep.Core.Analyzers.Linter.Rules;
 
 namespace Bicep.Core.Analyzers.Linter
 {
@@ -39,12 +41,36 @@ namespace Bicep.Core.Analyzers.Linter
 
         public IEnumerable<Type> GetRuleTypes()
         {
-            return Assembly.GetExecutingAssembly()
-                .GetTypes()
-                .Where(t => typeof(IBicepAnalyzerRule).IsAssignableFrom(t)
-                            && t.IsClass
-                            && t.IsPublic
-                            && t.GetConstructor(Type.EmptyTypes) != null);
+            // Can't use reflection to get this list because the output dotnet executable is trimmed,
+            //   and dependencies that can't be determined at compile time get removed.
+            return new[] {
+                typeof(AdminUsernameShouldNotBeLiteralRule),
+                typeof(ArtifactsParametersRule),
+                typeof(ExplicitValuesForLocationParamsRule),
+                typeof(MaxNumberOutputsRule),
+                typeof(MaxNumberParametersRule),
+                typeof(MaxNumberResourcesRule),
+                typeof(MaxNumberVariablesRule),
+                typeof(NoHardcodedEnvironmentUrlsRule),
+                typeof(NoHardcodedLocationRule),
+                typeof(NoLocationExprOutsideParamsRule),
+                typeof(NoUnnecessaryDependsOnRule),
+                typeof(NoUnusedExistingResourcesRule),
+                typeof(NoUnusedParametersRule),
+                typeof(NoUnusedVariablesRule),
+                typeof(OutputsShouldNotContainSecretsRule),
+                typeof(PreferInterpolationRule),
+                typeof(PreferUnquotedPropertyNamesRule),
+                typeof(SecretsInParamsMustBeSecureRule),
+                typeof(SecureParameterDefaultRule),
+                typeof(SecureParamsInNestedDeploymentsRule),
+                typeof(SimplifyInterpolationRule),
+                typeof(ProtectCommandToExecuteSecretsRule),
+                typeof(UseRecentApiVersionRule),
+                typeof(UseResourceIdFunctionsRule),
+                typeof(UseStableResourceIdentifiersRule),
+                typeof(UseStableVMImageRule),
+            };
         }
 
         public ImmutableDictionary<string, string> GetLinterRules() => linterRulesLazy.Value;
