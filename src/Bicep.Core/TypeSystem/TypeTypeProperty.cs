49c48e6341f6c2e058746f3bda120b372c00556c
Jonathan Eskew <jeskew@microsoft.com>
2022-11-06 17:11:12 -0500

    Add documentation on the purpose of TypeType and TypeTypeProperty

--- src/Bicep.Core/TypeSystem/TypeTypeProperty.cs
+++ src/Bicep.Core/TypeSystem/TypeTypeProperty.cs
@@ -1,15 +1,17 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-namespace Bicep.Core.TypeSystem
-{
-    public class TypeTypeProperty : TypeProperty
-    {
-        public TypeTypeProperty(string name, TypeType type, TypePropertyFlags flags = TypePropertyFlags.None, string? description = null) : base(name, type, flags, description)
-        {
-            TypeReference = type;
-        }
+namespace Bicep.Core.TypeSystem;
 
-        public new TypeType TypeReference { get; }
+/// <summary>
+/// A TypeProperty whose type must be a <see cref="TypeType" />. Intended for use on the types of objects that themselves have no runtime value (such as namespaces).
+/// </summary>
+public class TypeTypeProperty : TypeProperty
+{
+    public TypeTypeProperty(string name, TypeType type, TypePropertyFlags flags = TypePropertyFlags.None, string? description = null) : base(name, type, flags, description)
+    {
+        TypeReference = type;
     }
+
+    public new TypeType TypeReference { get; }
 }
