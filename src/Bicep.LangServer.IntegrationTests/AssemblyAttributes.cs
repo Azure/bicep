1a2b013752f176dfd758e3dadee1b1fef679841c
Shenglong Li <shenglong.li@outlook.com>
2022-11-22 16:57:53 -0800

    Add a comment

--- src/Bicep.LangServer.IntegrationTests/AssemblyAttributes.cs
+++ src/Bicep.LangServer.IntegrationTests/AssemblyAttributes.cs
@@ -3,4 +3,8 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 [assembly: Parallelize(Scope = ExecutionScope.MethodLevel, Workers = 0)]
+
+// From https://developercommunity.visualstudio.com/t/mstest-passing-readonly-struct-as-dynamicdata-fail/1620466:
+// Since 2.2.6, test case discovery for date source tests are moved to actual discovery phase from execution phase, this means that each test case data needs to be serializable.
+// TextSpan is a struct which is not serializable. To opt-out of this behavior, the attribute below is needed.
 [assembly:TestDataSourceDiscovery(TestDataSourceDiscoveryOption.DuringExecution)]
