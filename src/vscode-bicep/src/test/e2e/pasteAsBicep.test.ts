628ed92697cdceff035e18d712b08550fbe9915b
Stephen Weatherford <stephwe@microsoft.com>
2022-12-05 16:47:37 -0800

    work

--- src/vscode-bicep/src/test/e2e/pasteAsBicep.test.ts
+++ src/vscode-bicep/src/test/e2e/pasteAsBicep.test.ts
@@ -3,8 +3,38 @@
 
 import fse from "fs-extra";
 import { createUniqueTempFolder } from "../utils/createUniqueTempFolder";
-
+import fs from "fs";
+import path from "path";
+import vscode, { ConfigurationTarget } from "vscode";
 import { executeCloseAllEditors } from "./commands";
+import { getBicepConfiguration } from "../../language/getBicepConfiguration";
+
+const json = `
+{
+	"$schema": "https://schema.management.azure.com/schemas/2019-04-01/deploymentTemplate.json#",
+	"contentVersion": "1.0.0.0",
+	"metadata": {
+		"prefix": "arm-nested-template-inner",
+		"description": "Nested (inline) Inner-Scoped Deployment. Defines its own local parameters."
+	},
+	"resources": [
+		{
+            "name": "nestedDeployment1",
+            "type": "Microsoft.Resources/deployments",
+            "apiVersion": "2021-04-01",
+            "properties": {
+                "mode": "Incremental",
+                "template": {
+                    "$schema": "https://schema.management.azure.com/schemas/2019-04-01/deploymentTemplate.json#",
+                    "contentVersion": "1.0.0.0",
+                    "variables": {},
+                    "resources": [],
+                    "outputs": {}
+                }
+            }
+        }
+	]
+}`;
 
 describe("pasteAsBicep", (): void => {
   afterEach(async () => {
@@ -14,6 +44,26 @@ describe("pasteAsBicep", (): void => {
   it("todo", async () => {
     const folder = createUniqueTempFolder("pasteAsBicep");
     expect(true).toBeTruthy();
+
+    const inputPath = path.join(folder, "main.bicep");
+    fs.writeFileSync(inputPath, json);
+
+    const textDocument = await vscode.workspace.openTextDocument(inputPath);
+    await vscode.window.showTextDocument(textDocument);
+
+    // Make sure Decompile on Paste is on
+    await getBicepConfiguration().update(
+      "decompileOnPaste",
+      true,
+      ConfigurationTarget.Global
+    );
+
+    vscode.env.clipboard.writeText(json);
+    await vscode.commands.executeCommand("editor.action.clipboardPasteAction");
+
+    const buffer = textDocument.getText();
+
+    expect(buffer).toBe("asdfg");
 
     // Delete the folder
     fse.rmdirSync(folder, {
