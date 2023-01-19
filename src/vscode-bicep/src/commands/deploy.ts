488aaf6a4ac8d7ab894324fa508dcb38e173245e
Daniel Ciborowski <dciborow@microsoft.com>
2023-01-19 18:34:53 -0500

    dciborow/fix-deploy-params

--- src/vscode-bicep/src/commands/deploy.ts
+++ src/vscode-bicep/src/commands/deploy.ts
@@ -584,7 +584,7 @@ export class DeployCommand implements Command {
           placeHolder: `Please enter value for parameter "${paramName}"`,
         });
       } else {
-        if (deploymentParameter.isExpression) {
+        if (deploymentParameter.isExpression || (deploymentParameter.parameterType.isString && contains(deploymentParameter.value, "${") {
           paramValue = await this.selectValueForParameterOfTypeExpression(
             _context,
             paramName,
