c7d71571cf7600661ce0aae7013f8d3ba0f16e9b
Daniel Ciborowski <dciborow@microsoft.com>
2023-01-19 20:33:42 -0500

    Update src/Bicep.LangServer/Handlers/BicepDeploymentParametersHandler.cs

--- src/Bicep.LangServer/Handlers/BicepDeploymentParametersHandler.cs
+++ src/Bicep.LangServer/Handlers/BicepDeploymentParametersHandler.cs
@@ -148,7 +148,7 @@ namespace Bicep.LanguageServer.Handlers
             return modifier is ParameterDefaultValueSyntax parameterDefaultValueSyntax &&
                 parameterDefaultValueSyntax.DefaultValue is ExpressionSyntax expressionSyntax &&
                 expressionSyntax is not null &&
-                expressionSyntax is not StringSyntax &&  // This line is what is really causing the issue. Strings that contain ${} should be treated as expressions.
+                (expressionSyntax is not StringSyntax || specificationString.IsInterpolated()) &&
                 expressionSyntax is not IntegerLiteralSyntax &&
                 expressionSyntax is not BooleanLiteralSyntax;
         }
