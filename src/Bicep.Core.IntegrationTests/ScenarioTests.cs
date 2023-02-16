1a87fe095d37909a92200d0bffe756f12d214ded
Anthony Martin <38542602+anthony-c-martin@users.noreply.github.com>
2023-02-16 00:58:48 -0500

    Fix issue 9467

--- src/Bicep.Core.IntegrationTests/ScenarioTests.cs
+++ src/Bicep.Core.IntegrationTests/ScenarioTests.cs
@@ -4278,5 +4278,63 @@ output firstCertEnabled bool = Certificate[0].properties.attributes.enabled
                 result.Should().HaveTemplateWithOutput("firstCertEnabled", $"[{referenceExpression}.attributes.enabled]");
             }
         }
+
+        // https://github.com/Azure/bicep/issues/9467
+        [TestMethod]
+        public void Test_Issue9467()
+        {
+            var (parameters, _, _) = CompilationHelper.CompileParams(@"
+param CertificateSubjects = [{
+  subject: 'blah'
+  secretName: 'blah'
+  thumbprint: 'blah'
+  keyVault: {
+    name: 'myKv'
+    subscriptionId: 'mySub'
+    resourceGroupName: 'myRg'
+  }
+}]
+");
+
+            var result = CompilationHelper.Compile(Services.WithFeatureOverrides(new(UserDefinedTypesEnabled: true)), @"
+@description('Used to identify a Key Vault and where it\'s deployed to')
+type keyVaultIdentifier = {
+  @description('The name of the Key Vault')
+  name: string
+  @description('The ID of the subscription the Key Vault is associated with')
+  subscriptionId: string
+  @description('The name of the resource group the Key Vault is associated with')
+  resourceGroupName: string
+}
+
+@description('Used to identify the details of a specific certificate')
+type certificateMapping = {
+  @description('The subject value of the certificate')
+  subject: string
+  @description('The name of the secret in the Key Vault')
+  secretName: string
+  @description('The thumbprint of the certificate')
+  thumbprint: string
+  @description('The identifier of the Key Vault instance')
+  keyVault: keyVaultIdentifier
+}
+
+@description('The various subjects for which certificates should be secured for downstream resources')
+param CertificateSubjects certificateMapping[]
+
+//Specifically pulls the certificates from the CertificateSubject vault as specified
+resource CertificateVault 'Microsoft.KeyVault/vaults@2022-07-01' existing = {
+  name: first(map(CertificateSubjects, c => c.keyVault.name))
+  scope: resourceGroup(first(map(CertificateSubjects, c => c.keyVault.subscriptionId)), first(map(CertificateSubjects, c => c.keyVault.resourceGroupName)))
+}
+
+output vaultId string = CertificateVault.id
+");
+
+            result.Should().GenerateATemplate();
+
+            var evaluated = TemplateEvaluator.Evaluate(result.Template, parameters);            
+            evaluated.Should().HaveValueAtPath("$.outputs['vaultId'].value", "/subscriptions/mySub/resourceGroups/myRg/providers/Microsoft.KeyVault/vaults/myKv");
+        }
     }
 }
--- src/Bicep.Core/DataFlow/DataFlowAnalyzer.cs
+++ src/Bicep.Core/DataFlow/DataFlowAnalyzer.cs
@@ -7,50 +7,59 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace Bicep.Core.DataFlow
+namespace Bicep.Core.DataFlow;
+
+public class DataFlowAnalyzer
 {
-    public class DataFlowAnalyzer
+    private readonly SemanticModel semanticModel;
+
+    public DataFlowAnalyzer(SemanticModel semanticModel)
     {
-        private readonly SemanticModel semanticModel;
+        this.semanticModel = semanticModel;
+    }
 
-        public DataFlowAnalyzer(SemanticModel semanticModel)
+    public IList<LocalVariableSymbol> GetInaccessibleLocalsAfterSyntaxMove(SyntaxBase syntax, SyntaxBase newParent)
+    {
+        var symbolDependencies = LocalSymbolDependencyVisitor.GetLocalSymbolDependencies(this.semanticModel, syntax);
+
+        return symbolDependencies
+            .Where(symbol => !IsLocalAccessibleAfterMove(symbol, oldParent: syntax, newParent: newParent))
+            .ToList();
+    }
+
+    private bool IsLocalAccessibleAfterMove(LocalVariableSymbol symbol, SyntaxBase oldParent, SyntaxBase newParent)
+    {
+        // find out the scope where the local symbol starts being accessible
+        // (the declaration is outside of that node)
+        var scope = GetScope(symbol);
+
+        if (semanticModel.SourceFile.Hierarchy.IsEqualOrDescendent(node: newParent, potentialAncestor: scope.BindingSyntax))
         {
-            this.semanticModel = semanticModel;
+            // if the new parent is a child node of or equal to the binding syntax, the same scoping rules should apply.
+            return true;
         }
 
-        public IList<LocalVariableSymbol> GetInaccessibleLocalsAfterSyntaxMove(SyntaxBase syntax, SyntaxBase newParent)
+        if (semanticModel.SourceFile.Hierarchy.IsEqualOrDescendent(node: scope.DeclaringSyntax, potentialAncestor: oldParent))
         {
-            var symbolDependencies = LocalSymbolDependencyVisitor.GetLocalSymbolDependencies(this.semanticModel, syntax);
-
-            return symbolDependencies
-                .Where(symbol => !IsLocalAccessibleAfterMove(symbol, newParent))
-                .ToList();
+            // if the scope declaration is a child or equal to the old parent, we aren't moving the bound syntax outside of its original scope.
+            return true;
         }
 
-        private bool IsLocalAccessibleAfterMove(LocalVariableSymbol symbol, SyntaxBase newParent)
-        {
-            // find out the scope where the local symbol starts being accessible
-            // (the declaration is outside of that node)
-            var bindingContainer = GetScopeBindingContainer(symbol);
+        return false;
+    }
 
-            // the symbol remains accessible IFF the newParent is the binding container or is below the binding container
-            return ReferenceEquals(newParent, bindingContainer) ||
-                this.semanticModel.SourceFile.Hierarchy.IsDescendant(node: newParent, potentialAncestor: bindingContainer);
+    /// <summary>
+    /// Gets the syntax in which the specified symbol starts being accessible.
+    /// </summary>
+    /// <param name="symbol">the local variable symbol</param>
+    private LocalScope GetScope(LocalVariableSymbol symbol)
+    {
+        var parent = this.semanticModel.GetSymbolParent(symbol);
+        if (parent is LocalScope scope)
+        {
+            return scope;
         }
 
-        /// <summary>
-        /// Gets the syntax in which the specified symbol starts being accessible.
-        /// </summary>
-        /// <param name="symbol">the local variable symbol</param>
-        private SyntaxBase GetScopeBindingContainer(LocalVariableSymbol symbol)
-        {
-            var parent = this.semanticModel.GetSymbolParent(symbol);
-            if (parent is LocalScope scope)
-            {
-                return scope.BindingSyntax;
-            }
-
-            throw new InvalidOperationException($"Unexpected local variable parent type '{parent?.GetType().Name}'");
-        }
+        throw new InvalidOperationException($"Unexpected local variable parent type '{parent?.GetType().Name}'");
     }
 }
