21183a69b22903acfe179bef6e72df239e9e9570
Daniel Ciborowski <dciborow@microsoft.com>
2023-04-14 15:28:03 -0400

    Update src/Bicep.Core/Semantics/Namespaces/SystemNamespaceType.cs
    Co-authored-by: Jonny Eskew <jeskew@users.noreply.github.com>

--- src/Bicep.Core/Semantics/Namespaces/SystemNamespaceType.cs
+++ src/Bicep.Core/Semantics/Namespaces/SystemNamespaceType.cs
@@ -1164,7 +1164,7 @@ namespace Bicep.Core.Semantics.Namespaces
 
             if (ExtractTokenFromObject(fileContent) is not { } token)
             {
-                // Instead of catching and returning the JSON parse exception, we simply return a generic error.
+                // Instead of catching and returning the YML parse exception, we simply return a generic error.
                 // This avoids having to deal with localization, and avoids possible confusion regarding line endings in the message.
                 return new(ErrorType.Create(DiagnosticBuilder.ForPosition(arguments[0]).UnparseableJsonType()));
             }
