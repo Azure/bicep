b0a64f1521d787e1980b8c2460b5aa5b15832cc3
Shenglong Li <shenglol@microsoft.com>
2023-05-23 18:23:47 -0700

    Add test baselines

--- src/Bicep.Core.IntegrationTests/PrettyPrint/PrettyPrinterV2Tests.cs
+++ src/Bicep.Core.IntegrationTests/PrettyPrint/PrettyPrinterV2Tests.cs
@@ -29,17 +29,12 @@ namespace Bicep.Core.IntegrationTests.PrettyPrint
         public void Print_DifferentWidth_OptimizesLayoutAccordingly(int width)
         {
             var dataSet = DataSets.PrettyPrint_LF;
-            var program = ParserHelper.Parse(dataSet.Bicep, out var lexingErrorLookup, out var parsingErrorLookup);
+            var options = new PrettyPrinterV2Options(Width: width);
 
-            var writer = new StringWriter();
-            var context = PrettyPrinterV2Context.Create(program, new(Width: width), lexingErrorLookup, parsingErrorLookup);
-
-            PrettyPrinterV2.PrintTo(writer, context);
-            var output = writer.ToString();
+            var output = Print(dataSet.Bicep, options);
 
             var outputFileName = $"main.pprint.w{width}.bicep";
             var outputFile = FileHelper.SaveResultFile(this.TestContext, Path.Combine(dataSet.Name, outputFileName), output);
-
             var expected = dataSet.ReadDataSetFile(outputFileName);
 
             output.Should().EqualWithLineByLineDiffOutput(
@@ -47,50 +42,19 @@ namespace Bicep.Core.IntegrationTests.PrettyPrint
                 expected,
                 expectedLocation: DataSet.GetBaselineUpdatePath(dataSet, outputFileName),
                 actualLocation: outputFile);
-        }
 
-
-        [DataTestMethod]
-        [DataRow(40)]
-        [DataRow(80)]
-        [TestCategory(BaselineHelper.BaselineTestCategory)]
-        public void Print_FormattedOutput_ProducesTheSameOutput(int width)
-        {
-            var dataSet = DataSets.PrettyPrint_LF;
-            var options = new PrettyPrinterV2Options(Width: width);
-
-            var program = ParserHelper.Parse(dataSet.Bicep, out var lexingErrorLookup, out var parsingErrorLookup);
-
-            var writer = new StringWriter();
-            var context = PrettyPrinterV2Context.Create(program, options, lexingErrorLookup, parsingErrorLookup);
-
-            PrettyPrinterV2.PrintTo(writer, context);
-            var output = writer.ToString();
-
-            writer = new StringWriter();
-            program = ParserHelper.Parse(output, out lexingErrorLookup, out parsingErrorLookup);
-            context = PrettyPrinterV2Context.Create(program, options, lexingErrorLookup, parsingErrorLookup);
-
-            PrettyPrinterV2.PrintTo(writer, context);
-
-            var newOutput = writer.ToString();
-
-            newOutput.Should().Be(output);
+            AssertConsistentOutput(output, options);
         }
 
         [DataTestMethod]
         [DynamicData(nameof(GetData), DynamicDataSourceType.Method, DynamicDataDisplayNameDeclaringType = typeof(DataSet), DynamicDataDisplayName = nameof(DataSet.GetDisplayName))]
-        public void Print_DataSets_ShouldMatchBaseline(DataSet dataSet)
+        public void Print_DataSet_ProducesExpectedOutput(DataSet dataSet)
         {
-            if (dataSet.Name != "DisableNextLineDiagnosticsDirective_CRLF")
-            {
-                return;
-            }
-
             var program = ParserHelper.Parse(dataSet.Bicep, out var lexingErrorLookup, out var parsingErrorLookup);
+            var options = PrettyPrinterV2Options.Default;
 
             var writer = new StringWriter();
-            var context = PrettyPrinterV2Context.Create(program, PrettyPrinterV2Options.Default, lexingErrorLookup, parsingErrorLookup);
+            var context = PrettyPrinterV2Context.Create(program, options, lexingErrorLookup, parsingErrorLookup);
 
             PrettyPrinterV2.PrintTo(writer, context);
             var output = writer.ToString();
@@ -105,6 +69,22 @@ namespace Bicep.Core.IntegrationTests.PrettyPrint
                 expected,
                 expectedLocation: DataSet.GetBaselineUpdatePath(dataSet, outputFileName),
                 actualLocation: outputFile);
+
+            AssertConsistentOutput(output, options);
+        }
+
+        private static void AssertConsistentOutput(string formatted, PrettyPrinterV2Options options) =>
+            Print(formatted, options).Should().Be(formatted);
+
+        private static string Print(string programText, PrettyPrinterV2Options options)
+        {
+            var program = ParserHelper.Parse(programText, out var lexingErrorLookup, out var parsingErrorLookup);
+            var context = PrettyPrinterV2Context.Create(program, options, lexingErrorLookup, parsingErrorLookup);
+            var writer = new StringWriter();
+
+            PrettyPrinterV2.PrintTo(writer, context);
+
+            return writer.ToString();
         }
 
         private static IEnumerable<object[]> GetData() => DataSets.AllDataSets
