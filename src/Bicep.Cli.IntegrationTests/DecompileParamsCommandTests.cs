d0f96f111b2ed448b23f318094e3f0e9bffd8eaa
Muhammad Usman <mohammadusmanxero@gmail.com>
2023-05-30 11:48:51 -0500

    fix error

--- src/Bicep.Cli.IntegrationTests/DecompileParamsCommandTests.cs
+++ src/Bicep.Cli.IntegrationTests/DecompileParamsCommandTests.cs
@@ -35,7 +35,7 @@ namespace Bicep.Cli.IntegrationTests
   ""$schema"": ""https://schema.management.azure.com/schemas/2019-04-01/deploymentParameters.json#"",
   ""contentVersion"": ""1.0.0.0"",
   ""parameters"": {
-    ""foo"": {
+    ""first"": {
       ""value"": ""test""
     },
     ""second"": {
