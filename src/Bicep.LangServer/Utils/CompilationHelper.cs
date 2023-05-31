8cb71f7495496729fe1b92316d9e32de8b84385c
Stephen Weatherford <Stephen.Weatherford.com>
2023-05-31 14:50:57 -0700

    Fix UI portion of 9580 (restore force)

--- src/Bicep.LangServer/Utils/CompilationHelper.cs
+++ src/Bicep.LangServer/Utils/CompilationHelper.cs
@@ -20,12 +20,13 @@ namespace Bicep.LanguageServer.Utils
             this.compilationManager = compilationManager;
         }
 
-        public async Task<Compilation> GetCompilation(DocumentUri documentUri)
+        public async Task<Compilation> GetRefreshedCompilation(DocumentUri documentUri)
         {
             // Bicep file could contain load functions like loadTextContent(..). We'll refresh compilation to detect changes in files referenced in load functions.
             var fileUri = documentUri.ToUri();
             compilationManager.RefreshCompilation(fileUri);
             var context = compilationManager.GetCompilation(documentUri);
+
             // CompilationContext will be null if the file is not open in the editor.
             // E.g. When user right clicks on a file from the explorer context menu without opening the file and invokes build/deploy
             if (context is null)
