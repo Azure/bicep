d58ff50703e3e9aad053313c756e7a7531836722
Lauren Nguyen <t-lnguyen@microsoft.com>
2023-06-22 15:33:39 -0700

    Fix failing config schema test

--- src/Bicep.Core/Analyzers/Linter/Rules/MaxNumberAssertsRule.cs
+++ src/Bicep.Core/Analyzers/Linter/Rules/MaxNumberAssertsRule.cs
@@ -6,8 +6,6 @@ using Bicep.Core.Semantics;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Bicep.Core.Analyzers.Linter.Rules
 {
