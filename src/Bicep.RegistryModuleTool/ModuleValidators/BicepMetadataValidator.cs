137e168f745a9968e4d86b5dc57a67cfda07dadb
Stephen Weatherford (MSFT) <stephwe@microsoft.com>
2023-06-29 16:35:15 -0700

    fix CR comment

--- src/Bicep.RegistryModuleTool/ModuleValidators/BicepMetadataValidator.cs
+++ src/Bicep.RegistryModuleTool/ModuleValidators/BicepMetadataValidator.cs
@@ -6,17 +6,21 @@ using Bicep.RegistryModuleTool.Extensions;
 using Bicep.RegistryModuleTool.ModuleFiles;
 using Microsoft.Extensions.Logging;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 
 namespace Bicep.RegistryModuleTool.ModuleValidators
 {
-    public sealed class BicepMetadataValidator : IModuleFileValidator
+    public sealed partial class BicepMetadataValidator : IModuleFileValidator
     {
         private readonly ILogger logger;
 
         private readonly MainBicepFile latestMainBicepFile;
+
+        [GeneratedRegex("^(?:Azure\\/)?[a-zA-Z\\d](?:[a-zA-Z\\d]|-(?=[a-zA-Z\\d])){0,38}$")]
+        private static partial Regex OwnerRegex();
 
         public BicepMetadataValidator(ILogger logger, MainBicepFile latestMainBicepFile)
         {
@@ -33,21 +37,24 @@ namespace Bicep.RegistryModuleTool.ModuleValidators
             var moduleDescription = templateFile.DescriptionMetadata;
             var moduleOwner = templateFile.OwnerMetadata;
 
-            var missingMetadata = new List<string>();
-            if (moduleName is null)
+            if (moduleName is null || moduleDescription is null || moduleOwner is null)
             {
-                missingMetadata.Add(MainBicepFile.ModuleNameMetadataName);
-            }
-            if (moduleDescription is null)
-            {
-                missingMetadata.Add(MainBicepFile.ModuleDescriptionMetadataName);
-            }
-            if (moduleOwner is null)
-            {
-                missingMetadata.Add(MainBicepFile.ModuleOwnerMetadataName);
-            }
-            if (missingMetadata.Count > 0)
-            {
+                var missingMetadata = new List<string>();
+
+                if (moduleName is null)
+                {
+                    missingMetadata.Add(MainBicepFile.ModuleNameMetadataName);
+                }
+                if (moduleDescription is null)
+                {
+                    missingMetadata.Add(MainBicepFile.ModuleDescriptionMetadataName);
+                }
+                if (moduleOwner is null)
+                {
+                    missingMetadata.Add(MainBicepFile.ModuleOwnerMetadataName);
+                }
+                Debug.Assert(missingMetadata.Count > 0);
+
                 errorMessageBuilder.AppendLine($"The file \"{latestMainBicepFile.Path}\" is invalid. The following metadata statements are missing:");
                 foreach (var entry in missingMetadata)
                 {
@@ -57,17 +64,17 @@ namespace Bicep.RegistryModuleTool.ModuleValidators
                 throw new InvalidModuleException(errorMessageBuilder.ToString().Trim());
             }
 
-            ValidateMinLength("name", moduleName!, 10);
-            ValidateMaxLength("name", moduleName!, 60);
+            ValidateMinLength("name", moduleName, 10);
+            ValidateMaxLength("name", moduleName, 60);
 
-            ValidateMinLength("description", moduleDescription!, 10);
-            ValidateMaxLength("description", moduleDescription!, 120);
+            ValidateMinLength("description", moduleDescription, 10);
+            ValidateMaxLength("description", moduleDescription, 120);
 
-            ValidateAgainstRegex(
-                "owner",
-                moduleOwner!,
-                new Regex("^(?:Azure\\/)?[a-zA-Z\\d](?:[a-zA-Z\\d]|-(?=[a-zA-Z\\d])){0,38}$"),
-                "(it must be a GitHub username or a team under the Azure organization.)");
+            if (!OwnerRegex().IsMatch(moduleOwner))
+            {
+                errorMessageBuilder.AppendLine(
+                    $"The file \"{latestMainBicepFile.Path}\" is invalid. \"metadata {MainBicepFile.ModuleOwnerMetadataName}\" must be a GitHub username or a team under the Azure organization.");
+            }
 
             if (errorMessageBuilder.Length > 0)
             {
@@ -87,16 +94,6 @@ namespace Bicep.RegistryModuleTool.ModuleValidators
                 if (metadataValue.Length > maxLength)
                 {
                     errorMessageBuilder.AppendLine($"The file \"{latestMainBicepFile.Path}\" is invalid. \"metadata {metadataName}\" must contain no more than {maxLength} characters.");
-                }
-            }
-
-            void ValidateAgainstRegex(string metadataName, string metadataValue, Regex regex, string errorMessageAdditionalDetails)
-            {
-                if (!regex.IsMatch(metadataValue))
-                {
-                    errorMessageBuilder.AppendLine(
-                        $"The file \"{latestMainBicepFile.Path}\" is invalid. \"metadata {metadataName}\" must match the following regex: {regex.ToString()}"
-                        + (errorMessageAdditionalDetails is null ? "" : " " + errorMessageAdditionalDetails));
                 }
             }
         }
