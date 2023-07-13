411c12f96f01d45924c2e08c84525e43d947c69d
Muhammad Usman <mohammadusmanxero@gmail.com>
2023-07-13 11:44:04 -0500

    addressed some feedback

--- src/Bicep.LangServer/Deploy/IDeploymentHelper.cs
+++ src/Bicep.LangServer/Deploy/IDeploymentHelper.cs
@@ -10,6 +10,34 @@ namespace Bicep.LanguageServer.Deploy
 {
     public interface IDeploymentHelper
     {
-        Task<BicepDeploymentStartResponse> StartDeploymentAsync(IDeploymentCollectionProvider deploymentCollectionProvider, ArmClient armClient, string documentPath, string template, string id, string scope, string location, string deploymentId, string portalUrl, string deploymentName, JsonElement parametersFileContents, IDeploymentOperationsCache deploymentOperationsCache);
+        /// <summary>
+        /// Starts a deployment at provided target scope and returns <see cref="BicepDeploymentStartResponse"/>.
+        /// </summary>
+        /// <param name="deploymentCollectionProvider">deployment collection provider</param>
+        /// <param name="armClient">arm client</param>
+        /// <param name="documentPath">path to bicep file used in deployment</param>
+        /// <param name="template">template used in deployment</param>
+        /// <param name="id">id string to create the ResourceIdentifier from</param>
+        /// <param name="scope">target scope</param>
+        /// <param name="location">location to store the deployment data</param>
+        /// <param name="deploymentId">deployment id</param>
+        /// <param name="portalUrl">azure management portal URL</param>
+        /// <param name="deploymentName">deployment name</param>
+        /// <param name="parametersFileContents">contents of parameter file used in deployment</param>
+        /// <param name="deploymentOperationsCache">deployment operations cache that needs to be updated</param>
+        /// <returns><see cref="BicepDeploymentStartResponse"/></returns>
+        Task<BicepDeploymentStartResponse> StartDeploymentAsync(
+            IDeploymentCollectionProvider deploymentCollectionProvider, 
+            ArmClient armClient, 
+            string documentPath, 
+            string template, 
+            string id, 
+            string scope, 
+            string location, 
+            string deploymentId, 
+            string portalUrl, 
+            string deploymentName, 
+            JsonElement parametersFileContents, 
+            IDeploymentOperationsCache deploymentOperationsCache);
     }
 }
