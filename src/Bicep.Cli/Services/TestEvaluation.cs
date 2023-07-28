db28d4df973dbc4a4bbc614622b0a32216146476
JuanPabloMontoya271 <juanpablomontoyae@gmail.com>
2023-07-28 16:51:11 -0700

    Clean up comments

--- src/Bicep.Cli/Services/TestEvaluation.cs
+++ src/Bicep.Cli/Services/TestEvaluation.cs
@@ -1,35 +1,10 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-// using System;
-// using System.Collections.Generic;
-// using System.Linq;
-// using Azure.Deployments.Core.Definitions.Schema;
-
-// namespace Bicep.Cli.Services
-// {
-//     public class TestEvaluation
-//     {
-//         public Template? Template { get; }
-//         public string Error { get; }
-//         public Dictionary<string, bool> Assertions { get; }
-//         public Dictionary<string, bool> FailedAssertions { get; }
-//         public bool Success => Error == "" && (FailedAssertions.Count == 0);
-//         public bool Skip => Error != "";
-//         public TestEvaluation(Template? template, Dictionary<string, bool>? assertions, string? error)
-//         {
-//             Template = template;
-//             Assertions = assertions ?? new Dictionary<string, bool>();
-//             Error = error ?? "";
-//             FailedAssertions = assertions?.Where(a => !a.Value).ToDictionary(a => a.Key, a => a.Value) ?? new Dictionary<string, bool>();
-//         }
-//     }
-// }
-
 using System;
 using System.Collections.Immutable;
 using Azure.Deployments.Core.Definitions.Schema;
-using Bicep.Core.Semantics;
+
 
 namespace Bicep.Cli.Services;
 
