d620a8d028d1a736abdde2c626165f9543dfa91a
Anthony Martin <38542602+anthony-c-martin@users.noreply.github.com>
2023-08-24 20:46:31 -0600

    Add highlight.js build step & example usage (#11614)
    Motivated by #11600. We've already done most of the hard work - just
    haven't completed it by providing a bundled plugin & documentation.
    ###### Microsoft Reviewers: [Open in
    CodeFlow](https://microsoft.github.io/open-pr/?codeflow=https://github.com/Azure/bicep/pull/11614)

---
+++ src/highlightjs/test/grammar.test.ts
@@ -0,0 +1,32 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import { existsSync } from "fs";
+import { readFile, rm } from "fs/promises";
+import { spawnSync } from "child_process";
+
+const root = `${__dirname}/..`;
+
+const grammarPath = `${root}/dist/bicep.min.js`;
+async function generateGrammar() {
+  spawnSync(`webpack`, {
+    cwd: root,
+    stdio: 'inherit',
+    encoding: 'utf8'
+  });
+
+  return await readFile(`${root}/out/usage.min.js`, { encoding: 'utf8' });
+}
+
+describe('grammar tests', () => {
+  it('should exist', () => {
+    expect(existsSync(grammarPath)).toBeTruthy();
+  });
+
+  it('should be up-to-date', async () => {
+    const savedGrammar = await readFile(grammarPath, { encoding: 'utf8' });
+
+    const generatedGrammar = await generateGrammar();
+    expect(generatedGrammar).toStrictEqual(savedGrammar);
+  });
+});
