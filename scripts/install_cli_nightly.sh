223e0f0cabfaf16447e7b8eb4345028f88f48162
Anthony Martin <38542602+anthony-c-martin@users.noreply.github.com>
2023-09-11 17:16:27 -0400

    Update install_cli_nightly.sh

--- scripts/install_cli_nightly.sh
+++ scripts/install_cli_nightly.sh
@@ -5,6 +5,11 @@ usage="Usage: ./install_cli_nightly.sh <platform> <arch>"
 platform=${1:?"Missing platform (e.g. 'osx' or 'linux'). ${usage}"}
 arch=${2:?"Missing arch (e.g. 'x64' or 'arm64'). ${usage}"}
 
+if ! command -v gh > /dev/null; then
+  echo "Please install the GitHub CLI: https://cli.github.com/"
+  exit 1
+fi
+
 # Fetch
 REPO="Azure/bicep"
 lastRunId=$(gh run list -R $REPO --branch main --workflow build --status success -L 1 --json databaseId -q ".[0].databaseId")
