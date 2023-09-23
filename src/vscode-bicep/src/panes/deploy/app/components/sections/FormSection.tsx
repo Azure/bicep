f062088cbad3c476c7e0be91377c6cc852ed9bff
Anthony Martin <38542602+anthony-c-martin@users.noreply.github.com>
2023-09-23 10:38:48 -0400

    Allow collapsing/expanding of titles

--- src/vscode-bicep/src/panes/deploy/app/components/sections/FormSection.tsx
+++ src/vscode-bicep/src/panes/deploy/app/components/sections/FormSection.tsx
@@ -1,15 +1,20 @@
 import { VSCodeDivider } from "@vscode/webview-ui-toolkit/react";
-import { FC, PropsWithChildren } from "react";
+import { FC, PropsWithChildren, useState } from "react";
 
 type FormSectionProps = PropsWithChildren<{
   title: string;
 }>;
 
 export const FormSection: FC<FormSectionProps> = ({ title, children, }) => {
+  const [open, setOpen] = useState(true);
+
   return (
     <section className="form-section">
-      <h2>{title}</h2>
-      {children}
+      <h2 onClick={() => setOpen(!open)} style={{ userSelect: 'none', cursor: 'pointer' }}>
+        <span className={`codicon codicon-${open ? 'chevron-up' : 'chevron-down'}`} />
+        {title}
+      </h2>
+      {open && children}
       <VSCodeDivider />
     </section>
   );
