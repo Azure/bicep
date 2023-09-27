787e3f9e7914b68980d5c5622c355deb8af718a2
Anthony Martin <38542602+anthony-c-martin@users.noreply.github.com>
2023-09-27 12:26:16 -0400

    Reword error message (#11961)
    Closes #11953
    ###### Microsoft Reviewers: [Open in
    CodeFlow](https://microsoft.github.io/open-pr/?codeflow=https://github.com/Azure/bicep/pull/11961)

--- src/Bicep.Decompiler.IntegrationTests/DecompilationTests.cs
+++ src/Bicep.Decompiler.IntegrationTests/DecompilationTests.cs
@@ -96,7 +96,7 @@ namespace Bicep.Core.IntegrationTests
         [DataRow("Files/NonWorking/unknownprops.json", "[15:29]: Unrecognized top-level resource property 'madeUpProperty'")]
         [DataRow("Files/NonWorking/invalid-schema.json", "[2:98]: $schema value \"https://schema.management.azure.com/schemas/2019-04-01/deploymentParameters.json#\" did not match any of the known ARM template deployment schemas.")]
         [DataRow("Files/NonWorking/keyvault-secret-reference.json", "[25:38]: Failed to convert parameter \"mySecret\": KeyVault secret references are not currently supported by the decompiler.")]
-        [DataRow("Files/NonWorking/symbolic-names.json", "[27:16]: Decompilation of symbolic name templates is not currently supported")]
+        [DataRow("Files/NonWorking/symbolic-names.json", "[19:16]: Decompilation of symbolic name templates is not currently supported")]
         public async Task Decompiler_raises_errors_for_unsupported_features(string resourcePath, string expectedMessage)
         {
             Func<Task> onDecompile = async () =>
