755cd6d6c26e5dd592dff4000327d2eec357a1a4
asilverman <asilverman@microsoft.com>
2023-10-12 18:14:54 -0700

    simplify code

--- src/Bicep.Core/Syntax/ImportSpecification.cs
+++ src/Bicep.Core/Syntax/ImportSpecification.cs
@@ -18,40 +18,26 @@ namespace Bicep.Core.Syntax
     {
         // The setting below adds syntax highlighting for regex.
         // language=regex
-        private const string SchemePattern = "br";
-
-        // The setting below adds syntax highlighting for regex.
-        // language=regex
         private const string NamePattern = "[a-zA-Z][a-zA-Z0-9]+";
 
         // Regex copied from https://semver.org/.
         // language=regex
         private const string SemanticVersionPattern = @"(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?";
 
-        private static readonly Regex SpecificationWithAliasPattern = new(
-            @$"^(?<scheme>{SchemePattern})/(?<registryAlias>{NamePattern}):(?<name>{NamePattern})@(?<version>{SemanticVersionPattern})$",
-            RegexOptions.ECMAScript | RegexOptions.Compiled);
-        private static readonly Regex SpecificationWithoutAliasPattern = new(
-            @$"^(?<scheme>{SchemePattern}):(?<address>[a-zA-Z][\w\.-]+(:\d+)?(\/[a-zA-Z0-9]*)+)?@(?<version>{SemanticVersionPattern})$",
-            RegexOptions.ECMAScript | RegexOptions.Compiled);
         private static readonly Regex BareSpecification = new(
-            @$"^(?<name>{NamePattern})@(?<version>{SemanticVersionPattern})$");
+            @$"(?<name>{NamePattern})@(?<version>{SemanticVersionPattern})$");
 
 
-        private ImportSpecification(string scheme, string unqualifiedAddress, string registryAlias, string name, string version, bool isValid, TextSpan span)
+        private ImportSpecification(string unqualifiedAddress, string name, string version, bool isValid, TextSpan span)
         {
-            Scheme = scheme;
             UnqualifiedAddress = unqualifiedAddress;
-            RegistryAlias = registryAlias;
             Name = name;
             Version = version;
             IsValid = isValid;
             Span = span;
         }
 
-        public string Scheme { get; }
         private string UnqualifiedAddress { get; }
-        public string RegistryAlias { get; }
 
         public string Name { get; }
 
@@ -72,14 +58,10 @@ namespace Bicep.Core.Syntax
                         trivia.TriviaName,
                         trivia.TriviaName,
                         trivia.TriviaName,
-                        trivia.TriviaName,
-                        trivia.TriviaName,
                         false,
                         trivia.Span),
                 _
                     => new ImportSpecification(
-                        LanguageConstants.ErrorName,
-                        LanguageConstants.ErrorName,
                         LanguageConstants.ErrorName,
                         LanguageConstants.ErrorName,
                         LanguageConstants.ErrorName,
@@ -102,63 +84,22 @@ namespace Bicep.Core.Syntax
             var matchBareSpecification = BareSpecification.Match(value);
             if (matchBareSpecification.Success)
             {
-                var name = matchBareSpecification.Groups["name"].Value;
-                var version = matchBareSpecification.Groups["version"].Value;
-                if (name == "az")
-                {
-                    return new(
-                        LanguageConstants.ErrorName,
-                        LanguageConstants.ErrorName,
-                        LanguageConstants.ErrorName,
-                        LanguageConstants.ErrorName,
-                        LanguageConstants.ErrorName,
-                        false,
-                        stringSyntax.Span);
-                }
-                var span = new TextSpan(stringSyntax.Span.Position + 1, name.Length);
-                return new(string.Empty, string.Empty, string.Empty, name, version, true, span);
-            }
-            var matchSpecificationWithAlias = SpecificationWithAliasPattern.Match(value);
-            if (matchSpecificationWithAlias.Success)
-            {
-                var scheme = matchSpecificationWithAlias.Groups["scheme"].Value;
-                var alias = matchSpecificationWithAlias.Groups["registryAlias"].Value;
-                var name = matchSpecificationWithAlias.Groups["name"].Value;
-                var version = matchSpecificationWithAlias.Groups["version"].Value;
-
-                var address = value.Replace('@', ':');
-
-                var offset = value.Split('@')[0].Length;
-                var span = new TextSpan(stringSyntax.Span.Position + 1, offset);
-
-                return new(scheme, address, alias, name, version, true, span);
-
+                return new(
+                 LanguageConstants.ErrorName,
+                 LanguageConstants.ErrorName,
+                 LanguageConstants.ErrorName,
+                 false,
+                 stringSyntax.Span);
             }
 
-            var matchSpecificationWithoutAlias = SpecificationWithoutAliasPattern.Match(value);
-            if (matchSpecificationWithoutAlias.Success)
-            {
-                var scheme = matchSpecificationWithoutAlias.Groups["scheme"].Value;
-                var version = matchSpecificationWithoutAlias.Groups["version"].Value;
+            var name = matchBareSpecification.Groups["name"].Value;
+            var version = matchBareSpecification.Groups["version"].Value;
+            var parts = value.Split('@');
+            var artifactAddress = parts[0];
+            
+            var span = new TextSpan(stringSyntax.Span.Position + 1, artifactAddress.Length);
 
-                var address = value.Replace('@', ':');
-
-                var offset = address.Length;
-                var span = new TextSpan(stringSyntax.Span.Position + 1, offset);
-
-                var name = address.Split('/')[^1].Split(':')[0];
-
-                return new(scheme, address, string.Empty, name, version, true, span);
-            }
-
-            return new(
-                LanguageConstants.ErrorName,
-                LanguageConstants.ErrorName,
-                LanguageConstants.ErrorName,
-                LanguageConstants.ErrorName,
-                LanguageConstants.ErrorName,
-                false,
-                stringSyntax.Span);
+            return new(string.Join(':', parts), name, version, artifactAddress != "az", span);
         }
     }
 }
