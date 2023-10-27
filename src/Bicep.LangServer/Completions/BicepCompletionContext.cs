7cd6821d0c7c2f2f0bd5c61276b3c6dfc9e03f4d
Jonathan Eskew <jeskew@microsoft.com>
2023-10-27 11:41:49 -0400

    User `provider` keyword in syntax pattern declarations

--- src/Bicep.LangServer/Completions/BicepCompletionContext.cs
+++ src/Bicep.LangServer/Completions/BicepCompletionContext.cs
@@ -29,24 +29,24 @@ namespace Bicep.LanguageServer.Completions
 
         private static readonly CompositeSyntaxPattern ExpectingImportSpecification = CompositeSyntaxPattern.Create(
             cursor: '|',
-            "import |",
-            "import |'kuber'",
-            "import 'kuber|'");
+            "provider |",
+            "provider |'kuber'",
+            "provider 'kuber|'");
 
         private static readonly CompositeSyntaxPattern ExpectingImportWithOrAsKeyword = CompositeSyntaxPattern.Create(
             cursor: '|',
-            "import 'kubernetes@1.0.0' |",
-            "import 'kubernetes@1.0.0' a|",
-            "import 'kubernetes@1.0.0' |b");
+            "provider 'kubernetes@1.0.0' |",
+            "provider 'kubernetes@1.0.0' a|",
+            "provider 'kubernetes@1.0.0' |b");
 
         private static readonly CompositeSyntaxPattern ExpectingImportConfig = CompositeSyntaxPattern.Create(
             cursor: '|',
-            "import 'kubernetes@1.0.0' with |",
-            "import 'kubernetes@1.0.0' with | as foo");
+            "provider 'kubernetes@1.0.0' with |",
+            "provider 'kubernetes@1.0.0' with | as foo");
 
         private static readonly SyntaxPattern ExpectingImportAsKeyword = SyntaxPattern.Create(
             cursor: '|',
-            "import 'kubernetes@1.0.0' with { foo: true } |");
+            "provider 'kubernetes@1.0.0' with { foo: true } |");
 
         // completions will replace only these token types
         // all others will result in an insertion upon completion commit
