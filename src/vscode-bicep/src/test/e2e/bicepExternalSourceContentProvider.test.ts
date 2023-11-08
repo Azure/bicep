dbd5494ec585b49115db5b3680ec39bb70b7b989
Stephen Weatherford <Stephen.Weatherford.com>
2023-11-08 10:20:11 -0800

    fix e2e tests

--- src/vscode-bicep/src/test/e2e/bicepExternalSourceContentProvider.test.ts
+++ src/vscode-bicep/src/test/e2e/bicepExternalSourceContentProvider.test.ts
@@ -7,15 +7,15 @@ import { decodeExternalSourceUri } from "../../language/decodeExternalSourceUri"
 describe("bicepExternalSourceContentProvider", () => {
   it("decodes correctly with source", () => {
     const uri =
-      "bicep-extsrc:br%3Asaw.azurecr.io/complicated%3Av1.0-beta/my%20entrypoint.bicep%20%28complicated%3Av1.0-beta%29?br%3Asaw.azurecr.io%2Fcomplicated%3Av1.0-beta#my%20entrypoint.bicep";
+      "bicep-extsrc:br%3Asaw.azurecr.io/complicated%3Av1.0-beta/my%20entrypoint.bicep%20%28very%20complicated%20for%20just%20version%20v1.0-beta%29?br%3Asaw.azurecr.io%2Fcomplicated%3Av1.0-beta#my%20entrypoint.bicep";
     const decoded = decodeExternalSourceUri(Uri.parse(uri));
 
     expect(decoded.moduleReference).toBe(
       "br:saw.azurecr.io/complicated:v1.0-beta",
     );
-    expect(decoded.requestedSourceFile).toBe("entrypoint.bicep");
+    expect(decoded.requestedSourceFile).toBe("my entrypoint.bicep");
     expect(decoded.title).toBe(
-      "br:saw.azurecr.com/complicated:1.0-beta/entrypoint.bicep (complicated:1.0.2)",
+      "br:saw.azurecr.com/complicated:v1.0-beta/my entrypoint.bicep (very complicated for just version v1.0-beta)",
     );
   });
 
