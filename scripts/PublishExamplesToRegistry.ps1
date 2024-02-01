21b8e31183934f2600516701fb09c6789f971e3c
Stephen Weatherford (MSFT) <StephenWeatherford@users.noreply.github.com>
2024-02-01 09:47:08 -0800

    Improve PublishExamples.ps1 (#13159)
    Minor improvements
    
    ###### Microsoft Reviewers: [Open in
    CodeFlow](https://microsoft.github.io/open-pr/?codeflow=https://github.com/Azure/bicep/pull/13159)
    
    Co-authored-by: Stephen Weatherford <Stephen.Weatherford.com>

--- scripts/PublishExamplesToRegistry.ps1
+++ scripts/PublishExamplesToRegistry.ps1
@@ -6,46 +6,65 @@ param (
   $BicepPath,
 
   # E.g. ~/repos/bicep-registry-modules/modules
-  #   or ~/repos/azure-quickstart-templates/quickstarts or 
+  #   or ~/repos/azure-quickstart-templates/quickstarts or
   [Parameter(Mandatory = $true)]
   [string]
   $ExamplesPath,
 
+  # Registry name (to be placed in front of ".azurecr.io")
   [Parameter(Mandatory = $true)]
   [string]
   $RegistryName,
 
   [Parameter(Mandatory = $true)]
   [string]
-  $Tag
+  $Tag,
+
+  [Parameter(Mandatory = $false)]
+  [string]
+  $RegistryPathPrefix = "examples"
 )
 
 $ErrorActionPreference = 'Stop';
 
+# Resolve paths before we change current location
+$BicepPath = Resolve-Path $BicepPath
+$ExamplesPath = Resolve-Path $ExamplesPath
+
 $previousLocation = Get-Location;
-try
-{
+try {
   Set-Location $BicepPath;
 
   $levelDirs = Get-ChildItem -Path $ExamplesPath;
 
   foreach ($levelDir in $levelDirs) {
-    $moduleDirs = Get-ChildItem -Path $levelDir.FullName;
-    
-    foreach ($moduleDir in $moduleDirs) {
-      $bicep = Join-Path -Path $moduleDir.Fullname -ChildPath 'main.bicep';
+    $moduleDirs = Get-ChildItem -Path $levelDir.FullName -Directory
 
-      if(Test-Path $bicep)
-      {
-        $artifactRef = "br:$($RegistryName).azurecr.io/examples/$($levelDir.Name)/$($moduleDir.Name):$($Tag)";
-        Write-Output $artifactRef;
-        ./bicep publish $bicep --target $artifactRef
+    foreach ($moduleDir in $moduleDirs) {
+      $modulePath = Join-Path -Path $moduleDir.Fullname -ChildPath 'main.bicep'
+      Write-Host "Checking for $($modulePath)"
+
+      if (Test-Path $modulePath) {
+        $artifactRef = "br:$($RegistryName).azurecr.io/$($RegistryPathPrefix)/$($levelDir.Name.ToLowerInvariant())/$($moduleDir.Name.ToLowerInvariant()):$($Tag)";
+
+        Write-Host "./bicep publish $modulePath --target $artifactRef --with-source --force"
+        ./bicep publish $modulePath --target $artifactRef --with-source --force
+
+        # Write out the bicep needed to reference the module
+        Write-Output @"
+// $($levelDir.Name)/$($moduleDir.Name)
+module $($moduleDir.Name -replace "-","_") '$($artifactRef)' = {
+  name: '$($moduleDir.Name)'
+  params: {}
+}
+"@
+        Write-Host "Published $($modulePath) to $($artifactRef)"
+        Write-Host
       }
     }
   }
 }
-finally
-{
+finally {
   # restore previous location
   Set-Location $previousLocation;
 }
