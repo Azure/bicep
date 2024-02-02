9087813979715bb76d399905e8e0d622af6dc6f0
asilverman <asilverman@microsoft.com>
2024-02-01 23:41:14 -0800

    Update ImportSpecificationTests

--- src/Bicep.Core.UnitTests/Syntax/ImportSpecificationTest.cs
+++ src/Bicep.Core.UnitTests/Syntax/ImportSpecificationTest.cs
@@ -13,14 +13,14 @@ public class ImportSpecificationTests
     [TestMethod]
     [DataRow("br/public:az@1.0.0", true, "az")]
     [DataRow("br:mcr.microsoft.com/bicep/providers/az@1.0.0", true, "az")]
-    [DataRow("kubernetes@1.0.0", true, "kubernetes")]
-    [DataRow("sys@1.0.0", true, "sys")]
     [DataRow("br:mcr.microsoft.com/bicep/providers/az@1.0.0-beta", true, "az")] // valid prerelease version
     [DataRow("br:mcr.microsoft.com/bicep/providers/az@1.0.0+build.123", true, "az")] // valid build metadata
     [DataRow("br:mcr.microsoft.com/bicep/providers/az@1.0.0-beta+build.123", true, "az")] // valid prerelease version and build metadata
     [DataRow("br:mcr.microsoft.com/bicep/providers/kubernetes@1.0.0", true, "kubernetes")] // other provider
     [DataRow("sys", true, "sys")] // no version
     // Negative cases
+    [DataRow("kubernetes@1.0.0", false)]
+    [DataRow("sys@1.0.0", false)]
     [DataRow("az@1.0.0", false)] // 'az@1.0.0' is not a valid provider declaration statement
     [DataRow("br/public:az", false)] // is not a valid provider declaration statement, it lacks a version
     [DataRow("br:mcr.microsoft.com/bicep/providers/az@", false)] // is not a valid provider declaration statement, partially constructed
