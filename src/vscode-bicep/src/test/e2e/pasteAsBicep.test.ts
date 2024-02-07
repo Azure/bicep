860b002077043f2bedc90aff4a7ff5349303bca9
Shenglong Li <shenglol@microsoft.com>
2024-02-07 12:00:13 -0800

    Fix indentation

--- src/vscode-bicep/src/test/e2e/pasteAsBicep.test.ts
+++ src/vscode-bicep/src/test/e2e/pasteAsBicep.test.ts
@@ -221,39 +221,39 @@ describe("pasteAsBicep", (): void => {
   }`;
 
   const fullTemplateExpectedBicep = `param location string
-  param existingVirtualMachineNames array
-  param sqlServerLicenseType string
-  param existingVmResourceGroup string
-  param groupResourceId string
-  
-  @secure()
-  param domainAccountPassword string
-  
-  @secure()
-  param sqlServicePassword string
-  
-  resource existingVirtualMachineNames_resource 'Microsoft.SqlVirtualMachine/SqlVirtualMachines@2017-03-01-preview' = [
-    for item in existingVirtualMachineNames: {
-      name: trim(item)
-      location: location
-      properties: {
-        virtualMachineResourceId: resourceId(
-          existingVmResourceGroup,
-          'Microsoft.Compute/virtualMachines',
-          trim(item)
-        )
-        sqlServerLicenseType: sqlServerLicenseType
-        sqlVirtualMachineGroupResourceId: groupResourceId
-        wsfcDomainCredentials: {
-          clusterBootstrapAccountPassword: domainAccountPassword
-          clusterOperatorAccountPassword: domainAccountPassword
-          sqlServiceAccountPassword: sqlServicePassword
-        }
+param existingVirtualMachineNames array
+param sqlServerLicenseType string
+param existingVmResourceGroup string
+param groupResourceId string
+
+@secure()
+param domainAccountPassword string
+
+@secure()
+param sqlServicePassword string
+
+resource existingVirtualMachineNames_resource 'Microsoft.SqlVirtualMachine/SqlVirtualMachines@2017-03-01-preview' = [
+  for item in existingVirtualMachineNames: {
+    name: trim(item)
+    location: location
+    properties: {
+      virtualMachineResourceId: resourceId(
+        existingVmResourceGroup,
+        'Microsoft.Compute/virtualMachines',
+        trim(item)
+      )
+      sqlServerLicenseType: sqlServerLicenseType
+      sqlVirtualMachineGroupResourceId: groupResourceId
+      wsfcDomainCredentials: {
+        clusterBootstrapAccountPassword: domainAccountPassword
+        clusterOperatorAccountPassword: domainAccountPassword
+        sqlServiceAccountPassword: sqlServicePassword
       }
     }
-  ]
-  
-  `;
+  }
+]
+
+`;
 
   it("should convert pasted full ARM template - copy/paste", async () => {
     await runTest(`|`, fullTemplate1, "copy/paste", {
