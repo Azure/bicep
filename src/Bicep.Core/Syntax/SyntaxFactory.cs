f316152527d8baebde35dca8ecfc849a49c5396f
Ariel Silverman <asilverman@users.noreply.github.com>
2024-02-26 19:31:56 -0800

    MAINT: Delegate span calculation to StringSyntax method (#13460)
    ## Description
    
    Delegates the calculation of the InnerSpan of a string to a StringSyntax
    method to account for multiline strings.
    ###### Microsoft Reviewers: [Open in
    CodeFlow](https://microsoft.github.io/open-pr/?codeflow=https://github.com/Azure/bicep/pull/13460)

--- src/Bicep.Core/Syntax/SyntaxFactory.cs
+++ src/Bicep.Core/Syntax/SyntaxFactory.cs
@@ -214,7 +214,10 @@ namespace Bicep.Core.Syntax
             }
         }
 
-        public static StringSyntax CreateStringLiteral(string value) => CreateString(value.AsEnumerable(), Enumerable.Empty<SyntaxBase>());
+        public static StringSyntax CreateStringLiteral(string value) => CreateString(value.AsEnumerable(), []);
+
+        public static StringSyntax CreateStringLiteralWithTextSpan(string value)
+            => new(new FreeformToken(TokenType.StringComplete, new TextSpan(0, value.Length), value, [], []).AsEnumerable(), [], [value]);
 
         public static BooleanLiteralSyntax CreateBooleanLiteral(bool value) => new(value ? TrueKeywordToken : FalseKeywordToken, value);
 
