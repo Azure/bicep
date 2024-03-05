14af56cb1a3ba68501dc627dcc4b04f83ce1560b
Jonny Eskew <jeskew@users.noreply.github.com>
2024-03-05 11:31:08 -0500

    Update AzResourceTypeFactory cache key to take account of required flags (#13535)
    Resolves #13534 
    
    The cache within `AzResourceTypeFactory` was using the serialized type
    as a key, but since the AZ resource types reuse types pretty
    aggressively, this was leading to values being cached with the wrong
    flag assignments (which are dependent on *where* in a resource a
    particular type is used). This PR updates the cache to use a compound
    key instead to keep these usages separate.
    ###### Microsoft Reviewers: [Open in
    CodeFlow](https://microsoft.github.io/open-pr/?codeflow=https://github.com/Azure/bicep/pull/13535)

--- src/Bicep.Core.IntegrationTests/ScenarioTests.cs
+++ src/Bicep.Core.IntegrationTests/ScenarioTests.cs
@@ -5845,4 +5845,57 @@ var startAndEndBracketInString = 'x[]y'
 
         result.ExcludingLinterDiagnostics().Should().NotHaveAnyDiagnostics();
     }
+
+    // https://github.com/Azure/bicep/issues/13534
+    [TestMethod]
+    public void Test_Issue13534()
+    {
+        var result = CompilationHelper.Compile("""
+            var username = ''
+            var password = ''
+            var fileshareConnection = {
+              name: ''
+              authType: ''
+              rootfolder: ''
+              odgw: {
+                name: ''
+                resourceGroup: ''
+              }
+            }
+
+            var general = {
+              location: ''
+            }
+
+            resource resFileshareConnection 'Microsoft.Web/connections@2016-06-01' = {
+              name: fileshareConnection.name
+              location: general.location
+              kind: 'V2'
+              properties: {
+                displayName: fileshareConnection.name
+                customParameterValues: {}
+                parameterValues: {
+                  rootfolder: fileshareConnection.rootfolder
+                  authType: fileshareConnection.authType
+                  gateway: {
+                    name: fileshareConnection.odgw.name
+                    id: resourceId(fileshareConnection.odgw.resourceGroup, 'Microsoft.Web/connectionGateways', fileshareConnection.odgw.name)
+                    type: 'Microsoft.Web/connectionGateways'
+                  }
+                  username: username
+                  password: password
+                }
+                api: {
+                  id: subscriptionResourceId('Microsoft.Web/locations/managedApis', general.location, 'filesystem')
+                }
+              }
+            }
+            """);
+
+        result.ExcludingLinterDiagnostics().Should().HaveDiagnostics(new[]
+        {
+             ("BCP187", DiagnosticLevel.Warning, """The property "kind" does not exist in the resource or type definition, although it might still be valid. If this is an inaccuracy in the documentation, please report it to the Bicep Team."""),
+             ("BCP036", DiagnosticLevel.Warning, """The property "gateway" expected a value of type "string" but the provided value is of type "object"."""),
+        });
+    }
 }
