f93a058cd77ee5a2ee0be6fbc7f58dc7fc183941
polatengin <enpolat@microsoft.com>
2024-07-04 08:45:12 -0700

    removing unneeded using

--- src/Bicep.Core/Configuration/ExperimentalFeaturesExtensions.cs
+++ src/Bicep.Core/Configuration/ExperimentalFeaturesExtensions.cs
@@ -1,8 +1,6 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-using Bicep.Core.Analyzers.Linter;
-
 namespace Bicep.Core.Configuration
 {
     public static class ExperimentalFeaturesExtensions
