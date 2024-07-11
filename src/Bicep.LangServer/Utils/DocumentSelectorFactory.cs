8246b91dc893b3e25177ac7fd92bfd7c47007dd2
Stephen Weatherford (MSFT) <stephwe@microsoft.com>
2024-07-11 11:15:53 -0700

    Fix Bicep in VS receiving messages

--- src/Bicep.LangServer/Utils/DocumentSelectorFactory.cs
+++ src/Bicep.LangServer/Utils/DocumentSelectorFactory.cs
@@ -7,17 +7,30 @@ namespace Bicep.LanguageServer.Utils
 {
     public class DocumentSelectorFactory
     {
+        // VS doesn't currently support language filters in the document selector, so we must give it a file pattern
+        private static string Glob(params string[] extensions)
+        {
+            return "**/*{" + string.Join(',', extensions) + "}"; 
+        }
+
         public static readonly TextDocumentFilter[] BicepAndParams = [
             TextDocumentFilter.ForLanguage(LanguageConstants.LanguageId),
-            TextDocumentFilter.ForLanguage(LanguageConstants.ParamsLanguageId)
+            TextDocumentFilter.ForLanguage(LanguageConstants.ParamsLanguageId),
+            TextDocumentFilter.ForPattern(Glob(LanguageConstants.LanguageFileExtension, LanguageConstants.ParamsFileExtension))
         ];
 
         public static readonly TextDocumentFilter[] AllSupportedLangIds = [
-                TextDocumentFilter.ForLanguage(LanguageConstants.LanguageId),
+            TextDocumentFilter.ForLanguage(LanguageConstants.LanguageId),
             TextDocumentFilter.ForLanguage(LanguageConstants.ParamsLanguageId),
             TextDocumentFilter.ForLanguage(LanguageConstants.JsoncLanguageId),
             TextDocumentFilter.ForLanguage(LanguageConstants.JsonLanguageId),
-            TextDocumentFilter.ForLanguage(LanguageConstants.ArmTemplateLanguageId)
+            TextDocumentFilter.ForLanguage(LanguageConstants.ArmTemplateLanguageId),
+            TextDocumentFilter.ForPattern(Glob(
+                LanguageConstants.LanguageFileExtension,
+                LanguageConstants.ParamsFileExtension,
+                LanguageConstants.JsoncFileExtension,
+                LanguageConstants.JsonFileExtension,
+                LanguageConstants.ArmTemplateFileExtension))
         ];
 
         public static TextDocumentSelector CreateForBicepAndParams() => new(BicepAndParams);
