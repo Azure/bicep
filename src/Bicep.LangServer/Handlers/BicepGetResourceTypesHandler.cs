6e33a809a42b11ebb363022fed6185427aa9cb7f
Tate Smalligan <tasmalligan@microsoft.com>
2024-09-23 12:16:12 -0500

    Respond to comments

--- src/Bicep.LangServer/Handlers/BicepGetResourceTypesHandler.cs
+++ src/Bicep.LangServer/Handlers/BicepGetResourceTypesHandler.cs
@@ -4,6 +4,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using Bicep.Core.Analyzers.Linter.ApiVersions;
 using Bicep.Core.Configuration;
 using Bicep.Core.Features;
 using Bicep.Core.Resources;
+using Bicep.Core.TypeSystem.Providers;
 using Bicep.Core.TypeSystem.Providers.Az;
 using Bicep.LanguageServer.CompilationManager;
 using MediatR;
@@ -24,48 +26,35 @@ namespace Bicep.LanguageServer.Handlers
     // [Method("textDocument/getResourceCatalog", Direction.ClientToServer)]
     // public record BicepResourceTypeParams(TextDocumentIdentifier TextDocument) : ITextDocumentIdentifierParams, IRequest<BicepResourceTypes?>;
     [Method("bicep/getResourceCatalog", Direction.ClientToServer)]
-    public record BicepResourceTypeParams : IRequest<List<BicepResourceGroup>?>;
+    public record BicepResourceTypeParams : IRequest<ImmutableArray<BicepResourceTypeGroup>?>;
 
     public record BicepResourceType(string resourceType, string? apiVersion);
 
-    public record BicepResourceGroup(string group, IEnumerable<BicepResourceType> resourceTypes);
-    public class BicepGetResourceTypesHandler : IJsonRpcRequestHandler<BicepResourceTypeParams, List<BicepResourceGroup>?>
+    public record BicepResourceTypeGroup(string group, IEnumerable<BicepResourceType> resourceTypes);
+    public class BicepGetResourceTypesHandler : IJsonRpcRequestHandler<BicepResourceTypeParams, ImmutableArray<BicepResourceTypeGroup>?>
     {
-        public async Task<List<BicepResourceGroup>?> Handle(BicepResourceTypeParams request, CancellationToken cancellationToken)
+        private Lazy<IResourceTypeProvider> resourceTypeProviderLazy;
+
+        public BicepGetResourceTypesHandler()
         {
-            var resourceTypeProviderLazy = new Lazy<Core.TypeSystem.Providers.IResourceTypeProvider>(() => new AzResourceTypeProvider(new AzResourceTypeLoader(new AzTypeLoader())));
+            this.resourceTypeProviderLazy = new Lazy<IResourceTypeProvider>(() => new AzResourceTypeProvider(new AzResourceTypeLoader(new AzTypeLoader())));
+        }
+
+        public Task<ImmutableArray<BicepResourceTypeGroup>?> Handle(BicepResourceTypeParams request, CancellationToken cancellationToken)
+        {
             var types = resourceTypeProviderLazy.Value.GetAvailableTypes();
-            var typeList = types.ToList();
-            Dictionary<string, Dictionary<string, string?>> groupedResources = new();
-            foreach(var type in typeList)
-            {
-                var split = type.Type.Split('/');
-                var group = split[0];
-                var resourceType = split[1];
+            var result = types
+                .ToLookup(type => type.Type.Split('/')[0], x => new BicepResourceType(x.Type, x.ApiVersion))
+                .OrderBy(grouping => grouping.Key)
+                .Select(grouping => new BicepResourceTypeGroup(
+                    grouping.Key,
+                    grouping
+                        .GroupBy(type => type.resourceType)
+                        .Select(typeGroup => typeGroup.OrderByDescending(type => type.apiVersion).First()) // get the latest version
+                        .OrderBy(type => type.resourceType)))
+                .ToImmutableArray();
 
-                if (!groupedResources.ContainsKey(group))
-                {
-                    groupedResources[group] = new Dictionary<string, string?>();
-                }
-                if (!(type.ApiVersion != null && type.ApiVersion.Contains("preview")) && (!groupedResources[group].ContainsKey(resourceType) || string.Compare(groupedResources[group][resourceType], type.ApiVersion) < 0))
-                {
-                    groupedResources[group][resourceType] = type.ApiVersion;
-                }
-            }
-
-            var result = new List<BicepResourceGroup>();
-            foreach (var resourceGroup in groupedResources)
-            {
-                var resourceTypes = new List<BicepResourceType>();
-                foreach(var resourceType in resourceGroup.Value)
-                {
-                    resourceTypes.Add(new BicepResourceType(resourceType.Key, resourceType.Value));
-                }
-                ;
-                result.Add(new BicepResourceGroup(resourceGroup.Key, [.. resourceTypes.OrderBy(kvp => kvp.resourceType)]));
-            }
-            result.OrderBy(kvp => kvp.group);
-            return await Task.FromResult<List<BicepResourceGroup>?>([.. result.OrderBy(kvp => kvp.group)]);
+            return Task.FromResult<ImmutableArray<BicepResourceTypeGroup>?>(result);
         }
     }
 }
