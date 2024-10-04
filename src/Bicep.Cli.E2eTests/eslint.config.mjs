f62be088bd1058f9410d4ca54a34ef0ac4f89a6d
Shenglong Li <shenglol@microsoft.com>
2024-10-04 14:58:18 -0700

    Enable vitest lint

--- src/Bicep.Cli.E2eTests/eslint.config.mjs
+++ src/Bicep.Cli.E2eTests/eslint.config.mjs
@@ -4,25 +4,32 @@
 // @ts-check
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
+import vitest from "@vitest/eslint-plugin";
 import notice from "eslint-plugin-notice";
-import globals from "globals";
 
-export default tseslint.config(
-  {
-    files: ["src/**/*.ts"],
-    extends: [eslint.configs.recommended, ...tseslint.configs.recommended],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.node,
-    },
-    plugins: { notice },
-    rules: {
-      "notice/notice": [
-        "error",
-        {
-          templateFile: "../copyright-template.js",
-        },
-      ],
+export default tseslint.config({
+  files: ["src/**/*.ts"],
+  extends: [eslint.configs.recommended, ...tseslint.configs.recommended],
+  languageOptions: {
+    ecmaVersion: 2020,
+    globals: {
+      ...vitest.environments.env.globals,
     },
   },
-);
+  plugins: { notice, vitest },
+  rules: {
+    "notice/notice": [
+      "error",
+      {
+        templateFile: "../copyright-template.js",
+      },
+    ],
+    ...vitest.configs.recommended.rules,
+    "vitest/expect-expect": [
+      "error",
+      {
+        assertFunctionNames: ["expect", "expect*", "**.shouldSucceed", "**.shouldFail"],
+      },
+    ],
+  },
+});
