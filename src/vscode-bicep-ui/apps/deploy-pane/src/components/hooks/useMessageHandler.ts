a8c0a02f66ca4546aab30ddd6c36974c364e4fb1
Shenglong Li <shenglol@microsoft.com>
2024-11-15 19:10:38 -0800

    Undo a fix

--- src/vscode-bicep-ui/apps/deploy-pane/src/components/hooks/useMessageHandler.ts
+++ src/vscode-bicep-ui/apps/deploy-pane/src/components/hooks/useMessageHandler.ts
@@ -133,7 +133,10 @@ export function useMessageHandler(props: UseMessageHandlerProps) {
     vscode.postMessage(createReadyMessage());
     vscode.postMessage(createGetStateMessage());
     return () => window.removeEventListener("message", handleMessageEvent);
-  }, [persistedState, setErrorMessage, setLocalDeployRunning]);
+  // TODO: This is an anti-pattern. We should refactor this.
+  // We cannot add dependencies to this hook because it will cause infinite loops somehow.
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   function savePersistedState(state: DeployPaneState) {
     vscode.postMessage(createSaveStateMessage(state));
