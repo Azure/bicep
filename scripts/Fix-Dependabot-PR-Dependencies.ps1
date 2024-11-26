6837ce5dd6166c1510687e7d0d09640948db18f1
Stephen Weatherford (MSFT) <stephwe@microsoft.com>
2024-11-26 12:40:39 -0800

    work

--- scripts/Fix-Dependabot-PR-Dependencies.ps1
+++ scripts/Fix-Dependabot-PR-Dependencies.ps1
@@ -1,6 +1,7 @@
 # This script is used to fix the dependabot PR dependencies by triggering the lockfiles-command workflow for each PR and then closing and reopening the PRs to force a rebuild.
 
-$maxPRs = 2
+$maxPRs = 100
+$dryRun = $false
 
 function getPrLink($prNumber) {
     return "https://github.com/azure/bicep/pull/$($prNumber)"
@@ -18,16 +19,21 @@ function processPR {
     )
 
     Write-Host "Running lockfiles-command workflow for PR $(getPrLink($prNumber))"
-    gh workflow run lockfiles-command.yml --ref=$prRef
-    Start-Sleep -Seconds 15
+    if (!$dryRun) {
+        gh workflow run lockfiles-command.yml --ref=$prRef
+    }
 
-    Write-Host "Waiting for the workflow to complete for PR $(getPrLink($prNumber))"
+    Write-Host "Waiting for the lockfiles-command workflow to complete for PR $(getPrLink($prNumber))"
+    if (!$dryRun) {
+        Start-Sleep -Seconds 15
+    }
+
     while ($true) {
         $runningOutput = gh run list --workflow=lockfiles-command.yml --json status,headBranch
         $running = $runningOutput | ConvertFrom-Json | Where-Object { $_.headBranch -eq $prRef }
 
         if (!$running) {
-            Write-Host "No lockfiles-command workflows found for PR $(getPrLink($prNumber))"
+            Write-Warning "No lockfiles-command workflows found for PR $(getPrLink($prNumber))"
             $allPrs.Value = $allPrs.Value | Where-Object { $_.number -ne $prNumber }
             return  # Exit the method
         }
@@ -36,31 +42,36 @@ function processPR {
         if ($inProgress) {
             Write-Host -NoNewline "."
         } else {
-            Write-Host "lockfiles-command for PR $(getPrLink($prNumber)) has completed."
+            Write-Host "lockfiles-command for $(getPrLink($prNumber)) has completed."
             break
         }
 
         Start-Sleep -Seconds 15
     }
 
-    Write-Host "Closing and reopening PR $(getPrLink($prNumber)) to force a rebuild..."
-    gh pr close $prNumber
-    gh pr reopen $prNumber -c "Forcing rebuild"
-
-    Write-Host "Waiting for the checks to complete for PR $(getPrLink($prNumber))"
-    Start-Sleep -Seconds 15  # Wait for the PR to reopen before checking the status
-    try {
-        # Use gh checks wait with --fail-fast to exit on the first failure
-        gh pr checks $prNumber --watch --fail-fast
-        Write-Host "All checks for PR $(getPrLink($prNumber)) have completed successfully."
-        gh pr merge $prNumber --rebase --auto
-        Write-Host "PR $(getPrLink($prNumber)) has been set to auto-merge.";
-
-        $allPrs.Value = $allPrs.Value | Where-Object { $_.number -ne $prNumber }
-    } catch {
-        Write-Host "A check has failed for PR $(getPrLink($prNumber)). Continuing immediately."
-        # Handle the failure case here if needed
+    Write-Host "Closing and reopening $(getPrLink($prNumber)) to force a rebuild..."
+    if (!$dryRun) {
+        gh pr close $prNumber
+        gh pr reopen $prNumber -c "Forcing rebuild"
     }
+
+    Write-Host "Waiting for the required checks to complete for $(getPrLink($prNumber))"
+    if (!$dryRun) {
+        Start-Sleep -Seconds 15  # Wait for the PR to reopen before checking the status
+    }
+
+    # Use gh checks wait with --fail-fast to exit on the first failure
+    gh pr checks $prNumber --watch --fail-fast
+    if ($LASTEXITCODE -ne 0) {
+        Write-Error "Checks for $(getPrLink($prNumber)) have failed."
+        return  # Exit the method
+    }
+
+    Write-Host "All checks for $(getPrLink($prNumber)) have completed successfully."
+    gh pr merge $prNumber --rebase --auto
+    Write-Host "PR $(getPrLink($prNumber)) has been set to auto-merge.";
+
+    $allPrs.Value = $allPrs.Value | Where-Object { $_.number -ne $prNumber }
 }
 
 Write-Host "Getting list of matching PRs..."
@@ -76,3 +87,7 @@ foreach ($pr in $prs) {
 }
 
 Write-Host "All PRs processed."
+
+foreach ($pr in $prs) {
+    Write-Host "$(getPrLink($pr.number)): $(gh pr view $pr.number --json state | jq '.state')"
+}
