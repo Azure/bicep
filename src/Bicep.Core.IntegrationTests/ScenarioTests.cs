b2dec121256b05f4244e637b98c9ba163ccb743d
Jonny Eskew <jeskew@users.noreply.github.com>
2025-01-06 18:38:18 -0500

    Expose `fail` function (#15958)
    Resolves #14067
    
    This PR exposes the new `fail` function that is available in the ARM
    engine. One thing I went back and forth on is what the return type of
    this function should be. Three viable options are:
    - `Error`
    - `Any`
    - `Never`
    
    My first instinct was to use `Error`, but this makes the `fail` function
    surprisingly difficult to use. It's essentially forbidden everywhere
    unless you wrap it in `any()`, which hurts usability. I considered using
    a return type of `any` to remove the need for this boilerplate -- this
    is close to the semantic used in [the original C# proposal for `throw`
    expressions](https://github.com/dotnet/csharplang/blob/main/proposals/csharp-7.0/throw-expression.md)
    -- but I wasn't happy with the downstream effects of given Bicep's union
    type semantics. If `fail(...)` is of type `any`, then `condition ? 'boo'
    : fail('boo!')` is of type `any`, since the union of `any` with anything
    else is `any`. This isn't really correct, since the expression will only
    be evaluated if the condition is true, in which case the ternary's type
    would be `'boo'`.
    
    I ended up choosing `never` both because a `fail(...)` expression can't
    ever finish evaluating (and therefore doesn't really have a type) and
    because it had the right downstream assignability behavior. Very open to
    discussion on this if anyone feels strongly that `never` is incorrect,
    though!
    ###### Microsoft Reviewers: [Open in
    CodeFlow](https://microsoft.github.io/open-pr/?codeflow=https://github.com/Azure/bicep/pull/15958)

--- src/Bicep.Core.IntegrationTests/ScenarioTests.cs
+++ src/Bicep.Core.IntegrationTests/ScenarioTests.cs
@@ -6662,4 +6662,42 @@ var subnetId = vNet::subnets[0].id
 
         result.ExcludingLinterDiagnostics().Should().NotHaveAnyDiagnostics();
     }
+
+    // https://www.github.com/Azure/bicep/issues/14067
+    [TestMethod]
+    public void Fail_function_should_be_usable_anywhere_the_expression_will_be_evaluated_at_runtime()
+    {
+        var result = CompilationHelper.Compile("""
+            var foo = fail('foo')
+            param required string = fail('should have supplied one')
+
+            resource sa 'Microsoft.Storage/storageAccounts@2023-05-01' existing = if (fail('should be allowed here')) {
+              name: fail('should be allowed here, too')
+            }
+
+            output boolOutput bool = required == 'bar' && fail('this, too, should be fine')
+            output stringOutput string = required == 'bar' ? 'boo' : fail('ternary should have assigned type of \'boo\'')
+            """);
+
+        result.ExcludingLinterDiagnostics().Should().NotHaveAnyDiagnostics();
+    }
+
+    // https://www.github.com/Azure/bicep/issues/14067
+    [TestMethod]
+    public void Fail_function_should_not_be_usable_anywhere_the_expression_will_be_evaluated_at_compile_time()
+    {
+        var result = CompilationHelper.Compile("""
+            resource sa 'Microsoft.Storage/storageAccounts@2023-05-01' existing = {
+              name: 'acct'
+              dependsOn: [
+                fail('should not be allowed here')
+              ]
+            }
+            """);
+
+        result.ExcludingLinterDiagnostics().Should().HaveDiagnostics(new[]
+        {
+            ("BCP034", DiagnosticLevel.Error, "The enclosing array expected an item of type \"module[] | (resource | module) | resource[]\", but the provided item was of type \"never\"."),
+        });
+    }
 }
