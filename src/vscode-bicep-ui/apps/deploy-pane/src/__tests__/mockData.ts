399c8850f00d4d75bc6b1b74827daacac471c472
Bicep Automation <bicep@noreply.github.com>
2025-01-26 04:37:57 +0000

    Run code formatter

--- src/vscode-bicep-ui/apps/deploy-pane/src/__tests__/mockData.ts
+++ src/vscode-bicep-ui/apps/deploy-pane/src/__tests__/mockData.ts
@@ -1,10 +1,15 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-import type { DeploymentExtended, DeploymentOperation, DeploymentValidateResult, WhatIfOperationResult } from "@azure/arm-resources";
+import type {
+  DeploymentExtended,
+  DeploymentOperation,
+  DeploymentValidateResult,
+  WhatIfOperationResult,
+} from "@azure/arm-resources";
 import type { DeploymentScope } from "../models";
 
-export const fileUri = 'file:///my/deployment.bicep';
+export const fileUri = "file:///my/deployment.bicep";
 
 export const templateJson = `{
   "$schema": "https://schema.management.azure.com/schemas/2019-04-01/deploymentTemplate.json#",
@@ -38,7 +43,7 @@ export const parametersJson = `{
   }
 }`;
 
-export const scope: DeploymentScope & {scopeType: "resourceGroup"} = {
+export const scope: DeploymentScope & { scopeType: "resourceGroup" } = {
   scopeType: "resourceGroup",
   tenantId: "9eeeb42f-40a5-4229-849f-a71fae26c89f",
   subscriptionId: "a790367f-228f-43fd-a660-ed9abb7258ef",
@@ -52,19 +57,19 @@ export function getDeployResponse(): DeploymentExtended {
     properties: {
       outputs: {
         foo: {
-          value: 'bar'
-        }
-      }
-    }
+          value: "bar",
+        },
+      },
+    },
   };
 }
 
 export function getValidateResponse(): DeploymentValidateResult {
   return {
     error: {
-      code: 'InvalidTemplate',
-      message: 'The template is invalid'
-    }
+      code: "InvalidTemplate",
+      message: "The template is invalid",
+    },
   };
 }
 
@@ -72,35 +77,37 @@ export function getWhatIfResponse(): WhatIfOperationResult {
   return {
     changes: [
       {
-        changeType: 'Create',
+        changeType: "Create",
         resourceId: `/subscriptions/${scope.subscriptionId}/resourceGroups/${scope.resourceGroup}/providers/Microsoft.Storage/storageAccounts/fooAccount`,
-        delta: [{
-          propertyChangeType: 'Create',
-          path: 'properties',
-          before: {
-            accountType: 'Standard_LRS',
-            enableHttpsTrafficOnly: false
+        delta: [
+          {
+            propertyChangeType: "Create",
+            path: "properties",
+            before: {
+              accountType: "Standard_LRS",
+              enableHttpsTrafficOnly: false,
+            },
+            after: {
+              accountType: "Standard_ZRS",
+              enableHttpsTrafficOnly: true,
+            },
           },
-          after: {
-            accountType: 'Standard_ZRS',
-            enableHttpsTrafficOnly: true
-          }
-        }]
-      }
-    ]
-  }
+        ],
+      },
+    ],
+  };
 }
 
 export async function* getDeploymentOperations(): AsyncIterable<DeploymentOperation> {
   yield {
     properties: {
       provisioningOperation: "Create",
-      provisioningState: 'Succeeded',
+      provisioningState: "Succeeded",
       targetResource: {
-        resourceType: 'Microsoft.Storage/storageAccounts',
-        resourceName: 'fooAccount',
-        id: `/subscriptions/${scope.subscriptionId}/resourceGroups/${scope.resourceGroup}/providers/Microsoft.Storage/storageAccounts/fooAccount`
+        resourceType: "Microsoft.Storage/storageAccounts",
+        resourceName: "fooAccount",
+        id: `/subscriptions/${scope.subscriptionId}/resourceGroups/${scope.resourceGroup}/providers/Microsoft.Storage/storageAccounts/fooAccount`,
       },
-    }
-  }
+    },
+  };
 }
--- src/vscode-bicep-ui/apps/deploy-pane/vite.config.ts
+++ src/vscode-bicep-ui/apps/deploy-pane/vite.config.ts
@@ -2,8 +2,8 @@
 // Licensed under the MIT License.
 
 import react from "@vitejs/plugin-react";
-import { coverageConfigDefaults, defineConfig } from "vitest/config";
 import { ViteEjsPlugin } from "vite-plugin-ejs";
+import { coverageConfigDefaults, defineConfig } from "vitest/config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -21,14 +21,14 @@ export default defineConfig({
     globals: true,
     watch: false,
     restoreMocks: true,
-    environment: 'jsdom',
+    environment: "jsdom",
     coverage: {
       enabled: true,
       exclude: ["src/index.tsx", ...coverageConfigDefaults.exclude],
     },
     setupFiles: ["src/__tests__/setupGlobals.ts"],
     deps: {
-      inline: ['@vscode-elements/react-elements'],
-    }
-  }
+      inline: ["@vscode-elements/react-elements"],
+    },
+  },
 });
