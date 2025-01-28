Azure/bicep

commit f0470b85e309327a7ebde970efee5bd6060dc467

Add `resourceInput<>` and `resourceOutput<>` utility types (#15825)
Resolves #15183

This PR adds two new utility types (`resourceInput<>` and
`resourceOutput<>`) and deprecates the existing `resource<>` utility
types. These three utility types are all closely related but differ in
which property flags they surface:

- `resource<>` has the flags that were originally defined in the RP
types artifact. This is generally not what people want.
- `resourceInput<>` strips out all `WriteOnly` flags. This means that
template authors can access properties on a `resourceInput<>`-typed
parameter that would have previously raised a BCP077 diagnostic.
- `resourceOutput<>` strips out all `ReadOnly` flags.

Additionally, this PR allows the assignment of one `ReadOnly` property
to another `ReadOnly` property without raising any BCP073 diagnostics.
###### Microsoft Reviewers: [Open in
CodeFlow](https://microsoft.github.io/open-pr/?codeflow=https://github.com/Azure/bicep/pull/15825)

src/Bicep.Core/TypeSystem/TypeHelper.cs
@@ -693,5 +693,15 @@ namespace Bicep.Core.TypeSystem
 
         public static ObjectType MakeRequiredPropertiesOptional(ObjectType input)
             => TransformProperties(input, p => p.With(p.Flags & ~TypePropertyFlags.Required));
+
+        public static TypeSymbol RemovePropertyFlagsRecursively(TypeSymbol type, TypePropertyFlags flagsToRemove) => type switch
+        {
+            ObjectType @object => TransformProperties(@object, property => new(
+                property.Name,
+                new DeferredTypeReference(() => RemovePropertyFlagsRecursively(property.TypeReference.Type, flagsToRemove)),
+                property.Flags & ~flagsToRemove,
+                property.Description)),
+            _ => type,
+        };
     }
 }

src/Bicep.Core.IntegrationTests/UserDefinedDiscriminatedObjectUnionTests.cs
@@ -704,7 +704,7 @@ namespace Bicep.Core.IntegrationTests
                 new ServiceBuilder().WithFeatureOverrides(new(TestContext, ResourceDerivedTypesEnabled: true)),
                 """
                 @discriminator('computeType')
-                type taggedUnion = resource<'Microsoft.MachineLearningServices/workspaces/computes@2020-04-01'>.properties
+                type taggedUnion = resourceInput<'Microsoft.MachineLearningServices/workspaces/computes@2020-04-01'>.properties
                   | { computeType: 'foo', bar: string }
                 """);
 
@@ -716,43 +716,57 @@ namespace Bicep.Core.IntegrationTests
                       "DataFactory": {
                         "type": "object",
                         "metadata": {
-                          "__bicep_resource_derived_type!": "Microsoft.MachineLearningServices/workspaces/computes@2020-04-01#properties/properties/discriminator/mapping/DataFactory"
+                          "__bicep_resource_derived_type!": {
+                            "source": "Microsoft.MachineLearningServices/workspaces/computes@2020-04-01#properties/properties/discriminator/mapping/DataFactory"
+                          }
                         }
                       },
                       "Databricks": {
                         "type": "object",
                         "metadata": {
-                          "__bicep_resource_derived_type!": "Microsoft.MachineLearningServices/workspaces/computes@2020-04-01#properties/properties/discriminator/mapping/Databricks"
+                          "__bicep_resource_derived_type!": {
+                            "source": "Microsoft.MachineLearningServices/workspaces/computes@2020-04-01#properties/properties/discriminator/mapping/Databricks"
+                          }
                         }
                       },
                       "VirtualMachine": {
                         "type": "object",
                         "metadata": {
-                          "__bicep_resource_derived_type!": "Microsoft.MachineLearningServices/workspaces/computes@2020-04-01#properties/properties/discriminator/mapping/VirtualMachine"
+                          "__bicep_resource_derived_type!": {
+                            "source": "Microsoft.MachineLearningServices/workspaces/computes@2020-04-01#properties/properties/discriminator/mapping/VirtualMachine"
+                          }
                         }
                       },
                       "AmlCompute": {
                         "type": "object",
                         "metadata": {
-                          "__bicep_resource_derived_type!": "Microsoft.MachineLearningServices/workspaces/computes@2020-04-01#properties/properties/discriminator/mapping/AmlCompute"
+                          "__bicep_resource_derived_type!": {
+                            "source": "Microsoft.MachineLearningServices/workspaces/computes@2020-04-01#properties/properties/discriminator/mapping/AmlCompute"
+                          }
                         }
                       },
                       "AKS": {
                         "type": "object",
                         "metadata": {
-                          "__bicep_resource_derived_type!": "Microsoft.MachineLearningServices/workspaces/computes@2020-04-01#properties/properties/discriminator/mapping/AKS"
+                          "__bicep_resource_derived_type!": {
+                            "source": "Microsoft.MachineLearningServices/workspaces/computes@2020-04-01#properties/properties/discriminator/mapping/AKS"
+                          }
                         }
                       },
                       "HDInsight": {
                         "type": "object",
                         "metadata": {
-                          "__bicep_resource_derived_type!": "Microsoft.MachineLearningServices/workspaces/computes@2020-04-01#properties/properties/discriminator/mapping/HDInsight"
+                          "__bicep_resource_derived_type!": {
+                            "source": "Microsoft.MachineLearningServices/workspaces/computes@2020-04-01#properties/properties/discriminator/mapping/HDInsight"
+                          }
                         }
                       },
                       "DataLakeAnalytics": {
                         "type": "object",
                         "metadata": {
-                          "__bicep_resource_derived_type!": "Microsoft.MachineLearningServices/workspaces/computes@2020-04-01#properties/properties/discriminator/mapping/DataLakeAnalytics"
+                          "__bicep_resource_derived_type!": {
+                            "source": "Microsoft.MachineLearningServices/workspaces/computes@2020-04-01#properties/properties/discriminator/mapping/DataLakeAnalytics"
+                          }
                         }
                       },
                       "foo": {

src/Bicep.Core.Samples/Files/baselines/InvalidResourceDerivedTypes_LF/main.bicep
@@ -1,38 +1,38 @@
-type invalid1 = resource
+type invalid1 = resourceInput
 
-type invalid2 = resource<>
+type invalid2 = resourceInput<>
 
-type invalid3 = resource<'abc', 'def'>
-type invalid4 = resource<hello>
-type invalid5 = resource<'Microsoft.Storage/storageAccounts'>
-type invalid6 = resource<'Microsoft.Storage/storageAccounts@'>
-type invalid7 = resource<'Microsoft.Storage/storageAccounts@hello'>
-type invalid8 = resource<'notARealNamespace:Microsoft.Storage/storageAccounts@2022-09-01'>
-type invalid9 = resource<':Microsoft.Storage/storageAccounts@2022-09-01'>
-type invalid10 = resource<'abc' 'def'>
-type invalid11 = resource<123>
-type invalid12 = resource<resourceGroup()>
+type invalid3 = resourceInput<'abc', 'def'>
+type invalid4 = resourceInput<hello>
+type invalid5 = resourceInput<'Microsoft.Storage/storageAccounts'>
+type invalid6 = resourceInput<'Microsoft.Storage/storageAccounts@'>
+type invalid7 = resourceInput<'Microsoft.Storage/storageAccounts@hello'>
+type invalid8 = resourceInput<'notARealNamespace:Microsoft.Storage/storageAccounts@2022-09-01'>
+type invalid9 = resourceInput<':Microsoft.Storage/storageAccounts@2022-09-01'>
+type invalid10 = resourceInput<'abc' 'def'>
+type invalid11 = resourceInput<123>
+type invalid12 = resourceInput<resourceGroup()>
 
-type thisIsWeird = resource</*
+type thisIsWeird = resourceInput</*
 */'Astronomer.Astro/organizations@2023-08-01-preview'
 ///  >
 >
 
-type interpolated = resource<'Microsoft.${'Storage'}/storageAccounts@2022-09-01'>
+type interpolated = resourceInput<'Microsoft.${'Storage'}/storageAccounts@2022-09-01'>
 
 @sealed()
-type shouldNotBeSealable = resource<'Microsoft.Storage/storageAccounts@2022-09-01'>
+type shouldNotBeSealable = resourceInput<'Microsoft.Storage/storageAccounts@2022-09-01'>
 
 type hello = {
   @discriminator('hi')
-  bar: resource<'Astronomer.Astro/organizations@2023-08-01-preview'>
+  bar: resourceInput<'Astronomer.Astro/organizations@2023-08-01-preview'>
 }
 
-type typoInPropertyName = resource<'Microsoft.Storage/storageAccounts@2023-01-01'>.nom
-type typoInPropertyName2 = resource<'Microsoft.KeyVault/vaults@2022-07-01'>.properties.accessPolicies[*].tenatId
-type typoInPropertyName3 = resource<'Microsoft.KeyVault/vaults@2022-07-01'>.properties[*].accessPolicies.tenantId
-type typoInPropertyName4 = resource<'Microsoft.Web/customApis@2016-06-01'>.properties.connectionParameters.*.tyype
-type typoInPropertyName5 = resource<'Microsoft.Web/customApis@2016-06-01'>.properties.*.connectionParameters.type
+type typoInPropertyName = resourceInput<'Microsoft.Storage/storageAccounts@2023-01-01'>.nom
+type typoInPropertyName2 = resourceInput<'Microsoft.KeyVault/vaults@2022-07-01'>.properties.accessPolicies[*].tenatId
+type typoInPropertyName3 = resourceInput<'Microsoft.KeyVault/vaults@2022-07-01'>.properties[*].accessPolicies.tenantId
+type typoInPropertyName4 = resourceInput<'Microsoft.Web/customApis@2016-06-01'>.properties.connectionParameters.*.tyype
+type typoInPropertyName5 = resourceInput<'Microsoft.Web/customApis@2016-06-01'>.properties.*.connectionParameters.type
 
 module mod 'modules/mod.json' = {
   name: 'mod'

commit e845112a8cf0d0c8c5208cb834272972f2d4d18f

Reuse already updated object types when recursively removing type property flags (#16235)
Resolves #16219

The server crash reported in #16219 was caused by
https://github.com/Azure/bicep/pull/15825/files#diff-7d66583c4922f57735a4eac96cd074d356e8eace34ec5900860f093f3d0cbc69R701.
The FindPossibleSecretsVisitor relies on object identity to detect and
stop infinitely walking recursive types, and the linked line caused
otherwise identical object types within the assigned type of a
`resource` symbol to have different identities.
###### Microsoft Reviewers: [Open in
CodeFlow](https://microsoft.github.io/open-pr/?codeflow=https://github.com/Azure/bicep/pull/16235)

src/Bicep.Core/Analyzers/Linter/Common/FindPossibleSecretsVisitor.cs
@@ -17,6 +17,7 @@ namespace Bicep.Core.Analyzers.Linter.Common
 
         private readonly SemanticModel semanticModel;
         private readonly List<PossibleSecret> possibleSecrets = new();
+        private readonly HashSet<TypeSymbol> currentlyProcessing = new();
         private uint trailingAccessExpressions = 0;
 
         /// <summary>
@@ -70,17 +71,15 @@ namespace Bicep.Core.Analyzers.Linter.Common
 
         private static string PossibleSecretMessage(string possibleSecretName) => string.Format(CoreResources.PossibleSecretMessageSecureParam, possibleSecretName);
 
-        private IEnumerable<string> FindPathsToSecureTypeComponents(TypeSymbol type) => FindPathsToSecureTypeComponents(type, "", new());
+        private IEnumerable<string> FindPathsToSecureTypeComponents(TypeSymbol type) => FindPathsToSecureTypeComponents(type, "");
 
-        private IEnumerable<string> FindPathsToSecureTypeComponents(TypeSymbol type, string path, HashSet<TypeSymbol> visited)
+        private IEnumerable<string> FindPathsToSecureTypeComponents(TypeSymbol type, string path)
         {
             // types can be recursive. cut out early if we've already seen this type
-            if (visited.Contains(type))
+            if (!currentlyProcessing.Add(type))
             {
                 yield break;
             }
-
-            visited.Add(type);
 
             if (type.ValidationFlags.HasFlag(TypeSymbolValidationFlags.IsSecure))
             {
@@ -89,7 +88,7 @@ namespace Bicep.Core.Analyzers.Linter.Common
 
             if (type is UnionType union)
             {
-                foreach (var variantPath in union.Members.SelectMany(m => FindPathsToSecureTypeComponents(m.Type, path, visited)))
+                foreach (var variantPath in union.Members.SelectMany(m => FindPathsToSecureTypeComponents(m.Type, path)))
                 {
                     yield return variantPath;
                 }
@@ -118,25 +117,25 @@ namespace Bicep.Core.Analyzers.Linter.Common
                     case ObjectType obj:
                         if (obj.AdditionalProperties?.TypeReference.Type is TypeSymbol addlPropsType)
                         {
-                            foreach (var dictMemberPath in FindPathsToSecureTypeComponents(addlPropsType, $"{path}.*", visited))
+                            foreach (var dictMemberPath in FindPathsToSecureTypeComponents(addlPropsType, $"{path}.*"))
                             {
                                 yield return dictMemberPath;
                             }
                         }
 
-                        foreach (var propertyPath in obj.Properties.SelectMany(p => FindPathsToSecureTypeComponents(p.Value.TypeReference.Type, $"{path}.{p.Key}", visited)))
+                        foreach (var propertyPath in obj.Properties.SelectMany(p => FindPathsToSecureTypeComponents(p.Value.TypeReference.Type, $"{path}.{p.Key}")))
                         {
                             yield return propertyPath;
                         }
                         break;
                     case TupleType tuple:
-                        foreach (var pathFromIndex in tuple.Items.SelectMany((ITypeReference typeAtIndex, int index) => FindPathsToSecureTypeComponents(typeAtIndex.Type, $"{path}[{index}]", visited)))
+                        foreach (var pathFromIndex in tuple.Items.SelectMany((ITypeReference typeAtIndex, int index) => FindPathsToSecureTypeComponents(typeAtIndex.Type, $"{path}[{index}]")))
                         {
                             yield return pathFromIndex;
                         }
                         break;
                     case ArrayType array:
-                        foreach (var pathFromElement in FindPathsToSecureTypeComponents(array.Item.Type, $"{path}[*]", visited))
+                        foreach (var pathFromElement in FindPathsToSecureTypeComponents(array.Item.Type, $"{path}[*]"))
                         {
                             yield return pathFromElement;
                         }
@@ -144,7 +143,7 @@ namespace Bicep.Core.Analyzers.Linter.Common
                 }
             }
 
-            visited.Remove(type);
+            currentlyProcessing.Remove(type);
         }
 
 

src/Bicep.Core/Emit/ExpressionConverter.cs
@@ -448,9 +448,9 @@ namespace Bicep.Core.Emit
 
                 case "outputs":
                     var moduleSymbol = reference.Module;
-                    var hasSecureValues = expression.SourceSyntax is null
-                        || FindPossibleSecretsVisitor.FindPossibleSecretsInExpression(context.SemanticModel, expression.SourceSyntax).Any();
-                    if (context.SemanticModel.Features.SecureOutputsEnabled && hasSecureValues)
+                    if (context.SemanticModel.Features.SecureOutputsEnabled &&
+                        (expression.SourceSyntax is null ||
+                        FindPossibleSecretsVisitor.FindPossibleSecretsInExpression(context.SemanticModel, expression.SourceSyntax).Any()))
                     {
                         var deploymentResourceId = GetFullyQualifiedResourceId(moduleSymbol, reference.IndexContext?.Index);
                         var apiVersion = new JTokenExpression(EmitConstants.NestedDeploymentResourceApiVersion);

src/Bicep.Core/TypeSystem/TypeHelper.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+using System.Collections.Concurrent;
 using System.Collections.Immutable;
 using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
@@ -693,14 +694,30 @@ namespace Bicep.Core.TypeSystem
         public static ObjectType MakeRequiredPropertiesOptional(ObjectType input)
             => TransformProperties(input, p => p with { Flags = p.Flags & ~TypePropertyFlags.Required });
 
-        public static TypeSymbol RemovePropertyFlagsRecursively(TypeSymbol type, TypePropertyFlags flagsToRemove) => type switch
-        {
-            ObjectType @object => TransformProperties(@object, property => new(
+        public static TypeSymbol RemovePropertyFlagsRecursively(TypeSymbol type, TypePropertyFlags flagsToRemove)
+            => RemovePropertyFlagsRecursively(type, flagsToRemove, new());
+
+        private static TypeSymbol RemovePropertyFlagsRecursively(
+            TypeSymbol type,
+            TypePropertyFlags flagsToRemove,
+            ConcurrentDictionary<ObjectType, ObjectType> transformedObjectCache) => type switch
+            {
+                ObjectType @object => transformedObjectCache.GetOrAdd(
+                    @object,
+                    obj => RemovePropertyFlagsRecursively(obj, flagsToRemove, transformedObjectCache)),
+                _ => type,
+            };
+
+        private static ObjectType RemovePropertyFlagsRecursively(
+            ObjectType @object,
+            TypePropertyFlags flagsToRemove,
+            ConcurrentDictionary<ObjectType, ObjectType> cache) => TransformProperties(@object, property => new(
                 property.Name,
-                new DeferredTypeReference(() => RemovePropertyFlagsRecursively(property.TypeReference.Type, flagsToRemove)),
+                new DeferredTypeReference(() => RemovePropertyFlagsRecursively(
+                    property.TypeReference.Type,
+                    flagsToRemove,
+                    cache)),
                 property.Flags & ~flagsToRemove,
-                property.Description)),
-            _ => type,
-        };
+                property.Description));
     }
 }

src/Bicep.Core.IntegrationTests/ScenarioTests.cs
@@ -6747,4 +6747,34 @@ var subnetId = vNet::subnets[0].id
             ("BCP265", DiagnosticLevel.Error, "The name \"description\" is not a function. Did you mean \"sys.description\"?"),
         });
     }
+
+    [TestMethod]
+    public void Test_Issue16219()
+    {
+        // https://www.github.com/Azure/bicep/issues/16219
+        var result = CompilationHelper.Compile("""
+            @description('Required. The name of the Public IP Address.')
+            param name string
+
+            resource publicIpAddress 'Microsoft.Network/publicIPAddresses@2023-09-01' = {
+              name: name
+              location: resourceGroup().location
+              sku: {
+                name: 'Basic'
+                tier: 'Regional'
+              }
+              properties: {
+                publicIPAddressVersion: 'IPv4'
+                publicIPAllocationMethod: 'Static'
+                idleTimeoutInMinutes: 4
+                ipTags: []
+              }
+            }
+
+            @description('The public IP address of the public IP address resource.')
+            output ipAddress string = contains(publicIpAddress.properties, 'ipAddress') ? publicIpAddress.properties.ipAddress : ''
+            """);
+
+        result.ExcludingDiagnostics("use-safe-access").Should().NotHaveAnyDiagnostics();
+    }
 }

src/Bicep.Core.UnitTests/Diagnostics/Linter/Common/FindPossibleSecretsVisitorTests.cs
@@ -105,7 +105,6 @@ namespace Bicep.Core.UnitTests.Diagnostics.Linter.Common
                 output test = v.listAnything().keys[0].value
             "
             )]
-        /* TODO: blocked by https://github.com/Azure/bicep/issues/4833
         [DataRow(@"
                 param storageName string
 
@@ -119,8 +118,8 @@ namespace Bicep.Core.UnitTests.Diagnostics.Linter.Common
                   value: storage.listAnything().keys[0].value
                 }
             ",
-            "Outputs should not contain secrets. function 'listAnything'"
-        )]*/
+            "function 'listAnything'"
+        )]
         [DataRow(
             @"
                 param storageName string