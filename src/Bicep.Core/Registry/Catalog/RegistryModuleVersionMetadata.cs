4c0916cd79670c417b08c5bfc8c484814fee0367
Stephen Weatherford (MSFT) <stephwe@microsoft.com>
2025-01-29 10:21:57 -0800

    w

--- src/Bicep.Core/Registry/Catalog/RegistryModuleVersionMetadata.cs
+++ src/Bicep.Core/Registry/Catalog/RegistryModuleVersionMetadata.cs
@@ -5,7 +5,7 @@ namespace Bicep.Core.Registry.Catalog;
 
 public record RegistryModuleVersionMetadata(
     string Version,
-    bool? IsBicepModule,
+    bool? IsBicepModule, // null = unknown
     RegistryMetadataDetails Details
 );
 
