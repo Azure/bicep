07a563d222163fb4e50633e013793f37a1c1d668
Anthony Martin <38542602+anthony-c-martin@users.noreply.github.com>
2025-02-19 05:24:40 -0500

    Add basic param rendering tests

---
+++ src/vscode-bicep-ui/apps/deploy-pane/src/__tests__/components/ParamInputBox.test.tsx
@@ -0,0 +1,73 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import { render } from "@testing-library/react";
+import { expect, it, vi } from "vitest";
+import { ParamInputBox, type ParameterInputData } from "../../components/ParamInputBox";
+import type { ParamDefinition } from "../../models";
+
+// TODO these tests aren't particularly meaningful, as vite doesn't support web components
+
+it("renders a string input", () => {
+  const definition: ParamDefinition = {
+    name: "myStringParam",
+    type: "string",
+    defaultValue: "fooValue",
+  };
+  const data: ParameterInputData = {
+    isValid: true,
+  };
+  const onChangeData = vi.fn();
+
+  const { container } = render(<ParamInputBox definition={definition} data={data} disabled={false} onChangeData={onChangeData} />);
+
+  expect(container).toMatchSnapshot();
+});
+
+it("renders an integer input", () => {
+  const definition: ParamDefinition = {
+    name: "myIntParam",
+    type: "int",
+    defaultValue: 42,
+  };
+  const data: ParameterInputData = {
+    isValid: true,
+  };
+  const onChangeData = vi.fn();
+
+  const { container } = render(<ParamInputBox definition={definition} data={data} disabled={false} onChangeData={onChangeData} />);
+
+  expect(container).toMatchSnapshot();
+});
+
+it("renders a bool input", () => {
+  const definition: ParamDefinition = {
+    name: "myBoolParam",
+    type: "bool",
+    defaultValue: true,
+  };
+  const data: ParameterInputData = {
+    isValid: true,
+  };
+  const onChangeData = vi.fn();
+
+  const { container } = render(<ParamInputBox definition={definition} data={data} disabled={false} onChangeData={onChangeData} />);
+
+  expect(container).toMatchSnapshot();
+});
+
+it("renders a JSON input", () => {
+  const definition: ParamDefinition = {
+    name: "myJsonParam",
+    type: "object",
+    defaultValue: { foo: "bar" },
+  };
+  const data: ParameterInputData = {
+    isValid: true,
+  };
+  const onChangeData = vi.fn();
+
+  const { container } = render(<ParamInputBox definition={definition} data={data} disabled={false} onChangeData={onChangeData} />);
+
+  expect(container).toMatchSnapshot();
+});
