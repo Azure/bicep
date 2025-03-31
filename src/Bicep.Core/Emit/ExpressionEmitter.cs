3893100dda842a7b272e978b19e1131dd6bf88cf
Kyle Albert <5498623+kalbert312@users.noreply.github.com>
2025-03-31 12:59:25 -0400

    Code gen modifications for extensionConfig property access does not need a wrapper object.

--- src/Bicep.Core/Emit/ExpressionEmitter.cs
+++ src/Bicep.Core/Emit/ExpressionEmitter.cs
@@ -345,6 +345,7 @@ namespace Bicep.Core.Emit
                         [ExpressionFactory.CreateObjectProperty("keyVaultReference", ConvertToKeyVaultReference(functionCall))],
                         functionCall.SourceSyntax),
                 TernaryExpression ternary => new TernaryExpression(ternary.SourceSyntax, ternary.Condition, ConvertModuleExtensionConfig(ternary.True), ConvertModuleExtensionConfig(ternary.False)),
+                PropertyAccessExpression paExpr when IsExtensionConfigPropertyAccess(paExpr) => extensionConfigValueExpr,
                 _ => ExpressionFactory.CreateObject(
                     [ExpressionFactory.CreateObjectProperty("value", extensionConfigValueExpr)],
                     extensionConfigValueExpr.SourceSyntax)
@@ -371,6 +372,23 @@ namespace Bicep.Core.Emit
             }
 
             return ExpressionFactory.CreateObject(properties);
+        }
+
+        private static bool IsExtensionConfigPropertyAccess(PropertyAccessExpression propertyAccessExpr)
+        {
+            // NOTE(kylealbert): Extension config property access has an intermediate object of value and key vault reference. When this
+            // expression type is used, we must make sure the expression aligns with the schema where we're generating the language expression.
+
+            // check we're accessing the config property...
+            var parent = propertyAccessExpr.Base;
+
+            if (parent is not PropertyAccessExpression { PropertyName: LanguageConstants.ExtensionConfigPropertyName } parentPropertyAccessExpr)
+            {
+                return false;
+            }
+
+            // ... of an extension reference
+            return parentPropertyAccessExpr.Base is ExtensionReferenceExpression;
         }
 
         public void EmitProperty(ObjectPropertyExpression property)
