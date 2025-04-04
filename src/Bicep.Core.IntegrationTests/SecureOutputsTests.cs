7c84a216efc8025fed73ce4f84a34d5cd22712fc
MervynWuMSFT <114024746+MervynWuMSFT@users.noreply.github.com>
2025-04-04 09:55:27 -0700

    Remove Experimental Flag for SecureOutputs Feature. (#16796)
    # Contributing a Pull Request
    
    If you haven't already, read the full [contribution
    guide](https://github.com/Azure/bicep/blob/main/CONTRIBUTING.md). The
    guide may have changed since the last time you read it, so please
    double-check. Once you are done and ready to submit your PR, run through
    the relevant checklist below.
    
    ## Contributing to documentation
    
    * [ ] All documentation contributions should be made directly in the
    [Bicep documentation on Microsoft
    Docs](https://docs.microsoft.com/azure/azure-resource-manager/bicep/).
    
    ## Contributing an example
    
    We are integrating the Bicep examples into the [Azure QuickStart
    Templates](https://github.com/Azure/azure-quickstart-templates/blob/master/1-CONTRIBUTION-GUIDE/README.md).
    If you'd like to contribute new example `.bicep` files that showcase
    abilities of the language, please follow [these
    instructions](https://github.com/Azure/azure-quickstart-templates/blob/master/1-CONTRIBUTION-GUIDE/README.md)
    to add them directly there. We can still take bug reports and fixes for
    the existing examples for the time being.
    
    * [ ] This is a bug fix for an existing example
    * [ ] I have resolved all warnings and errors shown by the Bicep VS Code
    extension
    * [ ] I have checked that all tests are passing by running `dotnet test`
    * [ ] I have consistent casing for all of my identifiers and am using
    camelCasing unless I have a justification to use another casing style
    
    ## Contributing a feature
    
    * [ ] I have opened a new issue for the proposal, or commented on an
    existing one, and ensured that the Bicep maintainers are good with the
    design of the feature being implemented
    * [ ] I have included "Fixes #{issue_number}" in the PR description, so
    GitHub can link to the issue and close it when the PR is merged
    * [ ] I have appropriate test coverage of my new feature
    
    ## Contributing a snippet
    
    * [ ] I have a snippet that is either a single, generic resource or
    multi resource that uses [parent-child
    syntax](https://docs.microsoft.com/azure/azure-resource-manager/bicep/child-resource-name-type)
    * [ ] I have checked that there is not an equivalent snippet already
    submitted
    * [ ] I have used camelCasing unless I have a justification to use
    another casing style
    * [ ] I have placeholders values that correspond to their property names
    (e.g. `dnsPrefix: 'dnsPrefix'`), unless it's a property that MUST be
    changed or parameterized in order to deploy. In that case, I use
    'REQUIRED' e.g.
    [keyData](./src/Bicep.LangServer/Files/SnippetTemplates/res-aks-cluster.bicep#L26)
    * [ ] I have my symbolic name as the first tab stop ($1) in the snippet.
    e.g.
    [res-aks-cluster.bicep](./src/Bicep.LangServer/Files/SnippetTemplates/res-aks-cluster.bicep)
    * [ ] I have a resource name property equal to "name"
    * [ ] If applicable, I have set the `location` property to `location:
    /*${<id>:location}*/'location'` (not `resourceGroup().location`) where
    `<id>` is a placeholder id, and added `param location string` to the
    test's main.bicep file so that the resulting main.combined.bicep file
    used in the tests compiles without errors
    * [ ] I have verified that the snippet deploys correctly when used in
    the context of an actual bicep file
    
      e.g.
    
      ```bicep
    resource aksCluster
    'Microsoft.ContainerService/managedClusters@2021-03-01' = {
        name: 'name'
      ```
    
    ###### Microsoft Reviewers: [Open in
    CodeFlow](https://microsoft.github.io/open-pr/?codeflow=https://github.com/Azure/bicep/pull/16796)

--- src/Bicep.Core.IntegrationTests/SecureOutputsTests.cs
+++ src/Bicep.Core.IntegrationTests/SecureOutputsTests.cs
@@ -29,28 +29,7 @@ public class SecureOutputsTests
     public TestContext? TestContext { get; set; }
 
     [TestMethod]
-    public void Test_Issue2163_Deployments_Secure_Outputs_E2E()
-    {
-        // https://github.com/Azure/bicep/issues/2163
-        var result = CompilationHelper.Compile(new UnitTests.ServiceBuilder().WithFeatureOverrides(new(TestContext, SecureOutputsEnabled: true)),
-            ("main.bicep", @"
-                module foo 'foo.bicep' = {
-                  name: 'foo'
-                }
- 
-                output myOutput string = foo.outputs.secureOutput
-            "),
-            ("foo.bicep", @"
-                @secure()
-                output secureOutput string = '***secret***'
-            ")
-        );
-
-        result.Diagnostics.Should().NotHaveAnyDiagnostics();
-    }
-
-    [TestMethod]
-    public void Test_Issue2163_Deployments_Secure_Outputs_Expect_Error_SecureOutputsNotEnabled()
+    public void Test_Deployments_Secure_Outputs_Expect_No_Error()
     {
         // https://github.com/Azure/bicep/issues/2163
         var result = CompilationHelper.Compile(
@@ -75,18 +54,54 @@ public class SecureOutputsTests
                 output myOutput string = myInput
             ")
         );
-        result.ExcludingLinterDiagnostics().Should().HaveDiagnostics(new[]
-        {
-            ("BCP104", DiagnosticLevel.Error, "The referenced module has errors."),
-            ("BCP129", DiagnosticLevel.Error, "Function \"secure\" cannot be used as an output decorator.")
-        });
+        result.Diagnostics.Should().NotHaveAnyDiagnostics();
     }
 
     [TestMethod]
-    public void Test_Issue2163_Deployments_Secure_Outputs_Decorator_Translates_To_SecureString_And_SecureObject()
+    public void Test_Deployments_Secure_Outputs_Expect_Error()
+    {
+        // Test implicit secure value.
+        var result = CompilationHelper.Compile(
+            ("main.bicep", @"
+                @secure()
+                param myInput string
+
+                output myOutput string = myInput
+            ")
+        );
+        result.Diagnostics.Should().ContainDiagnostic("outputs-should-not-contain-secrets", DiagnosticLevel.Warning, "Outputs should not contain secrets. Found possible secret: secure value 'myInput'");
+
+        // Test referencing sub-module secure output value.
+        result = CompilationHelper.Compile(
+            ("main.bicep", @"
+                @secure()
+                param myInput string
+ 
+                module foo 'foo.bicep' = {
+                  name: 'foo'
+                  params: {
+                    myInput : myInput
+                  }
+                }
+
+                output myOutput string = foo.outputs.myOutput
+            "),
+            ("foo.bicep", @"
+                @secure()
+                param myInput string
+
+                @secure()
+                output myOutput string = myInput
+            ")
+        );
+        result.Diagnostics.Should().ContainDiagnostic("outputs-should-not-contain-secrets", DiagnosticLevel.Warning, "Outputs should not contain secrets. Found possible secret: secure value 'foo.outputs.myOutput'");
+    }
+
+    [TestMethod]
+    public void Test_Deployments_Secure_Outputs_Decorator_Translates_To_SecureString_And_SecureObject()
     {
         // https://github.com/Azure/bicep/issues/2163
-        var result = CompilationHelper.Compile(new UnitTests.ServiceBuilder().WithFeatureOverrides(new(TestContext, SecureOutputsEnabled: true)),
+        var result = CompilationHelper.Compile(
             ("main.bicep", @"
                 @secure()
                 output secureStringOutput string = 'intern project 2024'
@@ -102,72 +117,185 @@ public class SecureOutputsTests
         result.ExcludingLinterDiagnostics().Should().NotHaveAnyDiagnostics();
         result.Template.Should().HaveValueAtPath("$.outputs['secureStringOutput'].type", "securestring");
         result.Template.Should().HaveValueAtPath("$.outputs['secureObjectOutput'].type", "secureObject");
+
+        result = CompilationHelper.Compile(
+            ("main.bicep", @"
+                output secureObjectOutput {
+                      @secure()
+                      foo: string
+                      bar: string
+                    } = {
+                      foo: '***'
+                      bar: 'normal'
+                    }
+            ")
+        );
+
+        result.ExcludingLinterDiagnostics().Should().NotHaveAnyDiagnostics();
+        result.Template.Should().HaveValueAtPath("$.outputs['secureObjectOutput'].properties.foo.type", "securestring");
+        result.Template.Should().HaveValueAtPath("$.outputs['secureObjectOutput'].properties.bar.type", "string");
+        result.Template.Should().HaveValueAtPath("$.outputs['secureObjectOutput'].type", "object");
     }
 
     [TestMethod]
-    public void Test_Issue2163_Deployments_Secure_Outputs_Call_Correct_API()
+    public void Test_Deployments_Secure_Outputs_Call_Correct_API()
     {
         // https://github.com/Azure/bicep/issues/2163
-        var result = CompilationHelper.Compile(new UnitTests.ServiceBuilder().WithFeatureOverrides(new(TestContext, SecureOutputsEnabled: true)),
+        var result = CompilationHelper.Compile(
             ("main.bicep", @"
-                module foo 'foo.bicep' = {
-                  name: 'foo'
+                module secureOuputs 'secureOuputs.bicep' = {
+                  name: 'secureOuputs'
                 }
 
                 resource key 'Microsoft.KeyVault/vaults/secrets@2024-04-01-preview' = {
                   name: 'secrets/mysecret'
                   properties: {
-                    value: foo.outputs.secureOutput
+                    value: secureOuputs.outputs.secureOutput
                   }
                 }
 
-                module bar 'bar.bicep' = {
-                  name: 'bar'
+               resource key2 'Microsoft.KeyVault/vaults/secrets@2024-04-01-preview' = {
+                  name: 'secrets/mysecret2'
+                  properties: {
+                    value: implicitSecureOuputWithoutDecorator.outputs.secureObjectOutput.foo
+                  }
                 }
 
-                module baz 'baz.bicep' = {
-                  name: 'baz'
+                module noSecureOutput 'noSecureOutput.bicep' = {
+                  name: 'noSecureOutput'
+                }
+
+                module noSecureOutputWithSecureParam 'noSecureOutputWithSecureParam.bicep' = {
+                  name: 'noSecureOutputWithSecureParam'
                   params: {
-                    secureInput: foo.outputs.secureOutput
+                    secureInput: secureOuputs.outputs.secureOutput
                   }
                 }
 
-                output outputNormalVal string = foo.outputs.normalOutput
-                output outputSecureVal string = foo.outputs.secureOutput
-                output outputNormalVal2 string = bar.outputs.normalOutput
-                output outputNormalVal3 string = baz.outputs.normalOutput
+               module implicitSecureOuputWithoutDecorator 'implicitSecureOuputWithoutDecorator.bicep' = {
+                  name: 'implicitSecureOuputWithoutDecorator'
+                }
+
+                @secure()
+                output outputSecureVal string = secureOuputs.outputs.secureOutput
+                @secure()
+                output outputImplicitSecureObject_secureString string = implicitSecureOuputWithoutDecorator.outputs.secureObjectOutput.foo
+                @secure()
+                output outputImplicitSecureObject_normalString string = implicitSecureOuputWithoutDecorator.outputs.secureObjectOutput.bar
+                output outputNormalVal string = secureOuputs.outputs.normalOutput
+                output outputNormalVal2 string = noSecureOutput.outputs.normalOutput
+                output outputNormalVal3 string = noSecureOutputWithSecureParam.outputs.normalOutput
             "),
-            ("foo.bicep", @"
+            ("secureOuputs.bicep", @"
                 @secure()
                 output secureOutput string = '***secret***'
                 output normalOutput string = 'normal'
             "),
-            ("bar.bicep", @"
+            ("noSecureOutput.bicep", @"
                 output normalOutput string = 'normal'
             "),
-            ("baz.bicep", @"
+            ("noSecureOutputWithSecureParam.bicep", @"
                 @secure()
                 param secureInput string
 
                 output normalOutput string = 'normal'"
-            )
+            ),
+            ("implicitSecureOuputWithoutDecorator.bicep", @"
+                output secureObjectOutput {
+                      @secure()
+                      foo: string
+                      bar: string
+                    } = {
+                      foo: '***'
+                      bar: 'normal'
+                    }
+            ")
         );
 
         result.Diagnostics.Should().NotHaveAnyDiagnostics();
 
         // Verify referencing secure output in a resource property will be translated to listOutputsWithSecureValues function
-        result.Template.Should().HaveValueAtPath("$.resources[0].properties.value", "[listOutputsWithSecureValues(resourceId('Microsoft.Resources/deployments', 'foo'), '2022-09-01').secureOutput]");
+        result.Template.Should().HaveValueAtPath("$.resources['key'].properties.value", "[listOutputsWithSecureValues(resourceId('Microsoft.Resources/deployments', 'secureOuputs'), '2022-09-01').secureOutput]");
+
+        // Verify referencing implicit secure output in a resource property will be translated to listOutputsWithSecureValues function
+        result.Template.Should().HaveValueAtPath("$.resources['key2'].properties.value", "[listOutputsWithSecureValues(resourceId('Microsoft.Resources/deployments', 'implicitSecureOuputWithoutDecorator'), '2022-09-01').secureObjectOutput.foo]");
 
         // Verify referencing secure output will be translated to listOutputsWithSecureValues function
-        result.Template.Should().HaveValueAtPath("$.outputs['outputSecureVal'].value", "[listOutputsWithSecureValues(resourceId('Microsoft.Resources/deployments', 'foo'), '2022-09-01').secureOutput]");
+        result.Template.Should().HaveValueAtPath("$.outputs['outputSecureVal'].value", "[listOutputsWithSecureValues(resourceId('Microsoft.Resources/deployments', 'secureOuputs'), '2022-09-01').secureOutput]");
 
         // Verify referencing normal value from a deployment which contains secure outputs will be translated to listOutputsWithSecureValues function
-        result.Template.Should().HaveValueAtPath("$.outputs['outputNormalVal'].value", "[listOutputsWithSecureValues(resourceId('Microsoft.Resources/deployments', 'foo'), '2022-09-01').normalOutput]");
+        result.Template.Should().HaveValueAtPath("$.outputs['outputNormalVal'].value", "[listOutputsWithSecureValues(resourceId('Microsoft.Resources/deployments', 'secureOuputs'), '2022-09-01').normalOutput]");
 
         // Verify referencing normal value from a deployment which does NOT contain any secure outputs will be translated to normal reference function
-        result.Template.Should().HaveValueAtPath("$.outputs['outputNormalVal2'].value", "[reference(resourceId('Microsoft.Resources/deployments', 'bar'), '2022-09-01').outputs.normalOutput.value]");
+        result.Template.Should().HaveValueAtPath("$.outputs['outputNormalVal2'].value", "[reference('noSecureOutput').outputs.normalOutput.value]");
 
         // Verify referencing normal value from a deployment which does NOT contain any secure outputs but secure parameters will be translated to normal reference function
-        result.Template.Should().HaveValueAtPath("$.outputs['outputNormalVal3'].value", "[reference(resourceId('Microsoft.Resources/deployments', 'baz'), '2022-09-01').outputs.normalOutput.value]");
+        result.Template.Should().HaveValueAtPath("$.outputs['outputNormalVal3'].value", "[reference('noSecureOutputWithSecureParam').outputs.normalOutput.value]");
+
+        // Verify referencing secure output in a resource property will be translated to listOutputsWithSecureValues function
+        result.Template.Should().HaveValueAtPath("$.outputs['outputSecureVal'].value", "[listOutputsWithSecureValues(resourceId('Microsoft.Resources/deployments', 'secureOuputs'), '2022-09-01').secureOutput]");
+
+        // Verify referencing a normal string in an implicit secure output object will be translated to listOutputsWithSecureValues function
+        result.Template.Should().HaveValueAtPath("$.outputs['outputImplicitSecureObject_normalString'].value", "[listOutputsWithSecureValues(resourceId('Microsoft.Resources/deployments', 'implicitSecureOuputWithoutDecorator'), '2022-09-01').secureObjectOutput.bar]");
+
+        // Verify referencing a secure string in an implicit secure output object will be translated to listOutputsWithSecureValues function
+        result.Template.Should().HaveValueAtPath("$.outputs['outputImplicitSecureObject_secureString'].value", "[listOutputsWithSecureValues(resourceId('Microsoft.Resources/deployments', 'implicitSecureOuputWithoutDecorator'), '2022-09-01').secureObjectOutput.foo]");
+
+    }
+
+    [TestMethod]
+    public void Test_Deployments_Secure_Outputs_Using_Language_Version_2_0()
+    {
+        // secure parameter does NOT use language version 2.0
+        var result = CompilationHelper.Compile(
+            ("main.bicep", @"
+                @secure()
+                param secureInput string
+            ")
+        );
+        result.Template?.Root.ToString().Should().NotContain("\"languageVersion\": \"2.0\"");
+
+        // Generated template uses language version 2.0 when secure output is declared.
+        result = CompilationHelper.Compile(
+            ("main.bicep", @"
+                @secure()
+                param myInput string
+ 
+                @secure()
+                output myOutput string = myInput
+            ")
+        );
+        result.Template?.Root.ToString().Should().Contain("\"languageVersion\": \"2.0\"");
+
+        // Generated template uses language version 2.0 when secure output is declared in modules.
+        result = CompilationHelper.Compile(
+            ("main.bicep", @"
+                module foo 'foo.bicep' = {
+                  name: 'foo'
+                  params: {
+                    myInput : 'password'
+                  }
+                }
+
+                module bar 'bar.bicep' = {
+                  name: 'bar'
+                  params: {
+                    secureInput : foo.outputs.myOutput
+                  }
+                }
+            "),
+            ("foo.bicep", @"
+                @secure()
+                param myInput string
+ 
+                @secure()
+                output myOutput string = myInput
+            "),
+            ("bar.bicep", @"
+                @secure()
+                param secureInput string
+            ")
+        );
+        result.Template?.Root.ToString().Should().Contain("\"languageVersion\": \"2.0\"");
     }
 }
