27c8822a32ff7110c9d05bf410197ec57266bf14
Gabriel Ngandu-Biseba <gabriel.ngandu-biseba@tucrail.be>
2025-04-24 11:24:42 +0200

    Add detection of unused import alias, improve the code fix alias and use the correct type that should be detected in "invertedBindings" inside the comments

--- src/Bicep.Core/Analyzers/Linter/Rules/NoUnusedImportsRule.cs
+++ src/Bicep.Core/Analyzers/Linter/Rules/NoUnusedImportsRule.cs
@@ -3,11 +3,10 @@
 
 using Bicep.Core.Diagnostics;
 using Bicep.Core.Semantics;
-using Bicep.Core.Syntax;
 
 namespace Bicep.Core.Analyzers.Linter.Rules
 {
-    // Does not handle detection of unused exports when using wildcard imports
+    // Does not handle detection of unused exports when using wildcard imports. Only if the alias is unused.
     public sealed class NoUnusedImportsRule : NoUnusedRuleBase
     {
         public new const string Code = "no-unused-imports";
@@ -28,7 +27,7 @@ namespace Bicep.Core.Analyzers.Linter.Rules
         {
             var invertedBindings = model.Binder.Bindings.ToLookup(kvp => kvp.Value, kvp => kvp.Key);
 
-            // ImportedSymbolsListItemSyntax indicates a reference to the import
+            // VariableAccessSyntax or ImportedFunctionSymbol indicates a reference to the import
             var unreferencedImports = model.Root.ImportedSymbols
                 .Where(sym => sym.NameSource.IsValid)
                 .Where(sym => !invertedBindings[sym].Any(x => x != sym.DeclaringSyntax));
@@ -37,11 +36,23 @@ namespace Bicep.Core.Analyzers.Linter.Rules
             {
                 yield return CreateRemoveUnusedDiagnosticForSpan(diagnosticLevel, import.Name, import.NameSource.Span, import.DeclaringSyntax, model.SourceFile.ProgramSyntax);
             }
+
+            //Handle wildcard alias
+
+            // VariableAccessSyntax indicates a reference to the import
+            var unreferencedWildcardImports = model.Root.WildcardImports
+                .Where(sym => sym.NameSource.IsValid)
+                .Where(sym => !invertedBindings[sym].Any(x => x != sym.DeclaringSyntax));
+
+            foreach (var import in unreferencedWildcardImports)
+            {
+                yield return CreateRemoveUnusedDiagnosticForSpan(diagnosticLevel, import.Name, import.NameSource.Span, import.DeclaringSyntax, model.SourceFile.ProgramSyntax);
+            }
         }
 
         override protected string GetCodeFixDescription(string name)
         {
-            return $"Remove import parameter {name}";
+            return $"Remove import {name}";
         }
     }
 }
