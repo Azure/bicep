b4f257ff0c161623b12a9a9a4bd79d785688b43a
Kyle Albert <5498623+kalbert312@users.noreply.github.com>
2025-06-20 18:03:24 -0400

    Structure the data a bit more to make mocking easier.

--- src/Bicep.Core.UnitTests/Utils/ExtensionTestHelper.cs
+++ src/Bicep.Core.UnitTests/Utils/ExtensionTestHelper.cs
@@ -6,10 +6,29 @@ using System.IO.Abstractions.TestingHelpers;
 using Bicep.Core.Registry;
 using Bicep.Core.Registry.Oci;
 using Bicep.Core.UnitTests.Features;
+using Bicep.IO.Abstraction;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.WindowsAzure.ResourceStack.Common.Extensions;
 using static Bicep.Core.UnitTests.Utils.RegistryHelper;
 
 namespace Bicep.Core.UnitTests.Utils;
+
+public record RegistrySourcedExtensionMockData(
+    string Name,
+    string Version,
+    string RepoVersion,
+    string TypesJson)
+{
+    public string RegistryScheme => "br";
+
+    public string Registry => LanguageConstants.BicepPublicMcrRegistry;
+
+    public string RepoPath => $"bicep/extensions/{Name}/{RepoVersion}";
+
+    public string ExtensionRepoReference => $"{RegistryScheme}:{Registry}/{RepoPath}:{Version}";
+
+    public RepoDescriptor ToRepoDescriptor() => new(Registry, RepoPath, ["tag"]);
+}
 
 public static class ExtensionTestHelper
 {
@@ -45,15 +64,16 @@ public static class ExtensionTestHelper
         return services;
     }
 
+    public static RegistrySourcedExtensionMockData CreateMockExtension(string extName, string extVersion, string repoVersion = "v1")
+        => new(extName, extVersion, repoVersion, ExtensionTestHelper.CreateTypesJson(extName, extVersion));
+
     public static async Task<ServiceBuilder> AddMockMsGraphExtension(ServiceBuilder services, TestContext testContext)
     {
         var indexJsonBeta = FileHelper.SaveResultFile(testContext, "types/index-beta.json", BicepTestConstants.GetMsGraphIndexJson(BicepTestConstants.MsGraphVersionBeta));
         var indexJsonV10 = FileHelper.SaveResultFile(testContext, "types/index-v1.0.json", BicepTestConstants.GetMsGraphIndexJson(BicepTestConstants.MsGraphVersionV10));
 
-        var cacheRoot = FileHelper.GetCacheRootDirectory(testContext).EnsureExists();
-
         services = services
-            .WithFeaturesOverridden(f => f with { CacheRootDirectory = cacheRoot })
+            .WithFeaturesOverridden(f => f with { CacheRootDirectory = GetCacheRootDirectory(testContext) })
             .WithContainerRegistryClientFactory(RegistryHelper.CreateOciClientForMsGraphExtension());
 
         await RegistryHelper.PublishMsGraphExtension(services.Build(), indexJsonBeta, "beta", BicepTestConstants.MsGraphVersionBeta);
@@ -61,4 +81,44 @@ public static class ExtensionTestHelper
 
         return services;
     }
+
+    public static async Task<ServiceBuilder> AddMockExtensions(ServiceBuilder services, TestContext testContext, IEnumerable<RegistrySourcedExtensionMockData> extensionMocks)
+    {
+        var clientFactory = RegistryHelper.CreateMockRegistryClient(
+            extensionMocks.Select(ext => ext.ToRepoDescriptor()).ToArray());
+
+        services = services
+            .WithFeaturesOverridden(f => f with { CacheRootDirectory = ExtensionTestHelper.GetCacheRootDirectory(testContext) })
+            .WithContainerRegistryClientFactory(clientFactory);
+
+        await extensionMocks.ExecuteOperationsConcurrently(ext => ExtensionTestHelper.AddMockExtension(services, testContext, ext), 4);
+
+        return services;
+    }
+
+    public static async Task<ServiceBuilder> AddMockExtension(ServiceBuilder services, TestContext testContext, RegistrySourcedExtensionMockData registrySourcedExtensionMockData)
+    {
+        var indexJsonPath = FileHelper.SaveResultFile(testContext, $"types/index-{registrySourcedExtensionMockData.Version}.json", registrySourcedExtensionMockData.TypesJson);
+
+        await PublishExtensionToRegistryAsync(services.Build(), indexJsonPath, registrySourcedExtensionMockData.ExtensionRepoReference);
+
+        return services;
+    }
+
+    public static IDirectoryHandle GetCacheRootDirectory(TestContext testContext) => FileHelper.GetCacheRootDirectory(testContext).EnsureExists();
+
+    // TODO(kylealbert): types
+    public static string CreateTypesJson(string extName, string extVersion) =>
+        // language=JSON
+        $$"""
+          {
+            "resources": {},
+            "resourceFunctions": {},
+            "settings": {
+              "name": "{{extName}}",
+              "version": "{{extVersion}}",
+              "isSingleton": false
+            }
+          }
+          """;
 }
