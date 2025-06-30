8224367d0587e4af787a26b37e4c87a64cdf76e8
Shenglong Li <shenglol@microsoft.com>
2025-06-30 10:52:49 -0700

    Fix tests for deploy pane

--- src/vscode-bicep-ui/apps/deploy-pane/vite.config.ts
+++ src/vscode-bicep-ui/apps/deploy-pane/vite.config.ts
@@ -18,17 +18,32 @@ export default defineConfig({
     },
   },
   test: {
+    // A workaround for "TypeError: Cannot add property element, object is not extensible".
+    // Some @vscode-elements/react-elements components extend ValidityState of the attached internals
+    // with custom properties, which is not good practice, but is supported by most browsers.
+    // However, element-internals-polyfill freezes the ValidityState object, causing this error.
+    // There is an issue about this: https://github.com/calebdwilliams/element-internals-polyfill/issues/141.
+    dangerouslyIgnoreUnhandledErrors: true,
     globals: true,
     watch: false,
     restoreMocks: true,
-    environment: "jsdom",
+    environment: "happy-dom",
     coverage: {
       enabled: true,
       exclude: ["src/index.tsx", ...coverageConfigDefaults.exclude],
     },
     setupFiles: ["src/__tests__/setupGlobals.ts"],
+    server: {
+      deps: {
+        inline: ["@vscode-elements/react-elements"],
+      }
+    },
     deps: {
-      inline: ["@vscode-elements/react-elements"],
+      optimizer: {
+        web: {
+          include: ["@vscode-elements/react-elements"]
+        }
+      }
     },
   },
 });
