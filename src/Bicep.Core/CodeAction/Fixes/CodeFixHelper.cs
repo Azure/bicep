ff225582ba3bbbfaa8dad06c86aff81c9cf17930
Kyle Albert <5498623+kalbert312@users.noreply.github.com>
2025-07-03 12:20:13 -0400

    Smarter code insertion for missing ext config assignments.

--- src/Bicep.Core/CodeAction/Fixes/CodeFixHelper.cs
+++ src/Bicep.Core/CodeAction/Fixes/CodeFixHelper.cs
@@ -56,21 +56,8 @@ public static class CodeFixHelper
 
     public static CodeFix GetCodeFixForMissingBicepExtensionConfigAssignments(ProgramSyntax program, IReadOnlyList<(string Alias, ObjectLikeType ExpectedConfigType)> missingExtAliasesWithExpectedType)
     {
-        var terminatingNewlines = program.Children.LastOrDefault() is Token { Type: TokenType.NewLine } newLineToken ? StringUtils.CountNewlines(newLineToken.Text) : 0;
-
-        var newSyntaxList = new List<SyntaxBase>();
-
-        var precedingNewlineSyntax = terminatingNewlines switch
-        {
-            0 => SyntaxFactory.DoubleNewlineToken,
-            1 => SyntaxFactory.NewlineToken,
-            _ => null,
-        };
-
-        if (precedingNewlineSyntax is { })
-        {
-            newSyntaxList.Add(precedingNewlineSyntax);
-        }
+        // Create the new syntax
+        var newSyntaxList = new List<SyntaxBase>(missingExtAliasesWithExpectedType.Count * 2); // * 2 for new lines per new syntax
 
         for (var i = 0; i < missingExtAliasesWithExpectedType.Count; i++)
         {
@@ -82,6 +69,7 @@ public static class CodeFixHelper
                     => SyntaxFactory.CreateObject(
                         assignObjType.Properties
                             .Where(p => p.Value.Flags.HasFlag(TypePropertyFlags.Required))
+                            .OrderBy(p => p.Key)
                             .Select(p => SyntaxFactory.CreateObjectProperty(p.Key, SyntaxFactory.EmptySkippedTrivia))),
                 DiscriminatedObjectType discrimObjType
                     => SyntaxFactory.CreateObject([SyntaxFactory.CreateObjectProperty(discrimObjType.DiscriminatorKey, SyntaxFactory.EmptySkippedTrivia)]),
@@ -94,22 +82,58 @@ public static class CodeFixHelper
 
             newSyntaxList.Add(extConfigAssignmentSyntax);
 
-            var isLastEntry = i == missingExtAliasesWithExpectedType.Count - 1;
-            var trailingNewlineSyntax = isLastEntry switch
+            if (i != missingExtAliasesWithExpectedType.Count - 1)
             {
-                false => SyntaxFactory.DoubleNewlineToken,
-                true => SyntaxFactory.NewlineToken,
-            };
-
-            newSyntaxList.Add(trailingNewlineSyntax);
+                newSyntaxList.Add(SyntaxFactory.DoubleNewlineToken);
+            }
         }
 
-        var additionalProgram = new ProgramSyntax(newSyntaxList, SyntaxFactory.EndOfFileToken);
+        // Determine where the new syntax should go. Find the syntax we'll insert after and the proceeding syntax of it.
+        SyntaxBase insertAfterSyntax = program.EndOfFile;
+        SyntaxBase? existingSyntaxProceedingInsert = null;
+
+        for (var i = program.Children.Length - 1; i >= 0; --i)
+        {
+            var currentSyntax = program.Children[i];
+
+            if (currentSyntax is ExtensionConfigAssignmentSyntax) // Use last existing config assignment, if any.
+            {
+                insertAfterSyntax = currentSyntax;
+                existingSyntaxProceedingInsert = program.Children.ElementAtOrDefault(i + 1);
+
+                break;
+            }
+            else if (currentSyntax is UsingDeclarationSyntax) // otherwise, insert near the top of file by default if no existing site. Extension configs are more foundational for the deployment.
+            {
+                insertAfterSyntax = currentSyntax;
+                existingSyntaxProceedingInsert = program.Children.ElementAtOrDefault(i + 1);
+            }
+        }
+
+        // Determine spacing above.
+        newSyntaxList.Insert(0, SyntaxFactory.DoubleNewlineToken);
+
+        // Determine spacing below.
+        var numExistingProceedingNewLines = existingSyntaxProceedingInsert is Token { Type: TokenType.NewLine } proceedingNewLineToken
+            ? StringUtils.CountNewlines(proceedingNewLineToken.Text)
+            : object.ReferenceEquals(insertAfterSyntax, program.EndOfFile)
+                ? -1
+                : 0;
+
+        var newProceedingSyntax = numExistingProceedingNewLines switch
+        {
+            -1 => SyntaxFactory.EndOfFileToken,
+            0 => SyntaxFactory.DoubleNewlineToken,
+            _ => SyntaxFactory.NewlineToken,
+        };
+
+        // Make the code replacement.
+        var additionalProgram = new ProgramSyntax(newSyntaxList, newProceedingSyntax);
 
         return new(
             "Insert missing required extension configuration assignments",
             true,
             CodeFixKind.QuickFix,
-            new(program.EndOfFile.Span, additionalProgram.ToString()));
+            new(insertAfterSyntax.Span, additionalProgram.ToString()));
     }
 }
