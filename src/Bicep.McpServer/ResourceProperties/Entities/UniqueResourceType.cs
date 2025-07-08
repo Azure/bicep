487326871075bf576a612202b214139a73daeaf5
Gary Li <ligar@microsoft.com>
2025-07-08 11:10:48 -0400

    Use the existing ApiVersionComparer in Azure.Deployments.Core.Comparers

--- src/Bicep.McpServer/ResourceProperties/Entities/UniqueResourceType.cs
+++ src/Bicep.McpServer/ResourceProperties/Entities/UniqueResourceType.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT License.
 
 using System.Text.Json.Serialization;
+using Azure.Deployments.Core.Comparers;
 using Bicep.McpServer.ResourceProperties.Helpers;
 
 namespace Bicep.McpServer.ResourceProperties.Entities;
