204ab2cfd853995f79a47b2f9f2a83d9b00c0fe1
Anthony Martin <38542602+anthony-c-martin@users.noreply.github.com>
2025-10-06 07:28:22 -0400

    Use PipeReader & PipeWriter APIs

--- src/Bicep.RpcClient/JsonRpc/JsonRpcClient.cs
+++ src/Bicep.RpcClient/JsonRpc/JsonRpcClient.cs
@@ -1,17 +1,23 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+using System;
+using System.Buffers;
 using System.Collections.Concurrent;
-using System.Net.Security;
+using System.IO;
+using System.IO.Pipelines;
+using System.Linq;
 using System.Text;
 using System.Text.Encodings.Web;
 using System.Text.Json;
 using System.Text.Json.Nodes;
 using System.Text.Json.Serialization;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Bicep.RpcClient.JsonRpc;
 
-internal class JsonRpcClient(Stream reader, Stream writer) : IDisposable
+internal class JsonRpcClient(PipeReader reader, PipeWriter writer) : IDisposable
 {
     private record JsonRpcRequest<T>(
         string Jsonrpc,
@@ -20,7 +26,6 @@ internal class JsonRpcClient(Stream reader, Stream writer) : IDisposable
         int Id);
 
     private record MinimalJsonRpcResponse(
-        string Jsonrpc,
         int Id);
 
     private record JsonRpcResponse<T>(
@@ -34,10 +39,9 @@ internal class JsonRpcClient(Stream reader, Stream writer) : IDisposable
         string Message,
         JsonNode? Data);
 
-    private readonly byte[] terminator = "\r\n\r\n"u8.ToArray();
     private int nextId = 0;
     private readonly SemaphoreSlim writeSemaphore = new(1, 1);
-    private readonly ConcurrentDictionary<int, TaskCompletionSource<string>> pendingResponses = new();
+    private readonly ConcurrentDictionary<int, TaskCompletionSource<byte[]>> pendingResponses = new();
 
     private readonly JsonSerializerOptions jsonSerializerOptions = new()
     {
@@ -55,12 +59,12 @@ internal class JsonRpcClient(Stream reader, Stream writer) : IDisposable
         var requestContent = JsonSerializer.Serialize(jsonRpcRequest, jsonSerializerOptions);
         var requestLength = Encoding.UTF8.GetByteCount(requestContent);
         var rawRequest = $"Content-Length: {requestLength}\r\n\r\n{requestContent}";
-        var requestBytes = Encoding.UTF8.GetBytes(rawRequest);
 
         await writeSemaphore.WaitAsync(cancellationToken).ConfigureAwait(false);
         try
         {
-            await writer.WriteAsync(requestBytes, 0, requestBytes.Length, cancellationToken).ConfigureAwait(false);
+            var requestBytes = Encoding.UTF8.GetBytes(rawRequest).AsMemory();
+            await writer.WriteAsync(requestBytes, cancellationToken).ConfigureAwait(false);
             await writer.FlushAsync(cancellationToken).ConfigureAwait(false);
         }
         finally
@@ -68,7 +72,7 @@ internal class JsonRpcClient(Stream reader, Stream writer) : IDisposable
             writeSemaphore.Release();
         }
 
-        var tcs = new TaskCompletionSource<string>(TaskCreationOptions.RunContinuationsAsynchronously);
+        var tcs = new TaskCompletionSource<byte[]>(TaskCreationOptions.RunContinuationsAsynchronously);
         if (!pendingResponses.TryAdd(currentId, tcs))
         {
             throw new InvalidOperationException($"A request with ID {currentId} is already pending.");
@@ -87,13 +91,34 @@ internal class JsonRpcClient(Stream reader, Stream writer) : IDisposable
         return jsonRpcResponse.Result;
     }
 
-    public async Task ReadLoop(CancellationToken cancellationToken)
+    public Task Listen(Action onComplete, CancellationToken cancellationToken)
+        => Task.Run(async () =>
+        {
+            try
+            {
+                await ListenInternal(cancellationToken).ConfigureAwait(false);
+            }
+            catch (OperationCanceledException)
+            {
+                // Expected when disposing
+            }
+            finally
+            {
+                onComplete();
+            }
+        }, cancellationToken);
+
+    private async Task ListenInternal(CancellationToken cancellationToken)
     {
         while (true)
         {
             try
             {
                 var message = await ReadMessage(cancellationToken).ConfigureAwait(false);
+                if (message is null)
+                {
+                    return;
+                }
 
                 var response = JsonSerializer.Deserialize<MinimalJsonRpcResponse>(message, jsonSerializerOptions)
                     ?? throw new InvalidOperationException("Failed to deserialize JSON-RPC response");
@@ -105,77 +130,134 @@ internal class JsonRpcClient(Stream reader, Stream writer) : IDisposable
             }
             catch (Exception) when (cancellationToken.IsCancellationRequested)
             {
-                reader.Dispose();
-                writer.Dispose();
+                await reader.CompleteAsync().ConfigureAwait(false);
+                await writer.CompleteAsync().ConfigureAwait(false);
                 break;
             }
         }
     }
 
-    private async Task<string> ReadUntilTerminator(CancellationToken cancellationToken)
-    {
-        using var outputStream = new MemoryStream();
-        var patternIndex = 0;
-        var byteBuffer = new byte[1];
+    private record Headers(
+        int ContentLength);
 
+    private async Task<Headers?> ReadHeaders(CancellationToken cancellationToken)
+    {
+        int? contentLength = null;
         while (true)
         {
-            await ReadExactly(byteBuffer, byteBuffer.Length, cancellationToken).ConfigureAwait(false);
+            var readResult = await reader.ReadAsync(cancellationToken).ConfigureAwait(false);
 
-            await outputStream.WriteAsync(byteBuffer, 0, byteBuffer.Length, cancellationToken).ConfigureAwait(false);
-            patternIndex = terminator[patternIndex] == byteBuffer[0] ? patternIndex + 1 : 0;
-            if (patternIndex == terminator.Length)
+            if (readResult.Buffer.IsEmpty && readResult.IsCompleted)
             {
-                outputStream.Position = 0;
-                outputStream.SetLength(outputStream.Length - terminator.Length);
-                // return stream as string
-                return Encoding.UTF8.GetString(outputStream.ToArray());
-            }
-        }
-    }
-
-    private async Task<string> ReadContent(int length, CancellationToken cancellationToken)
-    {
-        var byteBuffer = new byte[length];
-        await ReadExactly(byteBuffer, length, cancellationToken).ConfigureAwait(false);
-
-        return Encoding.UTF8.GetString(byteBuffer);
-    }
-
-    private async Task<string> ReadMessage(CancellationToken cancellationToken)
-    {
-        var header = await ReadUntilTerminator(cancellationToken).ConfigureAwait(false);
-        var parsed = header.Split(':').Select(x => x.Trim()).ToArray();
-
-        if (parsed.Length != 2 ||
-            !parsed[0].Equals("Content-Length", StringComparison.OrdinalIgnoreCase) ||
-            !int.TryParse(parsed[1], out var contentLength) ||
-            contentLength <= 0)
-        {
-            throw new InvalidOperationException($"Invalid header: {header}");
-        }
-
-        return await ReadContent(contentLength, cancellationToken).ConfigureAwait(false);
-    }
-
-    private async ValueTask ReadExactly(byte[] buffer, int minimumBytes, CancellationToken cancellationToken)
-    {
-        var totalRead = 0;
-        while (totalRead < minimumBytes)
-        {
-            var read = await reader.ReadAsync(buffer, totalRead, minimumBytes - totalRead, cancellationToken).ConfigureAwait(false);
-            if (read == 0)
-            {
-                throw new EndOfStreamException("Stream closed before reading expected number of bytes");
+                return null; // remote end disconnected at a reasonable place.
             }
 
-            totalRead += read;
+            var lf = readResult.Buffer.PositionOf((byte)'\n');
+            if (!lf.HasValue)
+            {
+                if (readResult.IsCompleted)
+                {
+                    throw new EndOfStreamException();
+                }
+
+                // Indicate that we can't find what we're looking for and read again.
+                reader.AdvanceTo(readResult.Buffer.Start, readResult.Buffer.End);
+                continue;
+            }
+
+            var line = readResult.Buffer.Slice(0, lf.Value);
+
+            // Verify the line ends with an \r (that precedes the \n we already found)
+            var cr = line.PositionOf((byte)'\r');
+            if (!cr.HasValue || !line.GetPosition(1, cr.Value).Equals(lf))
+            {
+                throw new InvalidOperationException("Header does not end with expected \r\n character sequence");
+            }
+
+            // Trim off the \r now that we confirmed it was there.
+            line = line.Slice(0, line.Length - 1);
+
+            if (line.Length > 0)
+            {
+                var lineText = Encoding.UTF8.GetString(line.ToArray());
+                var split = lineText.Split([':'], 2);
+                if (split.Length != 2)
+                {
+                    throw new InvalidOperationException("Colon not found in header.");
+                }
+
+                var headerName = split[0].Trim();
+                var headerValue = split[1].Trim();
+
+                if (headerName == "Content-Length")
+                {
+                    contentLength = int.Parse(headerValue);
+                }
+            }
+
+            // Advance to the next line.
+            reader.AdvanceTo(readResult.Buffer.GetPosition(1, lf.Value));
+
+            if (line.Length == 0)
+            {
+                // We found the empty line that constitutes the end of the HTTP headers.
+                break;
+            }
         }
+
+        if (!contentLength.HasValue)
+        {
+            throw new InvalidOperationException("Failed to obtain Content-Length header");
+        }
+
+        return new(contentLength.Value);
+    }
+
+    protected async ValueTask<ReadResult> ReadAtLeastAsync(int requiredBytes, bool allowEmpty, CancellationToken cancellationToken)
+    {
+        var readResult = await reader.ReadAsync(cancellationToken).ConfigureAwait(false);
+        while (readResult.Buffer.Length < requiredBytes && !readResult.IsCompleted && !readResult.IsCanceled)
+        {
+            reader.AdvanceTo(readResult.Buffer.Start, readResult.Buffer.End);
+            readResult = await reader.ReadAsync(cancellationToken).ConfigureAwait(false);
+        }
+
+        if (allowEmpty && readResult.Buffer.Length == 0)
+        {
+            return readResult;
+        }
+
+        if (readResult.Buffer.Length < requiredBytes)
+        {
+            throw readResult.IsCompleted ? new EndOfStreamException() :
+                readResult.IsCanceled ? new OperationCanceledException() :
+                throw new InvalidOperationException(); // should be unreachable
+        }
+
+        return readResult;
+    }
+
+    private async Task<byte[]?> ReadMessage(CancellationToken cancellationToken)
+    {
+        var headers = await ReadHeaders(cancellationToken).ConfigureAwait(false);
+        if (headers is null)
+        {
+            return null;
+        }
+
+        var readResult = await ReadAtLeastAsync(headers.ContentLength, allowEmpty: false, cancellationToken).ConfigureAwait(false);
+
+        var contentBuffer = readResult.Buffer.Slice(0, headers.ContentLength);
+        var output = contentBuffer.ToArray();
+
+        reader.AdvanceTo(contentBuffer.End);
+
+        return output;
     }
 
     public void Dispose()
     {
-        writer.Dispose();
-        reader.Dispose();
+        writer.Complete();
+        reader.Complete();
     }
 }
