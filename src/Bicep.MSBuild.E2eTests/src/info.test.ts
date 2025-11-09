6eb0ab74dd1747d1af3208d89a9747160ad7f684
Bicep Automation <bicep@noreply.github.com>
2025-11-09 04:40:49 +0000

    Run code formatter

--- src/Bicep.MSBuild.E2eTests/src/info.test.ts
+++ src/Bicep.MSBuild.E2eTests/src/info.test.ts
@@ -12,10 +12,10 @@ describe("msbuild", () => {
 
     const result = example.build(false);
     asserts.expectLinesInLog(result.stdout, [
-          '1 Warning(s)',
-          '0 Error(s)',
-          'main.bicep(3,5): warning no-unused-vars: Variable "unused" is declared but never used.',
-        ]);
+      "1 Warning(s)",
+      "0 Error(s)",
+      'main.bicep(3,5): warning no-unused-vars: Variable "unused" is declared but never used.',
+    ]);
   });
 
   it("should promote info diagnostic to error", () => {
@@ -23,9 +23,6 @@ describe("msbuild", () => {
     example.cleanProjectDir();
 
     const result = example.build(false);
-    asserts.expectLinesInLog(result.stdout, [
-          '0 Warning(s)',
-          '0 Error(s)',
-        ]);
+    asserts.expectLinesInLog(result.stdout, ["0 Warning(s)", "0 Error(s)"]);
   });
 });
