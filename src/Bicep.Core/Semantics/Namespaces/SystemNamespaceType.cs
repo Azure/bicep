2f3624a24b95df9b1fc5438de5d1920e5b35efa2
Jonny Eskew <jeskew@users.noreply.github.com>
2025-11-14 12:18:19 -0500

    Use integer and boolean literal types when deriving return type for `loadJsonContent` (#18466)
    Resolves #18416

--- src/Bicep.Core/Semantics/Namespaces/SystemNamespaceType.cs
+++ src/Bicep.Core/Semantics/Namespaces/SystemNamespaceType.cs
@@ -5,6 +5,7 @@ using System.Collections.Immutable;
 using System.Globalization;
 using System.Numerics;
 using System.Text;
+using Azure.Deployments.Expression.Extensions;
 using Bicep.Core.Analyzers.Linter;
 using Bicep.Core.Diagnostics;
 using Bicep.Core.Extensions;
@@ -1666,10 +1667,10 @@ namespace Bicep.Core.Semantics.Namespaces
                 JValue value => value.Type switch
                 {
                     JTokenType.String => TypeFactory.CreateStringLiteralType(value.ToString(CultureInfo.InvariantCulture)),
-                    JTokenType.Integer => LanguageConstants.Int,
+                    JTokenType.Integer => TypeFactory.CreateIntegerLiteralType(value.ToLong()),
                     // Floats are currently not supported in Bicep, so fall back to the default behavior of "any"
                     JTokenType.Float => LanguageConstants.Any,
-                    JTokenType.Boolean => LanguageConstants.Bool,
+                    JTokenType.Boolean => TypeFactory.CreateBooleanLiteralType(value.ToObject<bool>()),
                     JTokenType.Null => LanguageConstants.Null,
                     _ => LanguageConstants.Any,
                 },
