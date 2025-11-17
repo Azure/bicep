24d1194b5b656371d54c2993e0f7a86f3fca1815
Engin Polat <118744+polatengin@users.noreply.github.com>
2025-11-17 16:17:11 +0000

    fix and add tests for quoted property names and enhance symbol resolution

--- src/Bicep.Core.UnitTests/Semantics/SymbolContextTests.cs
+++ src/Bicep.Core.UnitTests/Semantics/SymbolContextTests.cs
@@ -1,6 +1,8 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 using System.Collections.Immutable;
+using Bicep.Core.Navigation;
+using Bicep.Core.Parsing;
 using Bicep.Core.Semantics;
 using Bicep.Core.Syntax;
 using Bicep.Core.UnitTests.Utils;
@@ -33,6 +35,39 @@ namespace Bicep.Core.UnitTests.Semantics
             context.Unlock();
             context.TypeManager.Should().NotBeNull();
             context.Binder.Should().NotBeNull();
+        }
+
+        [TestMethod]
+        public void TestHoverOnQuotedPropertyReturnsPropertySymbol()
+        {
+            var text = @"
+resource storage 'Microsoft.Storage/storageAccounts@2021-02-01' = {
+  name: 'mystorageaccount'
+  location: 'eastus'
+  sku: {
+    'name': 'Standard_LRS'
+  }
+}
+";
+            var skuIndex = text.IndexOf("sku:");
+            var namePropertyIndex = text.IndexOf("'name'", skuIndex);
+            var cursor = namePropertyIndex + 2;
+
+            var compilation = CompilationHelper.Compile(text).Compilation;
+            var model = compilation.GetEntrypointSemanticModel();
+
+            var programSyntax = model.SourceFile.ProgramSyntax;
+
+            var node = programSyntax.TryFindMostSpecificNodeInclusive(
+                cursor,
+                n => n is not IdentifierSyntax && n is not Token && n is not AliasAsClauseSyntax);
+
+            node.Should().NotBeNull($"cursor at position {cursor} should find a node");
+            var symbol = model.GetSymbolInfo(node!);
+
+            symbol.Should().NotBeNull("hovering over a quoted property name should return a symbol");
+            symbol.Should().BeOfType<PropertySymbol>("quoted property name should resolve to PropertySymbol");
+            symbol!.Name.Should().Be("name");
         }
     }
 }
