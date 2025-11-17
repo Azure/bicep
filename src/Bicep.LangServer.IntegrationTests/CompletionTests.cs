def18c5c458096e84a6c41b816a93bfbafcfd8b5
Tate Smalligan <161764628+tsmallig33@users.noreply.github.com>
2025-11-17 16:04:32 -0600

    Add this.exists() and this.existingResource() as an experimental feature (#17727)
    ## Description
    
    Adds this.exists() and this.existingResource() for use in resource properties. `This` namespace is
    scoped as a local symbol to resource bodies
    
    ## Example Usage
    
    ```bicep
    resource storageAccount 'Microsoft.Storage/storageAccounts@2021-09-01' = {
      name: 'mystorageaccount'
      location: 'eastus'
      sku: {
        name: 'Standard_LRS'
      }
      kind: 'StorageV2'
      properties: {
        minimumTlsVersion: 'TLS1_2'
        publicNetworkAccess: this.exists() ? 'Enabled' : 'Disabled'
      }
    }
    ```
    
    ## Checklist
    
    - [X] I have read and adhere to the [contribution
    guide](https://github.com/Azure/bicep/blob/main/CONTRIBUTING.md).
    
    ###### Microsoft Reviewers: [Open in
    CodeFlow](https://microsoft.github.io/open-pr/?codeflow=https://github.com/Azure/bicep/pull/17727)

--- src/Bicep.LangServer.IntegrationTests/CompletionTests.cs
+++ src/Bicep.LangServer.IntegrationTests/CompletionTests.cs
@@ -5852,6 +5852,41 @@ output people Person[] = [{
         }
 
         [TestMethod]
+        public async Task Write_only_properties_are_not_offered_as_completions_this_namespace()
+        {
+            var customTypes = new[] {
+                TestTypeHelper.CreateCustomResourceTypeWithTopLevelProperties("My.Rp/myType", "2020-01-01", TypeSymbolValidationFlags.Default, null, [
+                    new NamedTypeProperty("required", LanguageConstants.String, TypePropertyFlags.Required),
+                    new NamedTypeProperty("readOnlyRequired", LanguageConstants.String, TypePropertyFlags.ReadOnly | TypePropertyFlags.Required),
+                    new NamedTypeProperty("writeOnly", LanguageConstants.String, TypePropertyFlags.WriteOnly),
+                ]),
+            };
+
+            var (text, cursor) = ParserHelper.GetFileWithSingleCursor("""
+            resource myRes 'My.Rp/myType@2020-01-01' = {
+              name: 'foo'
+              properties: {
+                required: this.existingResource().?properties.|
+              }
+            }
+            """);
+
+            var bicepFile = new LanguageClientFile(InMemoryFileResolver.GetFileUri("/path/to/main.bicep"), text);
+            using var helper = await LanguageServerHelper.StartServerWithText(
+                TestContext,
+                text,
+                bicepFile.Uri,
+                services => services.WithFeatureOverrides(new(ThisNamespaceEnabled: true)).WithAzResources(customTypes));
+
+            var file = new FileRequestHelper(helper.Client, bicepFile);
+            var completions = await file.RequestAndResolveCompletions(cursor);
+
+            completions.Should().Contain(x => x.Label == "required");
+            completions.Should().Contain(x => x.Label == "readOnlyRequired");
+            completions.Should().NotContain(x => x.Label == "writeOnly");
+        }
+
+        [TestMethod]
         public async Task Identity_property_completions_are_offered_for_resource()
         {
             // Resource identity property completion
