85df11077b8567fa44742e46b6c5b4ada484efb6
Andy Jordan <2226434+andyleejordan@users.noreply.github.com>
2025-11-19 12:37:44 -0800

    Add DSC unknown resource test

--- src/Bicep.Core.IntegrationTests/Extensibility/DesiredStateConfigurationCompatibilityTests.cs
+++ src/Bicep.Core.IntegrationTests/Extensibility/DesiredStateConfigurationCompatibilityTests.cs
@@ -4,6 +4,7 @@
 using System.IO.Abstractions;
 using System.IO.Abstractions.TestingHelpers;
 using Bicep.Core.Configuration;
+using Bicep.Core.Diagnostics;
 using Bicep.Core.UnitTests;
 using Bicep.Core.UnitTests.Assertions;
 using Bicep.Core.UnitTests.Features;
@@ -43,6 +44,10 @@ public class DesiredStateConfigurationCompatibilityTests
     {
         var services = await GetServicesWithPrepublishedTypes();
 
+        // TODO: This works under test without local deploy, but interactively
+        // we seem to need to use that because of a difference in
+        // expected/allowed scopes. That is, the mock type has `ScopeType.All`
+        // but the real example has to (ironically) set `targetScope = 'local'`.
         var result = await CompilationHelper.RestoreAndCompile(services, $$"""
 extension 'br:{{ExtensionReference}}'
 
@@ -52,7 +57,7 @@ resource myEcho 'Microsoft.DSC.Debug/Echo@1.0.0' = {
 }
 """);
 
-        result.ExcludingLinterDiagnostics().Should().NotHaveAnyDiagnostics();
+        result.Should().NotHaveAnyDiagnostics();
         result.Template.Should().NotBeNull();
         result.Template.Should().DeepEqual(JToken.Parse("""
 {
@@ -85,5 +90,26 @@ resource myEcho 'Microsoft.DSC.Debug/Echo@1.0.0' = {
   }
 }
 """));
+    }
+
+    [TestMethod]
+    public async Task DesiredStateConfiguration_unknown_resource_works()
+    {
+        var services = await GetServicesWithPrepublishedTypes();
+
+        var result = await CompilationHelper.RestoreAndCompile(services, $$"""
+extension 'br:{{ExtensionReference}}'
+
+resource myResource 'Foo/Bar@1.0.0' = {
+    field: 'value'
+    okay: true
+}
+""");
+
+        // TODO: This is the current behavior, but the goal is to workaround this error by adding some sort of "wildcard" logic so that unknown DSC resources (using SemVer) are usable.
+        result.Should().NotGenerateATemplate();
+        result.Should().HaveDiagnostics(new[]{
+            ("BCP029", DiagnosticLevel.Error, "The resource type is not valid. Specify a valid resource type of format \"<type-name>@<apiVersion>\"."),
+        });
     }
 }
