10ac80935018c9f44e53e9b128e080fd82a7fbaf
Engin Polat <118744+polatengin@users.noreply.github.com>
2026-03-02 17:54:43 +0000

    improve completion item documentation with detailed display names, paths, versions, and descriptions

--- src/Bicep.LangServer.IntegrationTests/CompletionTests.cs
+++ src/Bicep.LangServer.IntegrationTests/CompletionTests.cs
@@ -4390,7 +4390,7 @@ var file = " + functionName + @"(templ|)
                     first.SortText.Should().Be("0000");
                     first.Kind.Should().Be(CompletionItemKind.Snippet);
                     first.Detail.Should().Be("d1");
-                    first.Documentation!.MarkupContent!.Value.Should().Be("[View Documentation](contoso.com/help1)");
+                    first.Documentation!.MarkupContent!.Value.Should().Be("**Display name:** 1.0.2  \n**Full module path:** bicep/app/dapr-containerapp  \n**Version:** 1.0.2  \n**Description:** d1  \n[View Documentation](contoso.com/help1)");
                 },
                 second =>
                 {
@@ -4398,7 +4398,7 @@ var file = " + functionName + @"(templ|)
                     second.SortText.Should().Be("0001");
                     second.Kind.Should().Be(CompletionItemKind.Snippet);
                     second.Detail.Should().BeNull();
-                    second.Documentation.Should().BeNull();
+                    second.Documentation!.MarkupContent!.Value.Should().Be("**Display name:** 1.0.1  \n**Full module path:** bicep/app/dapr-containerapp  \n**Version:** 1.0.1  \n**Description:** N/A  \n**Documentation:** N/A");
                 }
             );
         }
@@ -4463,7 +4463,7 @@ var file = " + functionName + @"(templ|)
                     first.SortText.Should().Be("0000");
                     first.Kind.Should().Be(CompletionItemKind.Snippet);
                     first.Detail.Should().Be("d101");
-                    first.Documentation!.MarkupContent!.Value.Should().Be("[View Documentation](contoso.com/help/d101.html)");
+                    first.Documentation!.MarkupContent!.Value.Should().Be("**Display name:** v101  \n**Full module path:** app/private-app  \n**Version:** v101  \n**Description:** d101  \n[View Documentation](contoso.com/help/d101.html)");
                 },
                 second =>
                 {
@@ -4471,7 +4471,7 @@ var file = " + functionName + @"(templ|)
                     second.SortText.Should().Be("0001");
                     second.Kind.Should().Be(CompletionItemKind.Snippet);
                     second.Detail.Should().Be("d100");
-                    second.Documentation!.MarkupContent!.Value.Should().Be("[View Documentation](contoso.com/help/d100.html)");
+                    second.Documentation!.MarkupContent!.Value.Should().Be("**Display name:** v100  \n**Full module path:** app/private-app  \n**Version:** v100  \n**Description:** d100  \n[View Documentation](contoso.com/help/d100.html)");
                 }
             );
         }
