c25939a25798403f0e01975b3d6f010147fa8bbc
Anthony Martin <38542602+anthony-c-martin@users.noreply.github.com>
2026-05-11 07:42:50 -0400

    Commonize on eslint config

--- src/highlightjs/eslint.config.mjs
+++ src/highlightjs/eslint.config.mjs
@@ -5,37 +5,8 @@
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginJest from 'eslint-plugin-jest';
+import headers from "eslint-plugin-headers";
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
-
-const copyrightHeader = "// Copyright (c) Microsoft Corporation.\n// Licensed under the MIT License.";
-
-/** @type {import('eslint').Rule.RuleModule} */
-const copyrightNoticeRule = {
-  meta: {
-    type: "suggestion",
-    fixable: "code",
-    messages: {
-      missingCopyright: "File must start with the Microsoft copyright header.",
-    },
-  },
-  create(context) {
-    return {
-      Program(node) {
-        const sourceCode = context.sourceCode;
-        const text = sourceCode.getText();
-        if (!text.startsWith(copyrightHeader)) {
-          context.report({
-            node,
-            messageId: "missingCopyright",
-            fix(fixer) {
-              return fixer.insertTextBefore(node, copyrightHeader + "\n\n");
-            },
-          });
-        }
-      },
-    };
-  },
-};
 
 export default tseslint.config({
   files: ["src/**/*.ts", "test/**/*.ts", "test-live/**/*.ts"],
@@ -48,12 +19,15 @@ export default tseslint.config({
   languageOptions: {
     ecmaVersion: 2020,
   },
-  plugins: {
-    "copyright": {
-      rules: { notice: copyrightNoticeRule },
-    },
-  },
+  plugins: { headers },
   rules: {
-    "copyright/notice": "error",
+    "headers/header-format": [
+      "error",
+      {
+        source: "string",
+        style: "line",
+        content: "Copyright (c) Microsoft Corporation.\nLicensed under the MIT License.",
+      },
+    ],
   },
 });
