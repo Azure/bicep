0a52d72206793c0d484c7835a62f0ffadbf34d8c
willdavsmith <willdavsmith@gmail.com>
2026-07-08 13:52:02 -0700

    Fix Bicep.Wasm build: adapt WasmModuleRegistryProvider to new OciArtifactRegistry constructor
    The merge changed OciArtifactRegistry to take (RegistryConfiguration, IOciRegistryTransportFactory, IPublicModuleMetadataProvider, IFileExplorer, ILogger). WasmModuleRegistryProvider still used the old clientFactory-based signature, breaking the browser-wasm build (not part of Bicep.sln).

--- src/Bicep.Wasm/WasmModuleRegistryProvider.cs
+++ src/Bicep.Wasm/WasmModuleRegistryProvider.cs
@@ -4,7 +4,10 @@
 using Bicep.Core.Configuration;
 using Bicep.Core.Registry;
 using Bicep.Core.Registry.Catalog;
+using Bicep.Core.Registry.Oci;
 using Bicep.IO.Abstraction;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
 
 namespace Bicep.Wasm
 {
@@ -13,11 +16,12 @@ namespace Bicep.Wasm
         public WasmModuleRegistryProvider(
             RegistryConfiguration registryConfiguration,
             IPublicModuleMetadataProvider publicModuleMetadataProvider,
-            IContainerRegistryClientFactory clientFactory,
-            IFileExplorer fileExplorer)
+            IOciRegistryTransportFactory transportFactory,
+            IFileExplorer fileExplorer,
+            ILogger<OciArtifactRegistry>? logger = null)
             : base([
                 new LocalModuleRegistry(),
-                new OciArtifactRegistry(registryConfiguration, clientFactory, publicModuleMetadataProvider, fileExplorer),
+                new OciArtifactRegistry(registryConfiguration, transportFactory, publicModuleMetadataProvider, fileExplorer, logger ?? NullLogger<OciArtifactRegistry>.Instance),
             ])
         {
         }
