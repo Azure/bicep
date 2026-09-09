fff8e571d63fa53863fbf944009f019b9793496c
Brendan Burns <5751682+brendandburns@users.noreply.github.com>
2026-09-09 10:21:14 -0700

    Fix content escaping: correct verbatim string quotes in ValidateCommandTests.cs

--- src/Bicep.RegistryModuleTool.IntegrationTests/Commands/ValidateCommandTests.cs
+++ src/Bicep.RegistryModuleTool.IntegrationTests/Commands/ValidateCommandTests.cs
@@ -48,24 +48,24 @@ namespace Bicep.RegistryModuleTool.IntegrationTests.Commands
             var testFile = MainBicepTestFile.Open(fileSystem);
             var console = new MockConsole().ExpectErrorLines(StringUtils.SplitOnNewLine(
                 $"""
-                The file \"{fileSystem.Path.GetFullPath(MainBicepFile.FileName)}\" is invalid:
-                  - A description must be specified for parameter \"dnsPrefix\".
-                  - A description must be specified for parameter \"servicePrincipalClientSecret\".
-                  - A description must be specified for output \"controlPlaneFQDN\".
-                  - Metadata \"description\" must contain at least 10 characters.
+                The file "{fileSystem.Path.GetFullPath(MainBicepFile.FileName)}" is invalid:
+                  - A description must be specified for parameter "dnsPrefix".
+                  - A description must be specified for parameter "servicePrincipalClientSecret".
+                  - A description must be specified for output "controlPlaneFQDN".
+                  - Metadata "description" must contain at least 10 characters.
 
-                The file \"{testFile.Path}\" is invalid:
+                The file "{testFile.Path}" is invalid:
                   - Could not find tests in the file. Please make sure to add at least one module referencing the main Bicep file.
 
-                The file \"{fileSystem.Path.GetFullPath(MainArmTemplateFile.FileName)}\" is invalid:
-                  - The file is modified or outdated. Please run \"brm generate\" to regenerate it.
+                The file "{fileSystem.Path.GetFullPath(MainArmTemplateFile.FileName)}" is invalid:
+                  - The file is modified or outdated. Please run "brm generate" to regenerate it.
 
-                The file \"{fileSystem.Path.GetFullPath(ReadmeFile.FileName)}\" is invalid:
-                  - The file is modified or outdated. Please run \"brm generate\" to regenerate it.
+                The file "{fileSystem.Path.GetFullPath(ReadmeFile.FileName)}" is invalid:
+                  - The file is modified or outdated. Please run "brm generate" to regenerate it.
 
-                The file \"{fileSystem.Path.GetFullPath(VersionFile.FileName)}\" is invalid:
-                  - #: Required properties [\"$schema\",\"version\"] are not present.
-                  - The file is modified or outdated. Please run \"brm generate\" to regenerate it.
+                The file "{fileSystem.Path.GetFullPath(VersionFile.FileName)}" is invalid:
+                  - #: Required properties ["$schema","version"] are not present.
+                  - The file is modified or outdated. Please run "brm generate" to regenerate it.
 
                 """));
 
@@ -95,7 +95,7 @@ namespace Bicep.RegistryModuleTool.IntegrationTests.Commands
             var mainBicepFilePath = fileSystem.Path.GetFullPath(MainBicepFile.FileName);
             var console = new MockConsole().ExpectErrorLines(
                 @$"{mainBicepFilePath}(1,1) : Error BCP007: This declaration type is not recognized. Specify a metadata, parameter, variable, resource, or output declaration. [https://aka.ms/bicep/core-diagnostics#BCP007]",
-                @$"Failed to build \"{mainBicepFilePath}\".");
+                @$"Failed to build ""{mainBicepFilePath}"".");
             await fileSystem.File.WriteAllTextAsync(MainBicepFile.FileName, "something");
             var sut = CreateValidateCommand(fileSystem, console);
 
@@ -125,7 +125,7 @@ namespace Bicep.RegistryModuleTool.IntegrationTests.Commands
             await fileSystem.File.WriteAllTextAsync(bicepTestFile.Path, "something");
             var console = new MockConsole().ExpectErrorLines(
                 @$"{bicepTestFile.Path}(1,1) : Error BCP007: This declaration type is not recognized. Specify a metadata, parameter, variable, resource, or output declaration. [https://aka.ms/bicep/core-diagnostics#BCP007]",
-                @$"Failed to build \"{bicepTestFile.Path}\".");
+                @$"Failed to build ""{bicepTestFile.Path}"".");
             var sut = CreateValidateCommand(fileSystem, console);
 
             await sut.Parse("").InvokeAsync();
